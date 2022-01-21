3a46774a5c9af54890426a21863bfbe28126f160
Keenan Graham <ksgraham@stanford.edu>
2022-01-20 16:42:11 -0800

    Add tests

---
+++ src/igvfd/tests/test_permissions.py
@@ -0,0 +1,136 @@
+import pytest
+
+
+def _remote_user_testapp(app, remote_user):
+    from webtest import TestApp
+    environ = {
+        'HTTP_ACCEPT': 'application/json',
+        'REMOTE_USER': str(remote_user),
+    }
+    return TestApp(app, environ)
+
+
+@pytest.fixture
+def remote_user_testapp(app, remote_user):
+    return _remote_user_testapp(app, remote_user)
+
+
+@pytest.fixture
+def wrangler_testapp(wrangler, app, external_tx, zsa_savepoints):
+    return _remote_user_testapp(app, wrangler['uuid'])
+
+
+@pytest.fixture
+def submitter_testapp(submitter, app, external_tx, zsa_savepoints):
+    return _remote_user_testapp(app, submitter['uuid'])
+
+
+@pytest.fixture
+def viewing_group_member_testapp(viewing_group_member, app, external_tx, zsa_savepoints):
+    return _remote_user_testapp(app, viewing_group_member['uuid'])
+
+
+def test_user_view_details_admin(submitter, access_key_2, testapp):
+    res = testapp.get(submitter['@id'])
+    assert 'email' in res.json
+    assert 'access_keys' in res.json
+    assert 'access_key_id' in res.json['access_keys'][0]
+
+
+def test_users_view_details_self(submitter, access_key_2, submitter_testapp):
+    res = submitter_testapp.get(submitter['@id'])
+    assert 'email' in res.json
+    assert 'access_keys' in res.json
+    assert 'access_key_id' in res.json['access_keys'][0]
+
+
+def test_users_patch_self(submitter, access_key, submitter_testapp):
+    submitter_testapp.patch_json(submitter['@id'], {})
+
+
+def test_users_post_disallowed(submitter, access_key, submitter_testapp):
+    item = {
+        'first_name': 'IGVF',
+        'last_name': 'Submitter2',
+        'email': 'igvf_submitter2@example.org',
+    }
+    submitter_testapp.post_json('/user', item, status=403)
+
+
+def test_users_view_basic_authenticated(submitter, authenticated_testapp):
+    res = authenticated_testapp.get(submitter['@id'])
+    assert 'title' in res.json
+    assert 'email' not in res.json
+    assert 'access_keys' not in res.json
+
+
+def test_users_view_basic_anon(submitter, anontestapp):
+    res = anontestapp.get(submitter['@id'])
+    assert 'title' in res.json
+    assert 'email' not in res.json
+    assert 'access_keys' not in res.json
+
+
+def test_submitter_patch_lab_disallowed(submitter, other_lab, submitter_testapp):
+    res = submitter_testapp.get(submitter['@id'])
+    lab = {'lab': other_lab['@id']}
+    submitter_testapp.patch_json(res.json['@id'], lab, status=422)
+
+
+def test_wrangler_patch_lab_allowed(submitter, other_lab, wrangler_testapp):
+    res = wrangler_testapp.get(submitter['@id'])
+    lab = {'lab': other_lab['@id']}
+    wrangler_testapp.patch_json(res.json['@id'], lab, status=200)
+
+
+def test_submitter_patch_submits_for_disallowed(submitter, other_lab, submitter_testapp):
+    res = submitter_testapp.get(submitter['@id'])
+    submits_for = {'submits_for': res.json['submits_for'] + [other_lab['@id']]}
+    submitter_testapp.patch_json(res.json['@id'], submits_for, status=422)
+
+
+def test_wrangler_patch_submits_for_allowed(submitter, other_lab, wrangler_testapp):
+    res = wrangler_testapp.get(submitter['@id'])
+    submits_for = {'submits_for': res.json['submits_for'] + [other_lab['@id']]}
+    wrangler_testapp.patch_json(res.json['@id'], submits_for, status=200)
+
+
+def test_submitter_patch_groups_disallowed(submitter, other_lab, submitter_testapp):
+    res = submitter_testapp.get(submitter['@id'])
+    groups = {'groups': res.json['groups'] + ['admin']}
+    submitter_testapp.patch_json(res.json['@id'], groups, status=422)
+
+
+def test_wrangler_patch_groups_allowed(submitter, other_lab, wrangler_testapp):
+    res = wrangler_testapp.get(submitter['@id'])
+    groups = {'groups': res.json['groups'] + ['admin']}
+    wrangler_testapp.patch_json(res.json['@id'], groups, status=200)
+
+
+def test_submitter_patch_viewing_groups_disallowed(submitter, other_lab, submitter_testapp):
+    res = submitter_testapp.get(submitter['@id'])
+    vgroups = {'viewing_groups': res.json['viewing_groups'] + ['community']}
+    submitter_testapp.patch_json(res.json['@id'], vgroups, status=422)
+
+
+def test_wrangler_patch_viewing_groups_allowed(submitter, other_lab, wrangler_testapp):
+    res = wrangler_testapp.get(submitter['@id'])
+    vgroups = {'viewing_groups': res.json['viewing_groups'] + ['community']}
+    wrangler_testapp.patch_json(res.json['@id'], vgroups, status=200)
+
+
+def test_disabled_user_denied_authenticated(authenticated_testapp, disabled_user):
+    authenticated_testapp.get(disabled_user['@id'], status=403)
+
+
+def test_disabled_user_denied_submitter(submitter_testapp, disabled_user):
+    submitter_testapp.get(disabled_user['@id'], status=403)
+
+
+def test_disabled_user_wrangler(wrangler_testapp, disabled_user):
+    wrangler_testapp.get(disabled_user['@id'], status=200)
+
+
+def test_labs_view_wrangler(wrangler_testapp, other_lab):
+    labs = wrangler_testapp.get('/labs/', status=200)
+    assert(len(labs.json['@graph']) == 1)
