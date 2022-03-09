4e8ea0bc1649acbe5bf72634b1b3c4b62ef9be85
Keenan Graham <ksgraham@stanford.edu>
2022-03-08 17:32:08 -0800

    Update

--- cdk/infrastructure/stacks/ci.py
+++ cdk/infrastructure/stacks/ci.py
@@ -12,7 +12,6 @@ from aws_cdk.aws_iam import PolicyStatement
 from aws_cdk.aws_iam import Role
 from aws_cdk.aws_iam import ServicePrincipal
 
-
 from infrastructure.naming import prepend_project_name
 
 
