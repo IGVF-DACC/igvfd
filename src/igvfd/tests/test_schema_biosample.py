7051604e84545fe02003973e058118a9dde94ab0
ian-whaling <whaling.ian@gmail.com>
2022-03-25 16:30:15 -0700

    abstract sample, added biosample, sample tests are biosample tests

--- src/igvfd/tests/test_schema_biosample.py
+++ src/igvfd/tests/test_schema_biosample.py
@@ -1,13 +1,13 @@
 import pytest
 
 
-def test_sample_1(sample_1, testapp):
-    res = testapp.get(sample_1['@id'])
+def test_sample_1(biosample_1, testapp):
+    res = testapp.get(biosample_1['@id'])
     assert(res.json['accession'][:6] == "IGVFSM")
 
 
-def test_lot_id_dependency(sample_1, testapp):
+def test_lot_id_dependency(biosample_1, testapp):
     res = testapp.patch_json(
-        sample_1['@id'],
+        biosample_1['@id'],
         {"lot_id": "R00002"}, expect_errors=True)
     assert(res.status_code == 422)
--- src/igvfd/types/sample.py
+++ src/igvfd/types/sample.py
@@ -1,4 +1,5 @@
 from snovault import (
+    abstract_collection,
     collection,
     load_schema,
 )
@@ -8,13 +9,25 @@ from .base import (
 )
 
 
-@collection(
+@abstract_collection(
     name='samples',
-    unique_key='sample:uuid',
+    unique_key='accession',
     properties={
         'title': 'Samples',
         'description': 'Listing samples',
     })
 class Sample(Item):
-    item_type = 'sample'
+    base_types = ['Sample'] + Item.base_types
     schema = load_schema('igvfd:schemas/sample.json')
+
+
+@collection(
+    name='biosamples',
+    unique_key='accession',
+    properties={
+        'title': 'Biosamples',
+        'description': 'Listing biosamples',
+    })
+class Biosample(Sample):
+    item_type = 'biosample'
+    schema = load_schema('igvfd:schemas/biosample.json')
