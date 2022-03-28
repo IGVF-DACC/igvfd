143bf0fd0e933d629e949464689dea0e8b907bd6
ian-whaling <whaling.ian@gmail.com>
2022-03-28 10:46:44 -0700

    added organism, age, age_units properties

--- src/igvfd/tests/test_schema_biosample.py
+++ src/igvfd/tests/test_schema_biosample.py
@@ -11,3 +11,19 @@ def test_lot_id_dependency(biosample_1, testapp):
         biosample_1['@id'],
         {"lot_id": "R00002"}, expect_errors=True)
     assert(res.status_code == 422)
+
+def test_age_unit_dependency(biosample_1, testapp):
+    res = testapp.patch_json(
+        biosample_1['@id'],
+        {"organism": "Homo sapiens", "age_units": "year"})
+    assert(res.status_code == 200)
+    res = testapp.patch_json(
+        biosample_1['@id'],
+        {"age_units": "minute"}, expect_errors=True)
+    print(res.status_code)
+    assert(res.status_code == 422)
+    res = testapp.patch_json(
+        biosample_1['@id'],
+        {"organism": "Saccharomyces", "age_units": "year"}, expect_errors=True)
+    print(res.status_code)
+    assert(res.status_code == 422)
