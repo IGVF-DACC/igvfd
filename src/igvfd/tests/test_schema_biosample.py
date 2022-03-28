4bbb833aa1fdb1199bc0e556bf56884f020661a7
ian-whaling <whaling.ian@gmail.com>
2022-03-28 14:03:53 -0700

    pre-commit

--- src/igvfd/tests/test_schema_biosample.py
+++ src/igvfd/tests/test_schema_biosample.py
@@ -3,42 +3,44 @@ import pytest
 
 def test_sample_1(biosample_1, testapp):
     res = testapp.get(biosample_1['@id'])
-    assert(res.json['accession'][:6] == "IGVFSM")
+    assert(res.json['accession'][:6] == 'IGVFSM')
 
 
 def test_lot_id_dependency(biosample_1, testapp):
     res = testapp.patch_json(
         biosample_1['@id'],
-        {"lot_id": "R00002"}, expect_errors=True)
+        {'lot_id': 'R00002'}, expect_errors=True)
     assert(res.status_code == 422)
+
 
 def test_age_unit_dependency(biosample_1, testapp):
     res = testapp.patch_json(
         biosample_1['@id'],
-        {"organism": "Homo sapiens", "age_units": "year"})
+        {'organism': 'Homo sapiens', 'age_units': 'year'})
     assert(res.status_code == 200)
     res = testapp.patch_json(
         biosample_1['@id'],
-        {"age_units": "minute"}, expect_errors=True)
+        {'age_units': 'minute'}, expect_errors=True)
     assert(res.status_code == 422)
     res = testapp.patch_json(
         biosample_1['@id'],
-        {"organism": "Saccharomyces", "age_units": "year"}, expect_errors=True)
+        {'organism': 'Saccharomyces', 'age_units': 'year'}, expect_errors=True)
     assert(res.status_code == 422)
+
 
 def test_age_unit_dependency(biosample_1, testapp):
     print(biosample_1)
     res = testapp.patch_json(
         biosample_1['@id'],
-        {"organism": "Homo sapiens", "life_stage": "adult"})
+        {'organism': 'Homo sapiens', 'life_stage': 'adult'})
     assert(res.status_code == 200)
     res = testapp.patch_json(
         biosample_1['@id'],
-        {"organism": "Mus musculus", "life_stage": "stationary"}, expect_errors=True)
+        {'organism': 'Mus musculus', 'life_stage': 'stationary'}, expect_errors=True)
     print(res.status_code)
     assert(res.status_code == 422)
     res = testapp.patch_json(
         biosample_1['@id'],
-        {"organism": "Saccharomyces", "life_stage": "adult"}, expect_errors=True)
+        {'organism': 'Saccharomyces', 'life_stage': 'adult'}, expect_errors=True)
     print(res.status_code)
     assert(res.status_code == 422)
