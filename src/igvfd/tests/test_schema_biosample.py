74dbcd9fbcd1ffdae9b81e0bab929355518903eb
ian-whaling <whaling.ian@gmail.com>
2022-03-28 11:44:12 -0700

    added life_stage

--- src/igvfd/tests/test_schema_biosample.py
+++ src/igvfd/tests/test_schema_biosample.py
@@ -20,10 +20,25 @@ def test_age_unit_dependency(biosample_1, testapp):
     res = testapp.patch_json(
         biosample_1['@id'],
         {"age_units": "minute"}, expect_errors=True)
-    print(res.status_code)
     assert(res.status_code == 422)
     res = testapp.patch_json(
         biosample_1['@id'],
         {"organism": "Saccharomyces", "age_units": "year"}, expect_errors=True)
+    assert(res.status_code == 422)
+
+def test_age_unit_dependency(biosample_1, testapp):
+    print(biosample_1)
+    res = testapp.patch_json(
+        biosample_1['@id'],
+        {"organism": "Homo sapiens", "life_stage": "adult"})
+    assert(res.status_code == 200)
+    res = testapp.patch_json(
+        biosample_1['@id'],
+        {"organism": "Mus musculus", "life_stage": "stationary"}, expect_errors=True)
+    print(res.status_code)
+    assert(res.status_code == 422)
+    res = testapp.patch_json(
+        biosample_1['@id'],
+        {"organism": "Saccharomyces", "life_stage": "adult"}, expect_errors=True)
     print(res.status_code)
     assert(res.status_code == 422)
