7688044440840569ca0d382cb15c61faa766e690
ian-whaling <whaling.ian@gmail.com>
2022-04-11 10:14:03 -0700

    rebase problems

--- src/igvfd/schemas/changelogs/mixins.md
+++ src/igvfd/schemas/changelogs/mixins.md
@@ -3,3 +3,4 @@
 ### Changes since schema version 1
 
 * 3/24/22 Added product_id, lot_id, source mixins
+* 4/7/22 Added product_id, lot_id, source mixins to a set called product_info
--- src/igvfd/tests/test_schema_source.py
+++ src/igvfd/tests/test_schema_source.py
@@ -4,12 +4,7 @@ def test_sample_1(source, testapp):
 
 
 def test_lab_as_source(lab, award, testapp):
-
-<<<<<<< HEAD
-    res = testapp.post_json('/biosample',
-=======
     res = testapp.post_json('/cell_line',
->>>>>>> b5fb316 (added tissue)
                             {
                                 'award': award['@id'],
                                 'lab': lab['@id'],
