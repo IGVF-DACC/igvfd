fc8e9fbe909e15ce6ccbf18d961a983045a0d021
Jennifer Jou <jjou@stanford.edu>
2022-04-14 11:57:46 -0700

    Remove accidental line in schema test

--- src/igvfd/tests/test_schema_sample_ontology_term.py
+++ src/igvfd/tests/test_schema_sample_ontology_term.py
@@ -2,7 +2,6 @@ import pytest
 
 
 def test_sample_ontology_term_term_id_regex(sample_ontology_term_1, testapp):
-    res = testapp.get(biosample_1['@id'])
     res = testapp.patch_json(
         sample_ontology_term_1['@id'],
         {'term_id': 'ABC:12345'})
