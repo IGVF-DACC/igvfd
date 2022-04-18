ee5ebb42a5ea7cc333a8a23a919be3f30fcef09b
Keenan Graham <ksgraham@stanford.edu>
2022-04-18 14:20:30 -0700

    Make filename a variable

--- src/igvfd/ontology.py
+++ src/igvfd/ontology.py
@@ -9,6 +9,9 @@ def includeme(config):
     config.registry['ontology'] = load_ontology()
 
 
+ONTOLOGY_NAME = 'ontology.json.gz'
+
+
 def load_local_gz_json(path):
     with gzip.open(path, 'rt', encoding='utf-8') as local_file:
         data = json.load(local_file)
@@ -16,7 +19,7 @@ def load_local_gz_json(path):
 
 
 def get_ontology_gz_json_path():
-    return Path(__file__).resolve().parents[2] / 'assets' / 'ontology.json.gz'
+    return Path(__file__).resolve().parents[2] / 'assets' / ONTOLOGY_NAME
 
 
 def ontology_or_empty_dict(path):
