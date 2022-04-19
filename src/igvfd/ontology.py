c3b0081c448b232377e55299d69d0aaa6da632dc
keenangraham <ksgraham@stanford.edu>
2022-04-19 13:29:12 -0700

    IGVF-83-load-gz-ontology-json (#29)

---
+++ src/igvfd/ontology.py
@@ -0,0 +1,33 @@
+import gzip
+import json
+
+from pathlib import Path
+
+
+def includeme(config):
+    config.scan(__name__)
+    config.registry['ontology'] = load_ontology()
+
+
+ONTOLOGY_FILE_NAME = 'ontology.json.gz'
+
+
+def load_local_gz_json(path):
+    with gzip.open(path, 'rt', encoding='utf-8') as local_file:
+        data = json.load(local_file)
+    return data
+
+
+def get_ontology_gz_json_path():
+    return Path(__file__).resolve().parents[2] / 'assets' / ONTOLOGY_FILE_NAME
+
+
+def ontology_or_empty_dict(path):
+    if path.exists():
+        return load_local_gz_json(path)
+    return {}
+
+
+def load_ontology():
+    path = get_ontology_gz_json_path()
+    return ontology_or_empty_dict(path)
