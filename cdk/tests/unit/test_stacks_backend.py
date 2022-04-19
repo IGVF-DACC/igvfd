036efbdb8a135ac25dd1ec51564b29a8b8932d2b
Keenan Graham <ksgraham@stanford.edu>
2022-04-19 14:58:30 -0700

    Remove test

--- cdk/tests/unit/test_stacks_backend.py
+++ cdk/tests/unit/test_stacks_backend.py
@@ -6,13 +6,3 @@ from infrastructure.stacks.backend import BackendStack
 
 def test_stacks_backend_backend_stack_created():
     app = core.App()
-    stack = BackendStack(
-        app,
-        'BackendStack',
-    )
-    template = assertions.Template.from_stack(stack)
-    print(template.to_json())
-    assert True
-#    template.has_resource_properties("AWS::SQ::Queue", {
-#         "VisibilityTimeout": 300
-#     })
