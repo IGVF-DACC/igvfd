f41629daea78ed6463709b244b59021dcf458f0d
Jennifer Jou <jjou@stanford.edu>
2022-04-19 14:19:23 -0700

    Fix test

--- src/igvfd/tests/test_schema_sample_ontology_term.py
+++ src/igvfd/tests/test_schema_sample_ontology_term.py
@@ -4,5 +4,6 @@ import pytest
 def test_sample_ontology_term_term_id_regex(sample_ontology_term_1, testapp):
     res = testapp.patch_json(
         sample_ontology_term_1['@id'],
-        {'term_id': 'ABC:12345'})
+        {'term_id': 'ABC:12345'},
+        expect_errors=True)
     assert(res.status_code == 422)
