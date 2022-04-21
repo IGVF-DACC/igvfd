17f48f7d6ff06f6d7878b4e48d71360ca7e4dd04
Keenan Graham <ksgraham@stanford.edu>
2022-04-21 15:06:19 -0700

    Try splitting stacks

---
+++ cdk/infrastructure/stacks/repository.py
@@ -0,0 +1,40 @@
+import aws_cdk as cdk
+
+from aws_cdk.aws_ec2 import InstanceType
+from aws_cdk.aws_ec2 import InstanceClass
+from aws_cdk.aws_ec2 import InstanceSize
+from aws_cdk.aws_ec2 import SubnetSelection
+from aws_cdk.aws_ec2 import SubnetType
+
+from aws_cdk.aws_rds import DatabaseInstance
+from aws_cdk.aws_rds import DatabaseInstanceEngine
+from aws_cdk.aws_rds import PostgresEngineVersion
+
+
+class PostgresStack(cdk.Stack):
+
+    def __init__(self, scope, construct_id, vpc, security_group, branch, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+        self.engine = DatabaseInstanceEngine.postgres(
+            version=PostgresEngineVersion.VER_14_1
+        )
+        self.database_name = 'igvfd'
+        self.database = DatabaseInstance(
+            self,
+            'Postgres',
+            database_name=self.database_name,
+            engine=self.engine,
+            instance_type=InstanceType.of(
+                InstanceClass.BURSTABLE3,
+                InstanceSize.MEDIUM,
+            ),
+            vpc=vpc,
+            vpc_subnets=SubnetSelection(
+                subnet_type=SubnetType.PUBLIC,
+            ),
+            allocated_storage=10,
+            max_allocated_storage=20,
+            security_groups=[
+                security_group,
+            ],
+        )
