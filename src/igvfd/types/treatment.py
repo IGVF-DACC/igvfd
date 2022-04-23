9d88bcb2fc3f68c341005881ad9276923ed9dda1
Meenakshi <mkagd001@ucr.edu>
2022-04-23 14:04:04 -0700

    format types

--- src/igvfd/types/treatment.py
+++ src/igvfd/types/treatment.py
@@ -26,4 +26,10 @@ class Treatment(Item):
         }
     )
     def title(self, treatment_term_name, amount, amount_units, duration, duration_units):
-        return u'{} {} {} {} {}'.format(treatment_term_name, amount, amount_units, duration, duration_units)
+        text = 'Treated with {} {} {} for {} {}.'.format(
+            amount, amount_units, treatment_term_name, duration, duration_units)
+        if post_treatment_time:
+            text = 'Post treatment waited for {} {}.'.format(post_treatment_time, post_treatment_time_units)
+        else:
+            text = text
+        return(text)
