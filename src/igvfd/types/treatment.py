9cc3cbf043293b0f7c3bc3a5cc31f72e5052eb0b
Meenakshi <mkagd001@ucr.edu>
2022-04-23 13:18:41 -0700

    add types

---
+++ src/igvfd/types/treatment.py
@@ -0,0 +1,34 @@
+from snovault import (
+    collection,
+    load_schema,
+    calculated_property
+)
+from .base import (
+    Item
+)
+
+
+@collection(
+    name='treatments',
+    unique_key='uuid',
+    properties={
+        'title': 'Treatment',
+        'description': 'Listing of treatments',
+    })
+class Treatment(Item):
+    item_type = 'treatment'
+    schema = load_schema('igvfd:schemas/treatment.json')
+
+    @calculated_property(
+        schema={
+            'title': 'Title',
+            'type': 'string',
+        }
+    )
+    def title(self, treatment_term_name, amount, amount_units, duration, duration_units):
+        text = treatment_term_name
+        if amount:
+            text = text + ' ' + amount + ' ' + amount_units
+        if duration:
+            text = text + ' ' + duration + ' ' + duration_units
+        return text
