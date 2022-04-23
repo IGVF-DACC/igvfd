7247d579b8255a534d445fc467e7040548ed48aa
Keenan Graham <ksgraham@stanford.edu>
2022-04-22 20:48:47 -0700

    Run in igvf-dev account

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -1,5 +1,15 @@
+import aws_cdk as cdk
+
+
 config = {
     'org_name': 'IGVF',
     'project_name': 'igvfd',
     'default_branch': 'main',
+    'region': 'us-west-2',
 }
+
+
+IGVF_DEV_US_WEST_2 = cdk.Environment(
+    account='109189702753',
+    region='us-west-2',
+)
