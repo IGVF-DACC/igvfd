5d34dc8583b6073123362ac8e07a08a9005c69c6
Jennifer Jou <jjou@stanford.edu>
2022-04-26 12:22:53 -0700

    Fix description and add another test

--- src/igvfd/tests/test_schema_sample_ontology_term.py
+++ src/igvfd/tests/test_schema_sample_ontology_term.py
@@ -7,3 +7,15 @@ def test_sample_ontology_term_term_id_regex(sample_ontology_term_1, testapp):
         {'term_id': 'ABC:12345'},
         expect_errors=True)
     assert(res.status_code == 422)
+
+
+def test_sample_ontology_term_slims(sample_ontology_term_1, testapp):
+    res = testapp.get(sample_ontology_term_1['@id'] + '@@index-data')
+    expected_organ_slims = ['blood', 'bodily fluid']
+    expected_cell_slims = ['hematopoietic cell', 'cancer cell', 'leukocyte']
+    expected_developmental_slims = ['mesoderm']
+    expected_system_slims = ['immune system']
+    assert(all(slim in res['organ_slims'] for slim in expected_organ_slims))
+    assert(all(slim in res['cell_slims'] for slim in expected_cell_slims))
+    assert(all(slim in res['developmental_slims'] for slim in expected_developmental_slims))
+    assert(all(slim in res['system_slims'] for slim in expected_system_slims))
