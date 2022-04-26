5ff836798de7fd1b722b415f94c130fde0adf90a
Keenan Graham <ksgraham@stanford.edu>
2022-04-26 16:23:50 -0700

    Convert buildspec to object

--- cdk/infrastructure/stacks/ci.py
+++ cdk/infrastructure/stacks/ci.py
@@ -1,47 +1,41 @@
 import aws_cdk as cdk
 
-from aws_cdk.aws_codebuild import BuildSpec
-
-from infrastructure.constructs.ci import PublicContinuousIntegrationForGithub
-
 
 def get_build_spec():
-    return BuildSpec.from_object(
-        {
-            'version': '0.2',
-            'env': {
-                'secrets-manager': {
-                    'DOCKER_USER': 'docker-hub-credentials:DOCKER_USER',
-                    'DOCKER_SECRET': 'docker-hub-credentials:DOCKER_SECRET',
-                },
+    return {
+        'version': '0.2',
+        'env': {
+            'secrets-manager': {
+                'DOCKER_USER': 'docker-hub-credentials:DOCKER_USER',
+                'DOCKER_SECRET': 'docker-hub-credentials:DOCKER_SECRET',
             },
-            'phases': {
-                'install': {
-                    'runtime-versions': {
-                        'python': '3.9',
-                    },
-                    'commands': [
-                        'echo $CODEBUILD_WEBHOOK_TRIGGER',
-                        'echo $(git log -1 --pretty="%s (%h) - %an")',
-                        'echo Logging into Docker',
-                        'echo $DOCKER_SECRET | docker login --username $DOCKER_USER --password-stdin',
-                        'echo Building images',
-                        'docker-compose -f docker-compose.test.yml build',
-                        'echo Setting permission to share mounted volume between users',
-                        'sudo useradd -u 1444 igvfd',
-                        'sudo usermod -a -G igvfd root',
-                        'sudo chown -R root:igvfd ./',
-                        'sudo chmod -R g+rwX ./',
-                    ]
+        },
+        'phases': {
+            'install': {
+                'runtime-versions': {
+                    'python': '3.9',
                 },
-                'build': {
-                    'commands': [
-                        'docker-compose -f docker-compose.test.yml up --exit-code-from pyramid',
-                    ]
-                }
+                'commands': [
+                    'echo $CODEBUILD_WEBHOOK_TRIGGER',
+                    'echo $(git log -1 --pretty="%s (%h) - %an")',
+                    'echo Logging into Docker',
+                    'echo $DOCKER_SECRET | docker login --username $DOCKER_USER --password-stdin',
+                    'echo Building images',
+                    'docker-compose -f docker-compose.test.yml build',
+                    'echo Setting permission to share mounted volume between users',
+                    'sudo useradd -u 1444 igvfd',
+                    'sudo usermod -a -G igvfd root',
+                    'sudo chown -R root:igvfd ./',
+                    'sudo chmod -R g+rwX ./',
+                ]
             },
-        }
-    )
+            'build': {
+                'commands': [
+                    'docker-compose -f docker-compose.test.yml up --exit-code-from pyramid',
+                ]
+            }
+        },
+    }
 
 
 class ContinuousIntegrationStack(cdk.Stack):
