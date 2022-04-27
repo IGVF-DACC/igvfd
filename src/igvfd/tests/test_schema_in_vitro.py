b70c630e3ac62e54d9fbdb045510465245c215d5
ian-whaling <whaling.ian@gmail.com>
2022-04-27 11:30:50 -0700

    treatment description, comment, pluralization

--- src/igvfd/tests/test_schema_in_vitro.py
+++ src/igvfd/tests/test_schema_in_vitro.py
@@ -18,11 +18,7 @@ def test_differentiation_dependency(in_vitro, testapp):
     assert(res.status_code == 200)
 
 
-<<<<<<< HEAD
 def test_post_in_vitro(testapp, award, lab, treatment_2):
-=======
-def test_post_in_vitro(testapp, award, lab):
->>>>>>> 08759e2 (tests)
     res = testapp.post_json(
         '/in_vitro',
         {
@@ -31,11 +27,7 @@ def test_post_in_vitro(testapp, award, lab):
             'source': lab['@id'],
             'post_differentiation_time': 20,
             'post_differentiation_time_units': 'minute',
-<<<<<<< HEAD
             'treatments': [treatment_2['@id']]
-=======
-            'treatment': 'treatment'
->>>>>>> 08759e2 (tests)
         })
     assert(res.status_code == 201)
 
@@ -46,11 +38,6 @@ def test_post_in_vitro(testapp, award, lab):
             'lab': lab['@id'],
             'source': lab['@id'],
             'post_differentiation_time': 20,
-<<<<<<< HEAD
             'post_differentiation_time_units': 'second'
-=======
-            'post_differentiation_time_units': 'second',
-            'treatment': 'treatment'
->>>>>>> 08759e2 (tests)
         }, expect_errors=True)
     assert(res.status_code == 422)
