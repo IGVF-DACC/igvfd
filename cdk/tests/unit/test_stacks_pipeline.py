0b530887eece4fded5cbb7c84024c72f931763ad
Keenan Graham <ksgraham@stanford.edu>
2022-04-26 20:09:08 -0700

    Update test

--- cdk/tests/unit/test_stacks_pipeline.py
+++ cdk/tests/unit/test_stacks_pipeline.py
@@ -5,15 +5,14 @@ from infrastructure.stacks.pipeline import ContinuousDeploymentPipelineStack
 
 
 def test_stacks_pipeline_continuous_deployment_pipeline_stack_created():
-    from infrastructure.config import IGVF_DEV_US_WEST_2
-    from infrastructure.constructs.existing import IgvfDevExistingResources
+    from infrastructure.constructs.existing import igvf_dev
     app = core.App()
     stack = ContinuousDeploymentPipelineStack(
         app,
         'CDStack',
         branch='test-pipeline-branch',
-        existing_construct=IgvfDevExistingResources,
-        env=IGVF_DEV_US_WEST_2,
+        existing_resources=igvf_dev.Resources,
+        env=igvf_dev.US_WEST_2,
     )
     template = assertions.Template.from_stack(stack)
     template.resource_count_is(
@@ -24,7 +23,7 @@ def test_stacks_pipeline_continuous_deployment_pipeline_stack_created():
         'AWS::CodePipeline::Pipeline'
     )
     stages = code_pipeline_resource.get(
-        'testpipelinebranchCodePipelineBB1A604B'
+        'ContinuousDeploymentPipelinetestpipelinebranchCodePipelineD2E84AAD'
     ).get(
         'Properties'
     ).get(
