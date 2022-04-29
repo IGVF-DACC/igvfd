5350faadc2cbd26e7de8319446e421e9b1c62d07
Meenakshi <mkagd001@ucr.edu>
2022-04-29 11:00:48 -0700

    fix linting

--- src/igvfd/types/treatment.py
+++ src/igvfd/types/treatment.py
@@ -25,36 +25,11 @@ class Treatment(Item):
             'type': 'string',
         }
     )
-<<<<<<< HEAD
-<<<<<<< HEAD
-
     def title(self, treatment_term_name, amount, amount_units, duration=None, duration_units=None):
         if duration is not None:
             text = 'Treated with {} {} {} for {} {}'.format(
-=======
-    def title(self, treatment_term_name, amount, amount_units, duration, duration_units):
-        if duration:
-=======
-    def title(self, treatment_term_name, amount, amount_units, duration=None, duration_units=None):
-        if duration is not None:
-<<<<<<< HEAD
->>>>>>> 97e30e7 (update tests)
-            text = 'Treated with {} {} {} for {} {}.'.format(
->>>>>>> 950b9e5 (update required)
-=======
-            text = 'Treated with {} {} {} for {} {}'.format(
->>>>>>> 7e743b1 (update tests)
                 amount, amount_units, treatment_term_name, duration, duration_units)
         else:
             text = 'Treated with {} {} {} for non-specified duration'.format(
                 amount, amount_units, treatment_term_name)
-<<<<<<< HEAD
-<<<<<<< HEAD
         return text
-
-=======
-        return(text)
->>>>>>> 950b9e5 (update required)
-=======
-        return text
->>>>>>> 97e30e7 (update tests)
