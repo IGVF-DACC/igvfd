2ace2ed54f9cff02715d389ee6793e6ead2ddca8
Meenakshi <mkagd001@ucr.edu>
2022-04-29 15:40:37 -0700

    add test types

--- src/igvfd/tests/test_schema_treatment.py
+++ src/igvfd/tests/test_schema_treatment.py
@@ -23,16 +23,6 @@ def test_treatment_post_treatment_time_dependency(treatment_2, testapp):
     assert(res.status_code == 200)
 
 
-def test_treatment_calculated(treatment_1, testapp):
-    res = testapp.get(treatment_1['@id'])
-    assert(res.json['title'] == 'Treated with 10 mM lactate for 1 hour')
-
-
-def test_treatment_no_duration_calculated(treatment_2, testapp):
-    res = testapp.get(treatment_2['@id'])
-    assert(res.json['title'] == 'Treated with 10 ng/mL G-CSF for non-specified duration')
-
-
 def test_treatment_type_dependency(treatment_1, testapp):
     res = testapp.patch_json(
         treatment_1['@id'],
