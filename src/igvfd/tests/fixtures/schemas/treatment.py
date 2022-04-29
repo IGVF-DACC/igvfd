04393fc3fd5d8c9b87cfc6e492913e0327a57c0d
Meenakshi <mkagd001@ucr.edu>
2022-04-29 10:53:56 -0700

    fix linting

--- src/igvfd/tests/fixtures/schemas/treatment.py
+++ src/igvfd/tests/fixtures/schemas/treatment.py
@@ -14,29 +14,13 @@ def treatment_1(testapp):
     }
     return testapp.post_json('/treatment', item, status=201).json['@graph'][0]
 
-<<<<<<< HEAD
-@pytest.fixture
-=======
 
-<<<<<<< HEAD
->>>>>>> 950b9e5 (update required)
-=======
-@pytest.fixture
->>>>>>> 97e30e7 (update tests)
 def treatment_2(testapp):
     item = {
         'treatment_term_id': 'UniProtKB:P09919',
         'treatment_term_name': 'G-CSF',
         'treatment_type': 'protein',
         'amount': 10,
-<<<<<<< HEAD
-<<<<<<< HEAD
         'amount_units': 'ng/mL'
-=======
-        'amount_units': 'ng/kg'
->>>>>>> 950b9e5 (update required)
-=======
-        'amount_units': 'ng/mL'
->>>>>>> 97e30e7 (update tests)
     }
     return testapp.post_json('/treatment', item, status=201).json['@graph'][0]
