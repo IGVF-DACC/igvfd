9f9a0845c9e0d18b1d8f84c49c57354687a2f5c4
Keenan Graham <ksgraham@stanford.edu>
2022-05-03 15:41:45 -0700

    Add dev stage test

--- cdk/tests/unit/test_stages_dev.py
+++ cdk/tests/unit/test_stages_dev.py
@@ -2,4 +2,21 @@ import pytest
 
 
 def test_stages_dev_initialize_dev_stages():
-    assert False
+    from aws_cdk import Stage
+    from aws_cdk import App
+    from infrastructure.stages.dev import DevelopmentDeployStage
+    app = App()
+    branch = 'some-branch'
+    dev_deploy_stage = DevelopmentDeployStage(
+        app,
+        'TestDevelopmentDeployStage',
+        branch=branch,
+    )
+    cloud_assembly = dev_deploy_stage.synth()
+    assert [
+        stack.stack_name
+        for stack in cloud_assembly.stacks
+    ] == [
+        'TestDevelopmentDeployStage-PostgresStack',
+        'TestDevelopmentDeployStage-BackendStack',
+    ]
