3b2dbce87c7b23494a88eca5eb26e93eb52e5577
Keenan Graham <ksgraham@stanford.edu>
2022-05-03 15:41:45 -0700

    Add dev stage test

--- cdk/tests/unit/test_stages_ci.py
+++ cdk/tests/unit/test_stages_ci.py
@@ -1,5 +1,21 @@
 import pytest
 
+from aws_cdk.assertions import Template
+
 
 def test_stages_ci_initialize_ci_stage():
-    assert False
+    from aws_cdk import Stage
+    from aws_cdk import App
+    from infrastructure.stages.ci import CIDeployStage
+    app = App()
+    ci_deploy_stage = CIDeployStage(
+        app,
+        'TestCIDeployStage',
+    )
+    cloud_assembly = ci_deploy_stage.synth()
+    assert [
+        stack.stack_name
+        for stack in cloud_assembly.stacks
+    ] == [
+        'TestCIDeployStage-ContinuousIntegrationStack',
+    ]
