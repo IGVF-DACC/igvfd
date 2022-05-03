69956daed565379ebb17283a00eb908a20327869
Keenan Graham <ksgraham@stanford.edu>
2022-05-03 14:52:29 -0700

    Add test for empty stages

--- cdk/tests/unit/test_stages_prod.py
+++ cdk/tests/unit/test_stages_prod.py
@@ -2,4 +2,12 @@ import pytest
 
 
 def test_stages_prod_initialize_prod_stage():
-    assert False
+    from aws_cdk import Stage
+    from aws_cdk import App
+    from infrastructure.stages.prod import ProdDeployStage
+    app = App()
+    prod_deploy_stage = ProdDeployStage(
+        app,
+        'TestProdDeployStage'
+    )
+    assert isinstance(prod_deploy_stage, Stage)
