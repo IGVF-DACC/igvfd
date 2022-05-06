6b2db59c642f85eeea9e6a6132e48fd3668e3f14
Meenakshi <mkagd001@ucr.edu>
2022-05-05 22:48:04 -0700

    add tests

---
+++ src/igvfd/tests/test_schema_gene.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+def test_gene_assembly_locations(testapp, gene_myc_hs):
+    res = testapp.patch_json(
+        gene_myc_hs['@id'],
+        {'locations': [
+            {
+                'assembly': 'mm10', 'chromosome': 'chr18', 'start': 47808713, 'end': 47814692
+            }]}, expect_errors=True)
+    assert(res.status_code == 422)
+    res = testapp.patch_json(
+        gene_myc_hs['@id'],
+        {'locations': [{'assembly': 'hg19', 'chromosome': 'chr18', 'start': 47808713, 'end': 47814692}]})
+    assert(res.status_code == 200)
