51dad0449250ff60868551f96662caf5b6e6c07f
Idan <gabdank@stanford.edu>
2022-05-11 18:44:04 -0700

    red dot

--- src/igvfd/tests/fixtures/schemas/document.py
+++ src/igvfd/tests/fixtures/schemas/document.py
@@ -8,6 +8,6 @@ def experimental_protocol_document(testapp, other_lab, award):
         'description': 'Generic experimental protocol',
         'award': award['@id'],
         'lab': other_lab['@id'],
-        'attachment': {'download': 'Antibody_Characterization_IGVF.pdf', 'href': Antibody_Characterization_IGVF.pdf},
+        'attachment': {'download': 'red-dot.png', 'href': RED_DOT},
     }
     return testapp.post_json('/document', item).json['@graph'][0]
