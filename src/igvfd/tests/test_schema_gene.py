7c5273c9e5de9a325ce8b1cce0f98470755c3459
Meenakshi <mkagd001@ucr.edu>
2022-05-12 23:25:27 -0700

    add more tests

--- src/igvfd/tests/test_schema_gene.py
+++ src/igvfd/tests/test_schema_gene.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_gene_assembly_locations(testapp, gene_myc_hs):
+def test_gene_assembly_locations_hs(testapp, gene_myc_hs):
     res = testapp.patch_json(
         gene_myc_hs['@id'],
         {
@@ -20,10 +20,94 @@ def test_gene_assembly_locations(testapp, gene_myc_hs):
         {
             'locations': [
                 {
+                    'assembly': 'mm9',
+                    'chromosome': 'chr18',
+                    'start': 47808713,
+                    'end': 47814692
+                }
+            ]
+        }, expect_errors=True)
+    assert(res.status_code == 422)
+    res = testapp.patch_json(
+        gene_myc_hs['@id'],
+        {
+            'locations': [
+                {
                     'assembly': 'hg19',
                     'chromosome': 'chr18',
                     'start': 47808713,
                     'end': 47814692
+                }
+            ]
+        }
+    )
+    assert(res.status_code == 200)
+    res = testapp.patch_json(
+        gene_myc_hs['@id'],
+        {
+            'locations': [
+                {
+                    'assembly': 'GRCh38',
+                    'chromosome': 'chr18',
+                    'start': 47808713,
+                    'end': 47814692
+                }
+            ]
+        }
+    )
+    assert(res.status_code == 200)
+
+
+def test_gene_assembly_locations_mm(testapp, gene_zscan10_mm):
+    res = testapp.patch_json(
+        gene_zscan10_mm['@id'],
+        {
+            'locations': [
+                {
+                    'assembly': 'hg19',
+                    'chromosome': 'chr18',
+                    'start': 47808713,
+                    'end': 47814692
+                }
+            ]
+        }, expect_errors=True)
+    assert(res.status_code == 422)
+    res = testapp.patch_json(
+        gene_zscan10_mm['@id'],
+        {
+            'locations': [
+                {
+                    'assembly': 'GRCh38',
+                    'chromosome': 'chr18',
+                    'start': 47808713,
+                    'end': 47814692
+                }
+            ]
+        }, expect_errors=True)
+    assert(res.status_code == 422)
+    res = testapp.patch_json(
+        gene_zscan10_mm['@id'],
+        {
+            'locations': [
+                {
+                    'assembly': 'mm9',
+                    'chromosome': 'chr17',
+                    'start': 23737823,
+                    'end': 23747986
+                }
+            ]
+        }
+    )
+    assert(res.status_code == 200)
+    res = testapp.patch_json(
+        gene_zscan10_mm['@id'],
+        {
+            'locations': [
+                {
+                    'assembly': 'mm10',
+                    'chromosome': 'chr17',
+                    'start': 23600826,
+                    'end': 23611019
                 }
             ]
         }
