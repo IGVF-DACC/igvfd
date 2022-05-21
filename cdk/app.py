967b5faa74df5fc4a52780e1703a7a7ca1176619
Keenan Graham <ksgraham@stanford.edu>
2022-05-20 17:01:14 -0700

    Add app-level tests

--- cdk/app.py
+++ cdk/app.py
@@ -1,47 +1,13 @@
-import aws_cdk as cdk
+from aws_cdk import App
 
-from infrastructure.constructs.existing import igvf_dev
-
-from infrastructure.config import build_config_from_name
-from infrastructure.config import get_config_name_from_branch
-from infrastructure.naming import prepend_project_name
-from infrastructure.naming import prepend_branch_name
-
-from infrastructure.stacks.pipeline import pipeline_stack_factory
+from infrastructure.build import build
 
 
-app = cdk.App()
+def synth() -> None:
+    app = App()
+    build(app)
+    app.synth()
 
-branch = app.node.try_get_context('branch')
 
-if branch is None:
-    raise ValueError('Must specify branch context: `-c branch=$BRANCH`')
-
-config_name = (
-    app.node.try_get_context('config-name')
-    or get_config_name_from_branch(branch)
-)
-
-config = build_config_from_name(
-    config_name,
-    branch=branch,
-)
-
-pipeline_class = pipeline_stack_factory(
-    config.pipeline
-)
-
-pipeline = pipeline_class(
-    app,
-    prepend_project_name(
-        prepend_branch_name(
-            branch,
-            pipeline_class.__name__,
-        )
-    ),
-    existing_resources_class=igvf_dev.Resources,
-    config=config,
-    env=igvf_dev.US_WEST_2,
-)
-
-app.synth()
+if __name__ == '__main__':
+    synth()
