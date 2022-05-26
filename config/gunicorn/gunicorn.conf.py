ab76e17138fbf49ee1b924f8c91b79a0c18aced9
Keenan Graham <ksgraham@stanford.edu>
2022-05-25 20:18:36 -0700

    Run gunicorn with external command

---
+++ config/gunicorn/gunicorn.conf.py
@@ -0,0 +1,4 @@
+bind = '0.0.0.0:6543'
+workers = 4
+threads = 1
+accesslog = '-'
--- scripts/pyramid/run.sh
+++ scripts/pyramid/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export SQLALCHEMY_URL=postgresql://postgres:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}
-pserve ./config/pyramid/ini/fargate.ini
+gunicorn --config ./config/gunicorn/gunicorn.conf.py --paste ./config/pyramid/ini/fargate.ini
