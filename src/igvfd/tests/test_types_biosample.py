93323cb3788dc7824bdaa9d2714303c9c920a21e
Idan <gabdank@stanford.edu>
2022-06-08 13:58:59 -0700

    typo

--- src/igvfd/tests/test_types_biosample.py
+++ src/igvfd/tests/test_types_biosample.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_tissue_sex_calculation(testapp, tissue, human_donor, human_male_donor):
     res = testapp.get(tissue['@id'])
-    assert res.json.get('sex') in None
+    assert res.json.get('sex') is None
     res = testapp.patch_json(
         tissue['@id'],
         {'donors': [human_donor['@id']]})
