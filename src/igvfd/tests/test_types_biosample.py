78bc7d50c6bb9675426552eee1edd0fc9eaa7b9f
Idan <gabdank@stanford.edu>
2022-06-08 12:20:21 -0700

    correcting test

--- src/igvfd/tests/test_types_biosample.py
+++ src/igvfd/tests/test_types_biosample.py
@@ -6,7 +6,7 @@ def test_tissue_sex_calculation(testapp, tissue, human_donor):
     assert(not res.json.get('sex'))
     res = testapp.patch_json(
         tissue['@id'],
-        {'donor': human_donor['@id']})
+        {'donors': [human_donor['@id']]})
     res = testapp.get(tissue['@id'])
     assert(not res.json.get('sex'))
     res = testapp.patch_json(
