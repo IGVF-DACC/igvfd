cd19ce758b116b058712137f0d8b25e2b55f08f4
Idan <gabdank@stanford.edu>
2022-06-08 13:38:47 -0700

    fixing assert

--- src/igvfd/tests/test_types_biosample.py
+++ src/igvfd/tests/test_types_biosample.py
@@ -3,19 +3,19 @@ import pytest
 
 def test_tissue_sex_calculation(testapp, tissue, human_donor, human_male_donor):
     res = testapp.get(tissue['@id'])
-    assert(not res.json.get('sex'))
+    assert res.json.get('sex') in None
     res = testapp.patch_json(
         tissue['@id'],
         {'donors': [human_donor['@id']]})
     res = testapp.get(tissue['@id'])
-    assert(not res.json.get('sex'))
+    assert res.json.get('sex') is None
     res = testapp.patch_json(
         human_donor['@id'],
         {'sex': 'female'})
     res = testapp.get(tissue['@id'])
-    assert(res.json.get('sex') == 'female')
+    assert res.json.get('sex') == 'female'
     res = testapp.patch_json(
         tissue['@id'],
         {'donors': [human_donor['@id'], human_male_donor['@id']]})
     res = testapp.get(tissue['@id'])
-    assert(res.json.get('sex') == 'mixed')
+    assert res.json.get('sex') == 'mixed'
