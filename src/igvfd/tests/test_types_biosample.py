ad52db71d087b590126d9dc21b4355f34d12a467
Idan <gabdank@stanford.edu>
2022-06-08 16:52:02 -0700

    better readability

--- src/igvfd/tests/test_types_biosample.py
+++ src/igvfd/tests/test_types_biosample.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_tissue_sex_calculation(testapp, tissue, human_donor, human_male_donor):
     res = testapp.get(tissue['@id'])
-    assert res.json.get('sex') is None
+    assert 'sex' not in res.json
     res = testapp.patch_json(
         tissue['@id'],
         {'donors': [human_donor['@id']]})
