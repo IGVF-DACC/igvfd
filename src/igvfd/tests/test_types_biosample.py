dd93a8390db60f01dcfb12d5b5be3cd64b6d2e84
Idan <gabdank@stanford.edu>
2022-06-08 16:38:14 -0700

    updating test

--- src/igvfd/tests/test_types_biosample.py
+++ src/igvfd/tests/test_types_biosample.py
@@ -8,7 +8,7 @@ def test_tissue_sex_calculation(testapp, tissue, human_donor, human_male_donor):
         tissue['@id'],
         {'donors': [human_donor['@id']]})
     res = testapp.get(tissue['@id'])
-    assert res.json.get('sex') is None
+    assert res.json.get('sex') == 'unspecified'
     res = testapp.patch_json(
         human_donor['@id'],
         {'sex': 'female'})
