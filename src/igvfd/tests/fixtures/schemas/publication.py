655c078258f0a4a7529ceb4ba467c7344d2adcd0
ian-whaling <whaling.ian@gmail.com>
2022-06-23 17:50:36 -0700

    added more tests and inserts

--- src/igvfd/tests/fixtures/schemas/publication.py
+++ src/igvfd/tests/fixtures/schemas/publication.py
@@ -6,6 +6,7 @@ def publication(testapp, lab, award):
     item = {
         'award': award['@id'],
         'lab': lab['@id'],
-        'title': 'Publication'
+        'title': 'Publication',
+        'identifiers': ['PMID:1']
     }
     return testapp.post_json('/publication', item, status=201).json['@graph'][0]
