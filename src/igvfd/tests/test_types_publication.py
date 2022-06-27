ded366415ed5762312a79155a0139a7628c3e9bd
ian-whaling <whaling.ian@gmail.com>
2022-06-27 16:33:59 -0700

    test to make sure publication_year is not calculating

--- src/igvfd/tests/test_types_publication.py
+++ src/igvfd/tests/test_types_publication.py
@@ -2,6 +2,7 @@ import pytest
 
 
 def test_publication_year(testapp, publication):
+    assert 'publication_year' not in publication
     res = testapp.patch_json(
         publication['@id'],
         {'date_published': '2022-09-13'}).json['@graph'][0]
