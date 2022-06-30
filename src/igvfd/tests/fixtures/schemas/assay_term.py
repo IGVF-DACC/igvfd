d123545069f053080e9a6cd073e546b77076b8a1
Jennifer Jou <jjou@stanford.edu>
2022-06-30 11:22:05 -0700

    Fix test

--- src/igvfd/tests/fixtures/schemas/assay_term.py
+++ src/igvfd/tests/fixtures/schemas/assay_term.py
@@ -17,3 +17,12 @@ def assay_term_chip(testapp):
         'term_name': 'ChIP-seq'
     }
     return testapp.post_json('/assay_term', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def assay_term_dnase(testapp):
+    item = {
+        'term_id': 'OBI:0001853',
+        'term_name': 'DNase-seq'
+    }
+    return testapp.post_json('/assay_term', item, status=201).json['@graph'][0]
--- src/igvfd/tests/test_schema_assay_term.py
+++ src/igvfd/tests/test_schema_assay_term.py
@@ -9,7 +9,15 @@ def test_assay_term_term_id_regex(assay_term_starr, testapp):
     assert res.status_code == 422
 
 
-def test_assay_term_slims(assay_term_chip, testapp):
+def test_assay_term_slims(
+    assay_term_chip,
+    assay_term_dnase,
+    testapp
+):
     res = testapp.get(assay_term_chip['@id'] + '@@index-data')
     expected_assay_slims = ['DNA binding']
+    assert(all(slim in res.json['embedded']['assay_slims'] for slim in expected_assay_slims))
+
+    res = testapp.get(assay_term_dnase['@id'] + '@@index-data')
+    expected_category_slims = ['protein and DNA interaction']
     assert(all(slim in res.json['embedded']['category_slims'] for slim in expected_assay_slims))
