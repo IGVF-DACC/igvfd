ddb5b0d57ea5b8c17e973075d6689ff52974e087
jenjou <42983888+jenjou@users.noreply.github.com>
2022-07-19 22:28:17 -0700

    IGVF-199-assay-term-slims (#92)

--- src/igvfd/tests/test_schema_assay_term.py
+++ src/igvfd/tests/test_schema_assay_term.py
@@ -9,7 +9,17 @@ def test_assay_term_term_id_regex(assay_term_starr, testapp):
     assert res.status_code == 422
 
 
-def test_assay_term_slims(assay_term_chip, testapp):
+def test_assay_term_slims(
+    assay_term_chip,
+    assay_term_dnase,
+    testapp
+):
     res = testapp.get(assay_term_chip['@id'] + '@@index-data')
     expected_assay_slims = ['DNA binding']
-    assert(all(slim in res.json['embedded']['category_slims'] for slim in expected_assay_slims))
+    assert(all(slim in res.json['embedded']['assay_slims'] for slim in expected_assay_slims))
+
+    res = testapp.get(assay_term_dnase['@id'] + '@@index-data')
+    expected_category_slims = ['protein and DNA interaction']
+    expected_objective_slims = ['protein and DNA interaction identification objective']
+    assert(all(slim in res.json['embedded']['category_slims'] for slim in expected_category_slims))
+    assert(all(slim in res.json['embedded']['objective_slims'] for slim in expected_objective_slims))
