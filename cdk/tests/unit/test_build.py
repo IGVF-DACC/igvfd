234a85c1fb6e51f6968c3f194bf326ff93f97362
Keenan Graham <ksgraham@stanford.edu>
2022-07-29 12:11:33 -0700

    Config test pass

--- cdk/tests/unit/test_build.py
+++ cdk/tests/unit/test_build.py
@@ -63,7 +63,7 @@ def test_synth_get_config():
     config = get_config(args)
     assert config.branch == 'dev'
     assert config.pipeline == 'ContinuousDeploymentPipelineStack'
-    assert 'snapshot_source_db_identifier' not in config.postgres['instances'][0]
+    assert 'snapshot_source_db_identifier' not in config.postgres['instances'][0]['props']
     assert config.common.project_name == 'igvfd'
 
 
