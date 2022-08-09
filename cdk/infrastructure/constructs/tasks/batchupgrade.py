fee335fbb6189d09a8333a7535638f0c5ada2f5a
Keenan Graham <ksgraham@stanford.edu>
2022-08-09 12:19:50 -0700

    Add construct that runs batchupgrade when upgrade folder changes

---
+++ cdk/infrastructure/constructs/tasks/batchupgrade.py
@@ -0,0 +1,160 @@
+from constructs import Construct
+
+from aws_cdk.aws_logs import RetentionDays
+
+from aws_cdk.custom_resources import AwsCustomResource
+from aws_cdk.custom_resources import AwsCustomResourcePolicy
+from aws_cdk.custom_resources import AwsSdkCall
+from aws_cdk.custom_resources import PhysicalResourceId
+
+from aws_cdk.aws_ec2 import SubnetSelection
+from aws_cdk.aws_ec2 import SubnetType
+
+from aws_cdk.aws_events import Rule
+from aws_cdk.aws_events import EventPattern
+
+from aws_cdk.aws_events_targets import ContainerOverride
+from aws_cdk.aws_events_targets import EcsTask
+
+from aws_cdk.aws_s3_assets import Asset
+
+from infrastructure.config import Config
+
+from infrastructure.constructs.existing.types import ExistingResources
+
+from infrastructure.constructs.backend import Backend
+
+from infrastructure.events.naming import get_event_source_from_config
+
+from infrastructure.events.batchupgrade import BatchUpgradeEvents
+
+from dataclasses import dataclass
+
+from typing import Any
+from typing import List
+
+
+@dataclass
+class BatchUpgradeProps:
+    config: Config
+    existing_resources: ExistingResources
+    backend: Backend
+
+
+class BatchUpgrade(Construct):
+
+    event_source: str
+    event_detail_type: str
+    event_target: EcsTask
+    event_rule: Rule
+    event_trigger: AwsCustomResource
+    upgrade_folder: Asset
+
+    def __init__(
+            self,
+            scope: Construct,
+            construct_id: str,
+            *,
+            props: BatchUpgradeProps,
+            **kwargs: Any
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.define_event_source()
+        self.define_event_detail_type()
+        self.define_event_target()
+        self.define_event_rule()
+        self.define_event_trigger()
+        self.grant_put_events_to_trigger()
+
+    def define_event_source(self) -> None:
+        self.event_source = get_event_source_from_config(
+            self.props.config
+        )
+
+    def define_event_detail_type(self) -> None:
+        self.event_detail_type = BatchUpgradeEvents.UPGRADE_FOLDER_CHANGED
+
+    def _get_container_overrides(self) -> List[ContainerOverride]:
+        return [
+            ContainerOverride(
+                container_name='pyramid',
+                command=['/scripts/pyramid/batchupgrade.sh'],
+            ),
+            ContainerOverride(
+                container_name='nginx',
+                command=['sleep', '3600'],
+            ),
+        ]
+
+    def _get_subnet_selection(self) -> SubnetSelection:
+        return SubnetSelection(
+            subnet_type=SubnetType.PUBLIC
+        )
+
+    def define_event_target(self) -> None:
+        self.event_target = EcsTask(
+            cluster=self.props.backend.fargate_service.cluster,
+            task_definition=self.props.backend.fargate_service.task_definition,
+            container_overrides=self._get_container_overrides(),
+            security_groups=self.props.backend.fargate_service.service.connections.security_groups,
+            subnet_selection=self._get_subnet_selection(),
+        )
+
+    def define_event_rule(self) -> None:
+        self.event_rule = Rule(
+            self,
+            'RunBatchUpgrade',
+            event_pattern=EventPattern(
+                detail_type=[
+                    self.event_detail_type,
+                ],
+                source=[
+                    self.event_source,
+                ],
+            ),
+            targets=[
+                self.event_target,
+            ]
+        )
+
+    def define_upgrade_folder(self) -> None:
+        self.upgrade_folder = Asset(
+            self,
+            'UpgradeFolder',
+            path='../src/igvfd/upgrade',
+        )
+
+    def define_event_trigger(self) -> None:
+        # Put UpgradeFolderChanged event onto EventBridge bus
+        # when the upgrade folder asset hash changes.
+        self.event_trigger = AwsCustomResource(
+            self,
+            'PutUpgradeFolderChangedEvent',
+            on_update=AwsSdkCall(
+                service='EventBridge',
+                action='putEvents',
+                parameters={
+                    'Entries': [
+                        {
+                            'DetailType': self.event_detail_type,
+                            'Source': self.event_source,
+                            'Detail': json.dumps({}),
+                        }
+                    ]
+                },
+                physical_resource_id=PhysicalResourceId.of(
+                    self.upgrade_folder.asset_hash
+                )
+            ),
+            log_retention=RetentionDays.ONE_DAY,
+            policy=AwsCustomResourcePolicy.from_sdk_calls(
+                resources=[
+                    self.props.config.existing_resources.bus.default.event_bus_arn,
+                ]
+            ),
+        )
+
+    def grant_put_events_to_trigger(self) -> None:
+        self.props.config.existing_resources.bus.default.grant_put_events_to(
+            self.event_trigger
+        )
