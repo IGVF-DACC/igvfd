55ef51c2ee4965f849d10ac4b6ef594e8f9f1102
Keenan Graham <ksgraham@stanford.edu>
2022-08-08 21:55:06 -0700

    Change

--- src/igvfd/upgrade/whole_organism.py
+++ src/igvfd/upgrade/whole_organism.py
@@ -3,7 +3,6 @@ from snovault import upgrade_step
 
 @upgrade_step('whole_organism', '1', '2')
 def whole_organism_1_2(value, system):
-    # https://igvf.atlassian.net/browse/IGVF-207
     return
 
 
