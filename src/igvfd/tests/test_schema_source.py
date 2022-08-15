a7484c9871d7678075a187b3cab4eb900247765d
Khine Lin <khzlin@stanford.edu>
2022-08-15 11:03:28 -0700

    fix test

--- src/igvfd/tests/test_schema_source.py
+++ src/igvfd/tests/test_schema_source.py
@@ -3,13 +3,14 @@ def test_sample_1(source, testapp):
     assert res.json['name'] == 'sigma'
 
 
-def test_lab_as_source(lab, award, testapp, human_donor):
+def test_lab_as_source(lab, award, testapp, human_donor, sample_term_K562):
     res = testapp.post_json('/cell_line',
                             {
                                 'award': award['@id'],
                                 'lab': lab['@id'],
                                 'source': lab['@id'],
                                 'taxa': 'Homo sapiens',
-                                'donors': [human_donor['@id']]
+                                'donors': [human_donor['@id']],
+                                'biosample_term': sample_term_K562['@id']
                             })
     assert res.status_code == 201
