6f3f2ce0ee9d43a7931d87baff28503a46c34dea
Jennifer Jou <jjou@stanford.edu>
2022-08-15 15:53:41 -0700

    Add fixture and fix test

--- src/igvfd/tests/fixtures/schemas/technical_sample.py
+++ src/igvfd/tests/fixtures/schemas/technical_sample.py
@@ -2,12 +2,13 @@ import pytest
 
 
 @pytest.fixture
-def technical_sample(testapp, other_lab, award, source):
+def technical_sample(testapp, other_lab, award, source, sample_term_technical_sample):
     item = {
         'award': award['@id'],
         'lab': other_lab['@id'],
         'source': source['@id'],
-        'sample_material': 'synthetic'
+        'sample_material': 'synthetic',
+        'technical_sample_term': sample_term_technical_sample['@id']
     }
     return testapp.post_json('/technical_sample', item, status=201).json['@graph'][0]
 
