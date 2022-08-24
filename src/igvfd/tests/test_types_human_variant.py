151e13925115e253cc79feaa8fbe6bc0b2c090c7
ian-whaling <whaling.ian@gmail.com>
2022-08-23 17:16:03 -0700

    test for unique keys

---
+++ src/igvfd/tests/test_types_human_variant.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+def test_human_variant_keys(testapp):
+    item = {
+        'ref': 'A',
+        'alt': 'G',
+        'chromosome': 'chr1',
+        'locations': [{
+            'assembly': 'GRCh38',
+            'position': 1000000
+        }],
+        'rsid': 'rs100'
+    }
+    response = testapp.post_json('/human_variant', item, status=201)
+    assert response.status_code == 201
+    response = testapp.post_json('/human_variant', item, status=409)
+    assert response.status_code == 409
