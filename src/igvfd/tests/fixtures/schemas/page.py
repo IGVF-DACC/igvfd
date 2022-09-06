a2fe53d7651b6a6a04e02fb9544d5366f20e09bf
forresttanaka <fytanaka@stanford.edu>
2022-09-05 17:13:28 -0700

    IGVF-288-pages
    Remove page schema version 1 properties from fixture test objects, and add them when posting the object.

--- src/igvfd/tests/fixtures/schemas/page.py
+++ src/igvfd/tests/fixtures/schemas/page.py
@@ -6,21 +6,7 @@ def page(testapp):
     item = {
         'name': 'igvf-page',
         'title': 'IGVF Page',
-        'news': True,
-        'news_excerpt': 'Don’t miss the abstract deadline for ENCODE Users Meeting.',
-        'news_keywords': ['Conferences', 'Encyclopedia'],
         'layout': {
-            'rows': [
-                {
-                    'cols': [
-                        {
-                            'blocks': [
-                                '#block1'
-                            ]
-                        }
-                    ]
-                }
-            ],
             'blocks': [
                 {
                     '@id': '#block1',
@@ -28,7 +14,7 @@ def page(testapp):
                     'body': '<p></p>'
                 },
                 {
-                    '@id': '#block4',
+                    '@id': '#block2',
                     '@type': 'teaserblock',
                     'image': '/images/8a91ae78-731a-4a92-ae7c-273214be408a/',
                     'body': '<h2>Chromatin Structure</h2>',
@@ -45,5 +31,19 @@ def page_v2(page):
     item = page.copy()
     item.update({
         'schema_version': '2',
+        'news': True,
+        'news_excerpt': 'Don’t miss the abstract deadline for ENCODE Users Meeting.',
+        'news_keywords': ['Conferences', 'Encyclopedia']
+    })
+    item.update({item['layout']['rows']: [
+        {
+            'cols': [
+                {
+                    'blocks': [
+                        '#block1'
+                    ]
+                }
+            ]
+        }]
     })
     return item
