aacabae1c1b6854cf37452fda6b6c44708a8454d
Keenan Graham <ksgraham@stanford.edu>
2022-09-13 16:48:55 -0700

    Add back some search views

---
+++ src/igvfd/searches/defaults.py
@@ -0,0 +1,49 @@
+OPTIONAL_PARAMS = [
+    'annotation',
+    'cart',
+    'datastore',
+    'debug',
+    'field',
+    'filterresponse',
+    'format',
+    'frame',
+    'from',
+    'genome',
+    'limit',
+    'mode',
+    'referrer',
+    'region',
+    'remove',
+    'sort',
+    'type',
+    'config',
+]
+
+FREE_TEXT_QUERIES = [
+    'advancedQuery',
+    'searchTerm',
+]
+
+RESERVED_KEYS = NOT_FILTERS = OPTIONAL_PARAMS + FREE_TEXT_QUERIES
+
+DEFAULT_ITEM_TYPES = [
+    'Award',
+    'Biosample',
+    'Document',
+    'Donor',
+    'Page',
+    'Publication',
+    'Gene',
+    'Lab',
+]
+
+TOP_HITS_ITEM_TYPES = [
+    'Award',
+    'Biosample',
+    'Document',
+    'HumanDonor',
+    'RodentDonor',
+    'Page',
+    'Publication',
+    'Gene',
+]
