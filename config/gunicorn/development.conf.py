50e4c32ab86f3bc557fe8ba54c950cf32061e28d
Otto Jolanki <ojolanki@stanford.edu>
2022-09-19 14:03:15 -0700

    gunicorn log formats

--- config/gunicorn/development.conf.py
+++ config/gunicorn/development.conf.py
@@ -1,4 +1,5 @@
 bind = '0.0.0.0:6543'
 workers = 4
 threads = 1
-access_log_format = '%(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Stats}o)s"'
+accesslog = '-'
+access_log_format = '{\"host\":"%(h)s", \"user\":"%(u)s", \"time\":"%(t)s", \"statusline\":"%(r)s", \"status\":%(s)s, \"response_length\":%(b)s, \"referer\":"%(f)s", \"user_agent\":"%(a)s", \"x_stats\":"%({X-Stats}o)s"}'
--- scripts/pyramid/run-development.sh
+++ scripts/pyramid/run-development.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export SESSION_SECRET=$(cat /dev/urandom | head -c 256 | base64)
-gunicorn --config ./config/gunicorn/development.conf.py --paste ./config/pyramid/ini/development.ini --access-logfile '-' --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({X-Stats}o)s'
+gunicorn --config ./config/gunicorn/development.conf.py --paste ./config/pyramid/ini/development.ini
