7ea17b4a3605ced0a97614fccf822da7f72c8624
Jennifer Jou <jjou@stanford.edu>
2022-09-20 14:44:33 -0700

    IGVF-298 Add schema for Analysis Set

--- src/igvfd/types/file_set.py
+++ src/igvfd/types/file_set.py
@@ -22,3 +22,37 @@ class FileSet(Item):
     base_types = ['FileSet'] + Item.base_types
     name_key = 'accession'
     schema = load_schema('igvfd:schemas/file_set.json')
+
+
+@collection(
+    name='analysis-sets',
+    unique_key='accession',
+    properties={
+        'title': 'Analysis sets',
+        'description': 'Listing of analysis sets',
+    })
+class AnalysisSet(FileSet):
+    item_type = 'analysis_set'
+    schema = load_schema('igvfd:schemas/analysis_set.json')
+
+    @calculated_property(
+        schema={
+            'title': 'Assay Titles',
+            'description': 'Titles of assays that produced data analyzed in the analysis set.',
+            'type': 'array',
+            'uniqueItems': True,
+            'items': {
+                'title': 'Assay Title',
+                'description': 'Title of assay that produced data analyzed in the analysis set.',
+                'type': 'string'
+            },
+            'notSubmittable': True,
+        }
+    )
+    def assay_titles(self, request, input_file_sets=None):
+        assay_titles = []
+        for fileset in input_file_sets:
+            file_set_object = request.embed(fileset, '@@object')
+            if file_set_object.get('assay_title'):
+                assay_titles.append(file_set_object.get('assay_title'))
+        return list(set(assay_titles))
