0928d0a8d4b7294654a23a00cf95ce4b12c5d9aa
Jennifer Jou <jjou@stanford.edu>
2022-09-19 17:03:01 -0700

    Add a basic file_set abstract class

---
+++ src/igvfd/types/file_set.py
@@ -0,0 +1,24 @@
+from snovault import (
+    abstract_collection,
+    calculated_property,
+    collection,
+    load_schema,
+)
+
+from .base import (
+    Item,
+)
+
+
+@abstract_collection(
+    name='file-sets',
+    unique_key='accession',
+    properties={
+        'title': 'File sets',
+        'description': 'Listing of file sets',
+    })
+class FileSet(Item):
+    item_type = 'file_set'
+    base_types = ['FileSet'] + Item.base_types
+    name_key = 'accession'
+    schema = load_schema('igvfd:schemas/file_set.json')
