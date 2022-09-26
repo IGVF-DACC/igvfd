650a17bf7c6e769af6721b9b3804a028325b4019
Otto Jolanki <ojolanki@stanford.edu>
2022-09-26 13:46:22 -0700

    cdk version upgrade, tests update

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -84,7 +84,7 @@ class Common:
     organization_name: str = 'igvf-dacc'
     project_name: str = 'igvfd'
     default_region: str = 'us-west-2'
-    aws_cdk_version: str = '2.42.1'
+    aws_cdk_version: str = '2.43.1'
 
 
 @dataclass
