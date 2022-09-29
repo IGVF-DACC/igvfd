978518ff3393bb25f69f87850a55eb996f4b8192
ottojolanki <ojolanki@stanford.edu>
2022-09-29 15:04:43 -0700

    IGVF-314-disable-auto-minor-version-upgrade (#163)

--- cdk/infrastructure/constructs/postgres.py
+++ cdk/infrastructure/constructs/postgres.py
@@ -128,6 +128,7 @@ class Postgres(PostgresBase):
             ),
             allocated_storage=self.props.allocated_storage,
             max_allocated_storage=self.props.max_allocated_storage,
+            auto_minor_version_upgrade=False,
         )
 
     def _add_tags_to_database(self) -> None:
@@ -187,6 +188,7 @@ class PostgresFromSnapshotArn(PostgresBase):
             ),
             allocated_storage=self.props.allocated_storage,
             max_allocated_storage=self.props.max_allocated_storage,
+            auto_minor_version_upgrade=False,
         )
 
     def _add_tags_to_database(self) -> None:
