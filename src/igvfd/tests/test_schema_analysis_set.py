34874793d226494e8465ab8acd3073f311425bd6
Jennifer Jou <jjou@stanford.edu>
2022-10-06 16:10:04 -0700

    add the new test files

---
+++ src/igvfd/tests/test_schema_analysis_set.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+def test_analysis_set_sample_donor_dependency(
+    testapp,
+    analysis_set_with_donor,
+    analysis_set_with_sample,
+    human_donor,
+    cell_line
+):
+    res = testapp.patch_json(
+        analysis_set_with_donor['@id'],
+        {'sample': [cell_line['@id']]},
+        expect_errors=True
+    )
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        analysis_set_with_sample['@id'],
+        {'donor': [human_donor['@id']]},
+        expect_errors=True
+    )
+    assert res.status_code == 422
