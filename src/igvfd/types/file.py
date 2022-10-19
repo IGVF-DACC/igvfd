ed31044eff51e5f3d4f983c66446c7672553e78a
Jennifer Jou <jjou@stanford.edu>
2022-10-19 14:45:53 -0700

    Update file.py

--- src/igvfd/types/file.py
+++ src/igvfd/types/file.py
@@ -42,3 +42,43 @@ class SequenceData(File):
                 value = 'md5:{md5sum}'.format(**properties)
                 keys.setdefault('alias', []).append(value)
         return keys
+
+
+@collection(
+    name='reference-data',
+    unique_key='accession',
+    properties={
+        'title': 'Reference data',
+        'description': 'Listing of reference data files',
+    })
+class ReferenceData(File):
+    item_type = 'sequence_data'
+    schema = load_schema('igvfd:schemas/reference_data.json')
+
+    def unique_keys(self, properties):
+        keys = super(File, self).unique_keys(properties)
+        if properties.get('status') != 'replaced':
+            if 'md5sum' in properties:
+                value = 'md5:{md5sum}'.format(**properties)
+                keys.setdefault('alias', []).append(value)
+        return keys
+
+
+@collection(
+    name='imaging-data',
+    unique_key='accession',
+    properties={
+        'title': 'Imaging data',
+        'description': 'Listing of imaging data files',
+    })
+class ImagingData(File):
+    item_type = 'sequence_data'
+    schema = load_schema('igvfd:schemas/imaging_data.json')
+
+    def unique_keys(self, properties):
+        keys = super(File, self).unique_keys(properties)
+        if properties.get('status') != 'replaced':
+            if 'md5sum' in properties:
+                value = 'md5:{md5sum}'.format(**properties)
+                keys.setdefault('alias', []).append(value)
+        return keys
