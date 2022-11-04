0adfb3002992e35eeac107e172742afbe40cff5e
Keenan Graham <ksgraham@stanford.edu>
2022-11-04 13:27:14 -0700

    Update stages dev test

--- cdk/tests/unit/test_stages_dev.py
+++ cdk/tests/unit/test_stages_dev.py
@@ -1,7 +1,6 @@
 import pytest
 
 
-@pytest.mark.skip()
 def test_stages_dev_initialize_dev_stages(config):
     from aws_cdk import App
     from infrastructure.stages.dev import DevelopmentDeployStage
@@ -16,11 +15,21 @@ def test_stages_dev_initialize_dev_stages(config):
         stack.stack_name
         for stack in cloud_assembly.stacks
     ] == [
+        'TestDevelopmentDeployStage-OpensearchStack',
         'TestDevelopmentDeployStage-PostgresStack',
-        'TestDevelopmentDeployStage-BackendStack',
+        'TestDevelopmentDeployStage-BackendStack'
     ]
     stack = cloud_assembly.get_stack_by_name(
         'TestDevelopmentDeployStage-PostgresStack'
+    )
+    assert stack.tags == {
+        'environment': 'demo',
+        'branch': 'some-branch',
+        'project': 'igvfd',
+        'test': 'tag'
+    }
+    stack = cloud_assembly.get_stack_by_name(
+        'TestDevelopmentDeployStage-OpensearchStack'
     )
     assert stack.tags == {
         'environment': 'demo',
