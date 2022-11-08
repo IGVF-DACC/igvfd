8abc8ed0414e8c397879898fb838cde78762431d
Jennifer Jou <jjou@stanford.edu>
2022-11-08 13:42:55 -0800

    Remove unused test with removed properties

--- src/igvfd/tests/fixtures/schemas/sequence_data.py
+++ src/igvfd/tests/fixtures/schemas/sequence_data.py
@@ -12,18 +12,3 @@ def sequence_data(testapp, lab, award, analysis_set_with_sample):
         'content_type': 'reads'
     }
     return testapp.post_json('/sequence_data', item, status=201).json['@graph'][0]
-
-
-@pytest.fixture
-def sequence_data_paired_end_1(testapp, lab, award, analysis_set_with_sample):
-    item = {
-        'award': award['@id'],
-        'lab': lab['@id'],
-        'md5sum': 'd9208a6af38f0886bc59c629ff19e5c0',
-        'file_format': 'fastq',
-        'file_set': analysis_set_with_sample['@id'],
-        'content_type': 'reads',
-        'run_type': 'paired-ended',
-        'paired_end': '1'
-    }
-    return testapp.post_json('/sequence_data', item, status=201).json['@graph'][0]
