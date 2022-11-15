c5cdbb54f5480483f6678438f5133d0ea811e62d
Jennifer Jou <jjou@stanford.edu>
2022-11-15 09:29:14 -0800

    Fix inserts and fixture for fastqs

--- src/igvfd/tests/fixtures/schemas/sequence_data.py
+++ src/igvfd/tests/fixtures/schemas/sequence_data.py
@@ -9,6 +9,9 @@ def sequence_data(testapp, lab, award, analysis_set_with_sample):
         'md5sum': '515c8a6af303ea86bc59c629ff198277',
         'file_format': 'fastq',
         'file_set': analysis_set_with_sample['@id'],
+        'minimum_read_length': 99,
+        'maximum_read_length': 101,
+        'mean_read_length': 100,
         'content_type': 'reads'
     }
     return testapp.post_json('/sequence_data', item, status=201).json['@graph'][0]
