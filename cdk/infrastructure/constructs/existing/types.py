5e37151a2ac65b0134fbc79a98eaadabeb03e1d8
Keenan Graham <ksgraham@stanford.edu>
2022-11-29 18:03:02 -0800

    Update ExistingResource types

--- cdk/infrastructure/constructs/existing/types.py
+++ cdk/infrastructure/constructs/existing/types.py
@@ -1,10 +1,23 @@
 from infrastructure.constructs.existing.igvf_dev import Resources as IGVFDevResources
+from infrastructure.constructs.existing.igvf_staging import Resources as IGVFStagingResources
+from infrastructure.constructs.existing.igvf_sandbox import Resources as IGVFSandboxResources
+from infrastructure.constructs.existing.igvf_prod import Resources as IGVFProdResources
 
 from typing import Union
 
 from typing import Type
 
 
-ExistingResources = Union[IGVFDevResources]
+ExistingResources = Union[
+    IGVFDevResources,
+    IGVFStagingResources,
+    IGVFSandboxResources,
+    IGVFProdResources,
+]
 
-ExistingResourcesClass = Union[Type[IGVFDevResources]]
+ExistingResourcesClass = Union[
+    Type[IGVFDevResources],
+    Type[IGVFStagingResources],
+    Type[IGVFSandboxResources],
+    Type[IGVFProdResources],
+]
