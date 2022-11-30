4f55ffe45aba20d7e604161e3bab2b57142ea48e
Khine Lin <khzlin@stanford.edu>
2022-11-30 12:33:01 -0800

    add new fixtures

---
+++ src/igvfd/tests/fixtures/schemas/in_vitro_system.py
@@ -0,0 +1,43 @@
+import pytest
+
+
+@pytest.fixture
+def cell_line(testapp, other_lab, award, rodent_donor, sample_term_K562):
+    item = {
+        'classification': 'cell line',
+        'award': award['@id'],
+        'lab': other_lab['@id'],
+        'source': other_lab['@id'],
+        'taxa': 'Mus musculus',
+        'donors': [rodent_donor['@id']],
+        'biosample_term': sample_term_K562['@id']
+    }
+    return testapp.post_json('/in_vitro_system', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def differentiated_cell(testapp, lab, award, source, human_donor, sample_term_K562):
+    item = {
+        'classification': 'differentiated cell',
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'source': source['@id'],
+        'taxa': 'Homo sapiens',
+        'donors': [human_donor['@id']],
+        'biosample_term': sample_term_K562['@id']
+    }
+    return testapp.post_json('/in_vitro_system', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def differentiated_tissue(testapp, lab, award, source, human_donor, sample_term_adrenal_gland):
+    item = {
+        'classification': 'differentiated tissue',
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'source': source['@id'],
+        'taxa': 'Homo sapiens',
+        'donors': [human_donor['@id']],
+        'biosample_term': sample_term_adrenal_gland['@id']
+    }
+    return testapp.post_json('/in_vitro_system', item, status=201).json['@graph'][0]
