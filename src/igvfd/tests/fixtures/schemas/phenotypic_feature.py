4a24657391111e115a8c5ff3594946edffe5d0c2
Jennifer Jou <jjou@stanford.edu>
2022-12-12 16:45:35 -0800

    Make award and lab required

--- src/igvfd/tests/fixtures/schemas/phenotypic_feature.py
+++ src/igvfd/tests/fixtures/schemas/phenotypic_feature.py
@@ -2,8 +2,15 @@ import pytest
 
 
 @pytest.fixture
-def phenotypic_feature_basic(testapp, phenotype_term_ncit_feature):
+def phenotypic_feature_basic(
+    testapp,
+    phenotype_term_ncit_feature,
+    award,
+    lab
+):
     item = {
+        'award': award['@id'],
+        'lab': lab['@id'],
         'feature': phenotype_term_ncit_feature['@id']
     }
     return testapp.post_json('/phenotypic_feature', item, status=201).json['@graph'][0]
