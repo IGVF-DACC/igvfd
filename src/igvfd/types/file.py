8985bfcb728c626a4e2be51ca1a73fff812bdbff
Keenan Graham <ksgraham@stanford.edu>
2023-01-13 19:12:57 -0800

    Update

--- src/igvfd/types/file.py
+++ src/igvfd/types/file.py
@@ -16,7 +16,8 @@ from .base import (
     properties={
         'title': 'Files',
         'description': 'Listing of files',
-    })
+    }
+)
 class File(Item):
     item_type = 'file'
     base_types = ['File'] + Item.base_types
@@ -30,7 +31,8 @@ class File(Item):
     properties={
         'title': 'Sequence data',
         'description': 'Listing of sequence data files',
-    })
+    }
+)
 class SequenceData(File):
     item_type = 'sequence_data'
     schema = load_schema('igvfd:schemas/sequence_data.json')
@@ -56,7 +58,8 @@ class SequenceData(File):
     properties={
         'title': 'Reference data',
         'description': 'Listing of reference data files',
-    })
+    }
+)
 class ReferenceData(File):
     item_type = 'reference_data'
     schema = load_schema('igvfd:schemas/reference_data.json')
