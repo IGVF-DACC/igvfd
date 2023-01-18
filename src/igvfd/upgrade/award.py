a94a85ffa6aa9962b3e3e75a56ba8c3c636ab0f0
ian-whaling <whaling.ian@gmail.com>
2023-01-18 11:00:16 -0800

    upgrades

--- src/igvfd/upgrade/award.py
+++ src/igvfd/upgrade/award.py
@@ -7,3 +7,11 @@ def award_1_2(value, system):
     if 'aliases' in value:
         if len(value['aliases']) == 0:
             del value['aliases']
+
+
+@upgrade_step('award', '2', '3')
+def award_2_3(value, system):
+    # https://igvf.atlassian.net/browse/IGVF-321
+    if 'pi' in value:
+        value['pis'] = value['pi']
+        del value['pi']
---
+++ src/igvfd/upgrade/file_set.py
@@ -0,0 +1,17 @@
+from snovault import upgrade_step
+
+
+@upgrade_step('analysis_set', '1', '2')
+@upgrade_step('curated_set', '1', '2')
+@upgrade_step('measurement_set', '1', '2')
+def file_set_1_2(value, system):
+    # https://igvf.atlassian.net/browse/IGVF-321
+    if 'sample' in value:
+        value['samples'] = value['sample']
+        del value['sample']
+    if 'donor' in value:
+        value['donors'] = value['donor']
+        del value['donor']
+    if 'input_file_set' in value:
+        value['input_file_sets'] = value['input_file_set']
+        del value['input_file_set']
