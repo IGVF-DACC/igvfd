504b1fea0cd263369781f33e4f6055b833272409
Keenan Graham <ksgraham@stanford.edu>
2023-01-24 14:34:45 -0800

    Avoid specifying enable_performance_insights on snapshot restores

--- cdk/infrastructure/constructs/postgres.py
+++ cdk/infrastructure/constructs/postgres.py
@@ -190,7 +190,6 @@ class PostgresFromSnapshotArn(PostgresBase):
             allocated_storage=self.props.allocated_storage,
             max_allocated_storage=self.props.max_allocated_storage,
             auto_minor_version_upgrade=False,
-            enable_performance_insights=True,
         )
 
     def _add_tags_to_database(self) -> None:
