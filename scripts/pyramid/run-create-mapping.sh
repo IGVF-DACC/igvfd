4fe9e6c8a21537a04fa343b7a2c78f17fb2ea5e8
Keenan Graham <ksgraham@stanford.edu>
2023-02-23 11:56:42 -0800

    Update local reindexing behavior

--- scripts/pyramid/run-create-mapping.sh
+++ scripts/pyramid/run-create-mapping.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 10
-manage-mappings config/pyramid/ini/development.ini --app-name app
+manage-mappings config/pyramid/ini/development.ini --app-name app --should-reindex after-initial
