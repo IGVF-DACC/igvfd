5e28336f936e40a04b0fef1a69acc15d647b4978
Keenan Graham <ksgraham@stanford.edu>
2023-02-24 11:31:50 -0800

    Poll

--- src/igvfd/tests/indexing/conftest.py
+++ src/igvfd/tests/indexing/conftest.py
@@ -10,17 +10,16 @@ def external_tx():
     pass
 
 
-def wait_for_indexing():
-    time.sleep(45)
-
-
-def wait_for_indexing_poll(app):
+def make_test_app(app):
     from webtest import TestApp
     environ = {
         'HTTP_ACCEPT': 'application/json',
         'REMOTE_USER': 'TEST',
     }
-    testapp = TestApp(app, environ)
+    return TestApp(app, environ)
+
+
+def wait_for_indexing_poll(testapp):
     double_check_number = 3
     while True:
         print('Waiting for indexing', double_check_number)
@@ -32,6 +31,11 @@ def wait_for_indexing_poll(app):
         if double_check_number <= 0:
             break
         time.sleep(10)
+
+
+@pytest.fixture
+def poll_until_indexing_is_done():
+    return wait_for_indexing_poll
 
 
 @pytest.fixture(scope='session')
@@ -49,13 +53,18 @@ def app(app_settings):
     from igvfd import main
     app = main({}, **app_settings)
     yield app
+    from snovault import DBSESSION
+    DBSession = app.registry[DBSESSION]
+    # Dispose connections so postgres can tear down.
+    DBSession.bind.pool.dispose()
 
 
 @pytest.yield_fixture(scope='session')
 def workbook(app, app_settings):
-    from igvfd.loadxl import load_test_data
     from snovault.elasticsearch.manage_mappings import manage_mappings
+    from igvfd.loadxl import load_test_data
+    testapp = make_test_app(app)
     manage_mappings(app)
     load_test_data(app)
-    wait_for_indexing_poll(app)
+    wait_for_indexing_poll(testapp)
     yield
