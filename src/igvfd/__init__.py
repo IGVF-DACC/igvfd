b3984d710213827c7c3c8a8831f975335d827369
Mingjie Li <mingjiecn@gmail.com>
2023-02-28 10:27:35 -0600

    add batch_download.py

--- src/igvfd/__init__.py
+++ src/igvfd/__init__.py
@@ -236,6 +236,7 @@ def main(global_config, **local_config):
     config.include('.root')
 
     config.include('.ontology')
+    config.include('.batch_download')
 
     if 'elasticsearch.server' in config.registry.settings:
         config.include('snovault.elasticsearch')
