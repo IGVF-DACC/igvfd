4e017777c6e5b70ceb8874307569d7b1c47c0fab
ian-whaling <whaling.ian@gmail.com>
2023-03-01 10:14:21 -0800

    added reverse link property of file sets on sample

---
+++ src/igvfd/tests/test_types_sample.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+def test_summary(testapp, tissue, measurement_set, analysis_set_base, curated_set_genome):
+    testapp.patch_json(
+        measurement_set['@id'],
+        {
+            'samples': [tissue['@id']]
+        }
+    )
+    res = testapp.get(tissue['@id'])
+    assert res.json.get('file_sets') == [measurement_set['@id']]
+    testapp.patch_json(
+        analysis_set_base['@id'],
+        {
+            'samples': [tissue['@id']]
+        }
+    )
+    testapp.patch_json(
+        curated_set_genome['@id'],
+        {
+            'samples': [tissue['@id']]
+        }
+    )
+    res = testapp.get(tissue['@id'])
+    print(res.json.get('file_sets'))
+    assert res.json.get('file_sets') == [measurement_set['@id'], analysis_set_base['@id'], curated_set_genome['@id']]
