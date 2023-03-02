62a5dee348e39efcffeecfe7a3f21c4740d4233c
Keenan Graham <ksgraham@stanford.edu>
2023-03-01 16:33:32 -0800

    Try notification

--- scripts/pyramid/delete-and-create-mapping-and-reindex.sh
+++ scripts/pyramid/delete-and-create-mapping-and-reindex.sh
@@ -2,4 +2,4 @@
 export SQLALCHEMY_URL=postgresql://postgres:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}
 export OPENSEARCH_URL=$OPENSEARCH_FOR_WRITING_URL
 echo "MANAGE MAPPINGS"
-manage-mappings config/pyramid/ini/${INI_NAME} --app-name app --should-reindex after-initial --relative-mapping-directory src/igvfd/mappings
+manage-mappings-with-notification config/pyramid/ini/${INI_NAME} --app-name app --should-reindex after-initial --relative-mapping-directory src/igvfd/mappings
