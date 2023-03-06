47861e18233a235f44a6a46dd067262c03596c8d
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2023-03-06 10:46:59 -0600

    IGVF-480-publication-column (#244)

--- src/igvfd/searches/configs/publication.py
+++ src/igvfd/searches/configs/publication.py
@@ -5,4 +5,37 @@ from snovault.elasticsearch.searches.configs import search_config
     name='Publication'
 )
 def publication():
-    return {}
+    return {
+        'columns': {
+            'title': {
+                'title': 'Title'
+            },
+            'lab': {
+                'title': 'Lab'
+            },
+            'authors': {
+                'title': 'Authors'
+            },
+            'date_published': {
+                'title': 'Publication Date'
+            },
+            'issue': {
+                'title': 'Issue'
+            },
+            'page': {
+                'title': 'Page'
+            },
+            'volume': {
+                'title': 'Volume'
+            },
+            'journal': {
+                'title': 'Journal'
+            },
+            'status': {
+                'title': 'Status'
+            },
+            'identifiers': {
+                'title': 'Identifiers'
+            },
+        }
+    }
