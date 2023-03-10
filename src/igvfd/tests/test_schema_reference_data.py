b0765d3e263068a04b8f45b1532baf8573391161
corismall <corinn.small@gmail.com>
2023-03-10 09:55:15 -0800

    added dependency test combinations and changed mouse ref genome

--- src/igvfd/tests/test_schema_reference_data.py
+++ src/igvfd/tests/test_schema_reference_data.py
@@ -5,24 +5,28 @@ def test_transcriptome_annotation_dependency(testapp, reference_data):
     res = testapp.patch_json(
         reference_data['@id'],
         {
+            'assembly': 'GRCh38',
             'transcriptome_annotation': 'M30'
         }, expect_errors=True)
     assert res.status_code == 422
     res = testapp.patch_json(
         reference_data['@id'],
         {
+            'assembly': 'GRCh38',
             'transcriptome_annotation': 'M31'
         }, expect_errors=True)
     assert res.status_code == 422
     res = testapp.patch_json(
         reference_data['@id'],
         {
+            'assembly': 'GRCh38',
             'transcriptome_annotation': 'M32'
         }, expect_errors=True)
     assert res.status_code == 422
     res = testapp.patch_json(
         reference_data['@id'],
         {
+            'assembly': 'GRCh38',
             'transcriptome_annotation': 'V40'
         }
     )
@@ -30,6 +34,7 @@ def test_transcriptome_annotation_dependency(testapp, reference_data):
     res = testapp.patch_json(
         reference_data['@id'],
         {
+            'assembly': 'GRCh38',
             'transcriptome_annotation': 'V41'
 
         }
@@ -38,6 +43,7 @@ def test_transcriptome_annotation_dependency(testapp, reference_data):
     res = testapp.patch_json(
         reference_data['@id'],
         {
+            'assembly': 'GRCh38',
             'transcriptome_annotation': 'V42'
 
         }
@@ -46,8 +52,68 @@ def test_transcriptome_annotation_dependency(testapp, reference_data):
     res = testapp.patch_json(
         reference_data['@id'],
         {
+            'assembly': 'GRCh38',
             'transcriptome_annotation': 'V43'
 
         }
     )
     assert res.status_code == 200
+    res = testapp.patch_json(
+        reference_data['@id'],
+        {
+            'assembly': 'GRCm39',
+            'transcriptome_annotation': 'M30'
+        }
+    )
+    assert res.status_code == 200
+    res = testapp.patch_json(
+        reference_data['@id'],
+        {
+            'assembly': 'GRCm39',
+            'transcriptome_annotation': 'M31'
+        }
+    )
+    assert res.status_code == 200
+    res = testapp.patch_json(
+        reference_data['@id'],
+        {
+            'assembly': 'GRCm39',
+            'transcriptome_annotation': 'M32'
+        }
+    )
+    assert res.status_code == 200
+    res = testapp.patch_json(
+        reference_data['@id'],
+        {
+            'assembly': 'GRCm39',
+            'transcriptome_annotation': 'V40'
+        }, expect_errors=True
+    )
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        reference_data['@id'],
+        {
+            'assembly': 'GRCm39',
+            'transcriptome_annotation': 'V41'
+
+        }, expect_errors=True
+    )
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        reference_data['@id'],
+        {
+            'assembly': 'GRCm39',
+            'transcriptome_annotation': 'V42'
+
+        }, expect_errors=True
+    )
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        reference_data['@id'],
+        {
+            'assembly': 'GRCm39',
+            'transcriptome_annotation': 'V43'
+
+        }, expect_errors=True
+    )
+    assert res.status_code == 422
