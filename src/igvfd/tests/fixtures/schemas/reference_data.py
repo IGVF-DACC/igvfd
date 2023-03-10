e7c5adc0382be4bc22d80524e642f636b9a04f2c
corismall <corinn.small@gmail.com>
2023-03-09 17:08:24 -0800

    add fixture

---
+++ src/igvfd/tests/fixtures/schemas/reference_data.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.fixture
+def reference_data_v1(testapp, award, lab, attachment):
+    item = {
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'md5sum': attachment['md5sum'],
+        'file_format': 'gtf',
+        'file_set': 'igvf:analysis_set_1',
+        'content_type': 'transcriptome reference',
+        'assembly': 'GRCh38'
+    }
+    return item
