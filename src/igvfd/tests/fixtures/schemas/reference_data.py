d4f3cd45d14baa0f54e1b661c7d17674dea9fb30
corismall <corinn.small@gmail.com>
2023-03-10 09:37:23 -0800

    post fixture

--- src/igvfd/tests/fixtures/schemas/reference_data.py
+++ src/igvfd/tests/fixtures/schemas/reference_data.py
@@ -2,14 +2,13 @@ import pytest
 
 
 @pytest.fixture
-def reference_data_1(testapp, lab, award, attachment):
+def reference_data(testapp, lab, award, analysis_set_with_sample):
     item = {
         'award': award['@id'],
         'lab': lab['@id'],
-        'md5sum': attachment['md5sum'],
+        'md5sum': '7f1987dea86105dd9d2582c0a91c3856',
         'file_format': 'gtf',
-        'file_set': 'igvf:analysis_set_1',
-        'content_type': 'transcriptome reference',
-        'assembly': 'GRCh38'
+        'file_set': analysis_set_with_sample['@id'],
+        'content_type': 'transcriptome reference'
     }
     return testapp.post_json('/reference_data', item, status=201).json['@graph'][0]
