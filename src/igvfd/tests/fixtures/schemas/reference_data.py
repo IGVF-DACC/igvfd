3e6fd5c16dd5d8e938b6b928e77e7d563353b83c
corismall <corinn.small@gmail.com>
2023-03-09 18:02:56 -0800

    removed first fixture attempt

--- src/igvfd/tests/fixtures/schemas/reference_data.py
+++ src/igvfd/tests/fixtures/schemas/reference_data.py
@@ -2,20 +2,6 @@ import pytest
 
 
 @pytest.fixture
-def reference_data(testapp, award, lab, attachment):
-    item = {
-        'award': award['@id'],
-        'lab': lab['@id'],
-        'md5sum': attachment['md5sum'],
-        'file_format': 'gtf',
-        'file_set': 'igvf:analysis_set_1',
-        'content_type': 'transcriptome reference',
-        'assembly': 'GRCh38'
-    }
-    return item
-
-
-@pytest.fixture
 def reference_data_1(testapp):
     item = {
         'file_format': 'gtf',
