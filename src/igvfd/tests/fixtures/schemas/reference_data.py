9f94db520c38fe8592114bec83c68b1b3d42db07
corismall <corinn.small@gmail.com>
2023-03-09 17:29:34 -0800

    added reference_data_1 fixture

--- src/igvfd/tests/fixtures/schemas/reference_data.py
+++ src/igvfd/tests/fixtures/schemas/reference_data.py
@@ -13,3 +13,14 @@ def reference_data(testapp, award, lab, attachment):
         'assembly': 'GRCh38'
     }
     return item
+
+
+@pytest.fixture
+def reference_data_1(testapp):
+    item = {
+        'file_format': 'gtf',
+        'file_set': 'igvf:analysis_set_1',
+        'content_type': 'transcriptome reference',
+        'assembly': 'GRCh38'
+    }
+    return testapp.post_json('/reference_data', item, status=201).json['@graph'][0]
