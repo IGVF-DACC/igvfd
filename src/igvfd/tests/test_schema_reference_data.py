e5be3368f7173a5a0146f5d9a80299c9347b714f
corismall <corinn.small@gmail.com>
2023-03-09 17:39:00 -0800

    add dependency test

---
+++ src/igvfd/tests/test_schema_reference_data.py
@@ -0,0 +1,53 @@
+import pytest
+
+
+def test_transcriptome_annotation_dependency(testapp, reference_data_1):
+    res = testapp.patch_json(
+        reference_data_1['@id'],
+        {
+            'transcriptome_annotation': 'M30'
+        }, expect_errors=True)
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        reference_data_1['@id'],
+        {
+            'transcriptome_annotation': 'M31'
+        }, expect_errors=True)
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        reference_data_1['@id'],
+        {
+            'transcriptome_annotation': 'M32'
+        }, expect_errors=True)
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        reference_data_1['@id'],
+        {
+            'transcriptome_annotation': 'V40'
+        }
+    )
+    assert res.status_code == 200
+    res = testapp.patch_json(
+        reference_data_1['@id'],
+        {
+            'transcriptome_annotation': 'V41'
+
+        }
+    )
+    assert res.status_code == 200
+    res = testapp.patch_json(
+        reference_data_1['@id'],
+        {
+            'transcriptome_annotation': 'V42'
+
+        }
+    )
+    assert res.status_code == 200
+    res = testapp.patch_json(
+        reference_data_1['@id'],
+        {
+            'transcriptome_annotation': 'V43'
+
+        }
+    )
+    assert res.status_code == 200
