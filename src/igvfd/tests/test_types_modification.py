1b346c464db3f864ab4b4083d32e4e44ba49ed90
Jennifer Jou <jjou@stanford.edu>
2023-03-23 15:19:53 -0700

    Add summary calc prop and search config

---
+++ src/igvfd/tests/test_types_modification.py
@@ -0,0 +1,28 @@
+import pytest
+
+
+def test_modification_summary(testapp, modification, gene_myc_hs):
+    res = testapp.get(modification['@id'])
+    assert res.json.get('summary') == 'CRISPRi dCas9'
+
+    testapp.patch_json(
+        modification['@id'],
+        {
+            'modality': 'activation',
+            'fused_domain': 'VP64'
+        }
+    )
+    res = testapp.get(modification['@id'])
+    assert res.json.get('summary') == 'CRISPRa dCas9-VP64'
+
+    testapp.patch_json(
+        modification['@id'],
+        {
+            'cas': 'Cas13',
+            'modality': 'localizing',
+            'fused_domain': 'ANTI-FLAG',
+            'tagged_protein': gene_myc_hs['@id']
+        }
+    )
+    res = testapp.get(modification['@id'])
+    assert res.json.get('summary') == 'CRISPR localizing Cas13-ANTI-FLAG fused to MYC'
--- src/igvfd/types/modification.py
+++ src/igvfd/types/modification.py
@@ -18,3 +18,34 @@ from .base import (
 class Modification(Item):
     item_type = 'modification'
     schema = load_schema('igvfd:schemas/modification.json')
+
+    @calculated_property(
+        schema={
+            'title': 'Summary',
+            'type': 'string',
+            'notSubmittable': True,
+        }
+    )
+    def summary(self, request, cas, modality, fused_domain=None, tagged_protein=None):
+        crispr_label_mapping = {
+            'activation': 'CRISPRa',
+            'base editing': 'CRISPR base editing',
+            'cutting': 'CRISPR cutting',
+            'interference': 'CRISPRi',
+            'knockout': 'CRISPRko',
+            'localizing': 'CRISPR localizing',
+            'prime editing': 'CRISPR prime editing'
+        }
+
+        formatted_domain = ''
+        if fused_domain:
+            formatted_domain = f'-{fused_domain}'
+
+        summary = f'{crispr_label_mapping[modality]} {cas}{formatted_domain}'
+        if tagged_protein:
+            tagged_protein_object = request.embed(tagged_protein, '@@object?skip_calculated=true')
+            tagged_protein_symbol = tagged_protein_object.get('symbol')
+
+            summary = f'{summary} fused to {tagged_protein_symbol}'
+
+        return summary
