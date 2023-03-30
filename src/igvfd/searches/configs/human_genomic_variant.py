6ad7c84a51ce54906c56af06b9bb7c1707e8846f
Eric Douglass <dougli1sqrd+git@gmail.com>
2023-03-29 17:41:37 -0700

    IGVF-527-human-genomic-variant-search  (#282)

--- src/igvfd/searches/configs/human_genomic_variant.py
+++ src/igvfd/searches/configs/human_genomic_variant.py
@@ -5,4 +5,40 @@ from snovault.elasticsearch.searches.configs import search_config
     name='HumanGenomicVariant'
 )
 def human_genomic_variant():
-    return {}
+    return {
+        'columns': {
+            'uuid': {
+                'title': 'UUID'
+            },
+            'status': {
+                'title': 'Status'
+            },
+            'ref': {
+                'title': 'Reference Allele'
+            },
+            'alt': {
+                'title': 'Alternative Allele'
+            },
+            'assembly': {
+                'title': 'Genome Assembly'
+            },
+            'position': {
+                'title': 'Position'
+            },
+            'refseq_id': {
+                'title': 'RefSeq Sequence Identifier'
+            },
+            'reference_sequence': {
+                'title': 'Reference Sequence'
+            },
+            'chromosome': {
+                'title': 'Chromosome'
+            },
+            'rsid': {
+                'title': 'RS Identifier'
+            },
+            'aliases': {
+                'title': 'Aliases'
+            }
+        }
+    }
