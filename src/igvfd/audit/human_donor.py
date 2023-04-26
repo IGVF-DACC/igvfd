146ec8212bf91294231247cee12f009e795ce83c
ian-whaling <whaling.ian@gmail.com>
2023-04-26 15:16:39 -0700

    stop audits from duplicated

--- src/igvfd/audit/human_donor.py
+++ src/igvfd/audit/human_donor.py
@@ -13,23 +13,19 @@ def audit_related_donors(value, system):
     '''HumanDonors should have unique related donors and should be mutually specified
     between each HumanDonor in their related donors.'''
     if 'related_donors' in value:
-        for i in range(len(value['related_donors'])):
-            for j in range(i + 1, len(value['related_donors'])):
-                if value['related_donors'][i]['donor'] == value['related_donors'][j]['donor']:
-                    duplicated_donor = value['related_donors'][i]['donor']
-                    detail = (
-                        f'HumanDonor {audit_link(path_to_text(value["@id"]), value["@id"])} '
-                        f'has a duplicated related donor {audit_link(path_to_text(duplicated_donor), duplicated_donor)}.'
-                    )
-                    yield AuditFailure('inconsistent related donors metadata', detail, level='WARNING')
-            related_donor_object = system.get('request').embed(
-                value['related_donors'][i]['donor'], '@@object?skip_calculated=true')
-            if 'related_donors' not in related_donor_object or value['@id'] not in [related_donor['donor'] for related_donor in related_donor_object['related_donors']]:
-                related_donor_not_mutual = value['related_donors'][i]['donor']
+        for unique_related_donor in set([related_donor['donor'] for related_donor in value['related_donors']]):
+            if [related_donor['donor'] for related_donor in value['related_donors']].count(unique_related_donor) > 1:
                 detail = (
                     f'HumanDonor {audit_link(path_to_text(value["@id"]), value["@id"])} '
-                    f'has {audit_link(path_to_text(related_donor_not_mutual), related_donor_not_mutual)} '
-                    f'as a related donor, but {audit_link(path_to_text(related_donor_not_mutual), related_donor_not_mutual)} '
+                    f'has a duplicated related donor {audit_link(path_to_text(unique_related_donor), unique_related_donor)}.'
+                )
+                yield AuditFailure('inconsistent related donors metadata', detail, level='WARNING')
+            related_donor_object = system.get('request').embed(unique_related_donor, '@@object?skip_calculated=true')
+            if 'related_donors' not in related_donor_object or value['@id'] not in [related_donor['donor'] for related_donor in related_donor_object['related_donors']]:
+                detail = (
+                    f'HumanDonor {audit_link(path_to_text(value["@id"]), value["@id"])} '
+                    f'has {audit_link(path_to_text(unique_related_donor), unique_related_donor)} '
+                    f'as a related donor, but {audit_link(path_to_text(unique_related_donor), unique_related_donor)} '
                     f'does not mutually specify {audit_link(path_to_text(value["@id"]), value["@id"])} as a related donor.'
                 )
                 yield AuditFailure('inconsistent related donors metadata', detail, level='ERROR')
