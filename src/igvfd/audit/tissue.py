5eda5cd9190d63805b2ba3c592a7d456d67115c9
KAndreeva <andreva@stanford.edu>
2023-04-27 21:21:00 -0400

    IGVF-589 Added audit for tissue common coordinate framework identifier (CCF ID).

---
+++ src/igvfd/audit/tissue.py
@@ -0,0 +1,20 @@
+from snovault.auditor import (
+    audit_checker,
+    AuditFailure,
+)
+from .formatter import (
+    audit_link,
+    path_to_text,
+)
+
+
+@audit_checker('Tissue', frame='object')
+def audit_tissue_ccf_id(value, system):
+    '''Tissue objects must specify an common coordinate framework identifier (CCF ID) required for human data.'''
+    if ('ccf_id' not in value) and (value.get('taxa') == 'Homo sapiens'):
+        value_id = system.get('path')
+        detail = (
+            f'Tissue {audit_link(path_to_text(value_id), value_id)} '
+            f'is missing common coordinate framework identifier (CCF ID) required for human data.'
+        )
+        yield AuditFailure('missing ccf_id', detail, level='NOT_COMPLIANT')
