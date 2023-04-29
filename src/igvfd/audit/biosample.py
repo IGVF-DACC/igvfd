fca43d900d7d7fc03ea930bc0f0ede5a44d048d8
KAndreeva <andreva@stanford.edu>
2023-04-29 13:07:59 -0400

    IGVF-572 Updated text and variable name for biosample audit for NIH institutional certification.

--- src/igvfd/audit/biosample.py
+++ src/igvfd/audit/biosample.py
@@ -12,9 +12,9 @@ from .formatter import (
 def audit_biosample_nih_institutional_certification(value, system):
     '''Biosample objects must specify an NIH Institutional Certification required for human data.'''
     if ('nih_institutional_certification' not in value) and (value.get('taxa') == 'Homo sapiens'):
-        value_id = system.get('path')
+        sample_id = value.get('@id')
         detail = (
-            f'Biosample {audit_link(path_to_text(value_id), value_id)} '
-            f'is missing NIH institutional certification required for human data.'
+            f'Biosample {audit_link(path_to_text(sample_id), sample_id)} '
+            f'is missing NIH institutional certificate that is required for human samples.'
         )
         yield AuditFailure('missing nih_institutional_certification', detail, level='ERROR')
