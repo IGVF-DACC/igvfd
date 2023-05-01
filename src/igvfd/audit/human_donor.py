66361a03d49d595c4abd102c609505c6488f5f0f
Ian Whaling <78115078+ian-whaling@users.noreply.github.com>
2023-05-01 11:11:17 -0700

    IGVF-514-audit-related-donors (#310)

---
+++ src/igvfd/audit/human_donor.py
@@ -0,0 +1,31 @@
+from snovault.auditor import (
+    audit_checker,
+    AuditFailure,
+)
+from .formatter import (
+    audit_link,
+    path_to_text,
+)
+
+
+@audit_checker('HumanDonor', frame='object')
+def audit_related_donors(value, system):
+    '''HumanDonors should have unique related donors and should be mutually specified
+    between each HumanDonor in their related donors.'''
+    if 'related_donors' in value:
+        for unique_related_donor in set([related_donor['donor'] for related_donor in value['related_donors']]):
+            if [related_donor['donor'] for related_donor in value['related_donors']].count(unique_related_donor) > 1:
+                detail = (
+                    f'HumanDonor {audit_link(path_to_text(value["@id"]), value["@id"])} '
+                    f'has a duplicated related donor {audit_link(path_to_text(unique_related_donor), unique_related_donor)}.'
+                )
+                yield AuditFailure('inconsistent related donors metadata', detail, level='WARNING')
+            related_donor_object = system.get('request').embed(unique_related_donor, '@@object?skip_calculated=true')
+            if 'related_donors' not in related_donor_object or value['@id'] not in [related_donor['donor'] for related_donor in related_donor_object['related_donors']]:
+                detail = (
+                    f'HumanDonor {audit_link(path_to_text(value["@id"]), value["@id"])} '
+                    f'has {audit_link(path_to_text(unique_related_donor), unique_related_donor)} '
+                    f'as a related donor, but {audit_link(path_to_text(unique_related_donor), unique_related_donor)} '
+                    f'does not mutually specify {audit_link(path_to_text(value["@id"]), value["@id"])} as a related donor.'
+                )
+                yield AuditFailure('inconsistent related donors metadata', detail, level='ERROR')
---
+++ src/igvfd/tests/test_audit_human_donor.py
@@ -0,0 +1,99 @@
+import pytest
+
+
+def test_audit_related_donors(
+    testapp,
+    human_donor,
+    parent_human_donor_1,
+    parent_human_donor_2
+):
+    # Related donors should not have duplicated donor objects and should mutually specify one another
+    testapp.patch_json(
+        human_donor['@id'],
+        {
+            'related_donors': [{'donor': parent_human_donor_1['@id'], 'relationship_type': 'parent'},
+                               {'donor': parent_human_donor_2['@id'], 'relationship_type': 'parent'},
+                               {'donor': parent_human_donor_1['@id'], 'relationship_type': 'second cousin'}]
+        }
+    )
+    res = testapp.get(human_donor['@id'] + '@@index-data')
+    assert any(
+        error['category'] == 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('WARNING', [])
+    )
+    assert any(
+        error['category'] == 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        human_donor['@id'],
+        {
+            'related_donors': [{'donor': parent_human_donor_1['@id'], 'relationship_type': 'parent'}]
+        }
+    )
+    testapp.patch_json(
+        parent_human_donor_1['@id'],
+        {
+            'related_donors': [{'donor': parent_human_donor_2['@id'], 'relationship_type': 'child'}]
+        }
+    )
+    res = testapp.get(human_donor['@id'] + '@@index-data')
+    assert all(
+        error['category'] != 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('WARNING', [])
+    )
+    assert any(
+        error['category'] == 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        parent_human_donor_1['@id'],
+        {
+            'related_donors': [{'donor': human_donor['@id'], 'relationship_type': 'child'}]
+        }
+    )
+    res = testapp.get(human_donor['@id'] + '@@index-data')
+    assert all(
+        error['category'] != 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    # A donor should inherit audits from a related donor
+    testapp.patch_json(
+        parent_human_donor_1['@id'],
+        {
+            'related_donors': [{'donor': parent_human_donor_2['@id'], 'relationship_type': 'child'},
+                               {'donor': parent_human_donor_2['@id'], 'relationship_type': 'second cousin'}]
+        }
+    )
+    res = testapp.get(human_donor['@id'] + '@@index-data')
+    assert any(
+        error['category'] == 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('WARNING', [])
+    )
+    assert any(
+        error['category'] == 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        parent_human_donor_1['@id'],
+        {
+            'related_donors': [{'donor': human_donor['@id'], 'relationship_type': 'child'},
+                               {'donor': parent_human_donor_2['@id'], 'relationship_type': 'second cousin'}]
+        }
+    )
+    testapp.patch_json(
+        parent_human_donor_2['@id'],
+        {
+            'related_donors': [{'donor': human_donor['@id'], 'relationship_type': 'child'},
+                               {'donor': parent_human_donor_1['@id'], 'relationship_type': 'second cousin'}]
+        }
+    )
+    res = testapp.get(human_donor['@id'] + '@@index-data')
+    assert all(
+        error['category'] != 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('WARNING', [])
+    )
+    assert all(
+        error['category'] != 'inconsistent related donors metadata'
+        for error in res.json['audit'].get('ERROR', [])
+    )
