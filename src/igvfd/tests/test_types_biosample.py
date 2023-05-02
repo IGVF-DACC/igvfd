7be213058934639931aa307636aeda694fca2db0
corismall <corinn.small@gmail.com>
2023-05-02 14:44:29 -0700

    add test for taxa calculation

--- src/igvfd/tests/test_types_biosample.py
+++ src/igvfd/tests/test_types_biosample.py
@@ -4,7 +4,7 @@ import pytest
 def test_tissue_sex_calculation(testapp, tissue, human_donor, human_male_donor):
     res = testapp.patch_json(
         tissue['@id'],
-        {'donors': [human_donor['@id']], 'taxa': 'Homo sapiens'})
+        {'donors': [human_donor['@id']]})
     res = testapp.get(tissue['@id'])
     assert res.json.get('sex') == 'unspecified'
     res = testapp.patch_json(
@@ -155,3 +155,21 @@ def test_summary(testapp, tissue, primary_cell, whole_organism, in_vitro_cell_li
     assert res.json.get('summary') == 'brown adipose tissue organoid, Mus musculus (1 month)'
     res = testapp.get(technical_sample['@id'])
     assert res.json.get('summary') == 'synthetic technical sample'
+
+
+def test_tissue_taxa_calculation(testapp, tissue, human_donor, rodent_donor):
+    res = testapp.patch_json(
+        tissue['@id'],
+        {'donors': [human_donor['@id']]})
+    res = testapp.get(tissue['@id'])
+    assert res.json.get('taxa') == 'Homo sapiens'
+    res = testapp.patch_json(
+        tissue['@id'],
+        {'donors': [rodent_donor['@id']]})
+    res = testapp.get(tissue['@id'])
+    assert res.json.get('taxa') == 'Mus musculus'
+    res = testapp.patch_json(
+        tissue['@id'],
+        {'donors': [human_donor['@id'], rodent_donor['@id']]})
+    res = testapp.get(tissue['@id'])
+    assert res.json.get('taxa') == None
