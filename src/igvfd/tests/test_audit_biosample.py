4b66cef274391a7d3f25b3eaf38a7a415b9bafdd
corismall <corinn.small@gmail.com>
2023-05-02 15:17:21 -0700

    add/edit tests

--- src/igvfd/tests/test_audit_biosample.py
+++ src/igvfd/tests/test_audit_biosample.py
@@ -21,3 +21,34 @@ def test_audit_biosample_nih_institutional_certification(
         error['category'] != 'missing nih_institutional_certification'
         for error in res.json['audit'].get('ERROR', [])
     )
+
+
+def test_audit_biosample_taxa_check(testapp, tissue, rodent_donor, human_donor, human_male_donor):
+    '''A Biosample that has donors of different taxa should not have calculated taxa property and trigger audit.'''
+
+    testapp.patch_json(tissue['@id'],
+                       {'donors': [rodent_donor['@id'],
+                                   human_donor['@id']]}
+                       )
+    res = testapp.get(tissue['@id'] + '@@index-data')
+    assert any(
+        error['category'] == 'inconsistent/mixed donor taxa'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(tissue['@id'],
+                       {'donors': [human_donor['@id'],
+                                   human_male_donor['@id']]}
+                       )
+    res = testapp.get(tissue['@id'] + '@@index-data')
+    assert not any(
+        error['category'] == 'inconsistent/mixed donor taxa'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(tissue['@id'],
+                       {'donors': [human_donor['@id']]}
+                       )
+    res = testapp.get(tissue['@id'] + '@@index-data')
+    assert not any(
+        error['category'] == 'inconsistent/mixed donor taxa'
+        for error in res.json['audit'].get('ERROR', [])
+    )
