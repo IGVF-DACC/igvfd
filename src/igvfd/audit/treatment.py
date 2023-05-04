4d6d48bce76c45832843990495e9764bbdae91de
corismall <corinn.small@gmail.com>
2023-05-04 13:55:40 -0700

    changed audit description

--- src/igvfd/audit/treatment.py
+++ src/igvfd/audit/treatment.py
@@ -10,10 +10,10 @@ from .formatter import (
 
 @audit_checker('Treatment', frame='object')
 def audit_treatment_term_id_check(value, system):
-    '''If treatment_term_id starts with NTR, flag treatment object with warning audit.'''
+    '''If treatment_term_id starts with NTR, flag treatment object with warning of new term request.'''
     if 'treatment_term_id' in value:
         treatment_id = value['@id']
         term_id = value['treatment_term_id']
         if term_id.startswith('NTR'):
             detail = f'Treatment {audit_link(path_to_text(treatment_id, treatment_id))} has term_id {audit_link(term_id,term_id)}.'
-            yield AuditFailure('treatment term is newly requested', detail, level='WARNING')
+            yield AuditFailure('treatment term has been newly requested', detail, level='WARNING')
