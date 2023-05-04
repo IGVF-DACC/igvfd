665de8da79c79ad55bb452ff08cae45815ebca78
corismall <corinn.small@gmail.com>
2023-05-04 15:04:57 -0700

    cleaned up code

--- src/igvfd/audit/treatment.py
+++ src/igvfd/audit/treatment.py
@@ -13,8 +13,7 @@ def audit_treatment_term_id_check(value, system):
     '''If treatment_term_id starts with NTR, flag treatment object with warning of new term request.'''
     if 'treatment_term_id' in value:
         term_id = value['treatment_term_id']
-        if term_id.startswith('NTR') == True:
+        if term_id.startswith('NTR'):
             treatment_id = value['@id']
             detail = f'Treatment {audit_link(treatment_id, treatment_id)} has term_id {audit_link(term_id,term_id)}.'
-            print('Detail:', detail)
             yield AuditFailure('treatment term has been newly requested', detail, level='WARNING')
