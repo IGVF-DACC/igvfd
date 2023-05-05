a729a13864f4417298b4253ae3421a7453e7b157
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2023-05-05 17:46:00 -0500

    IGVF-506-use-columns-from-subtypes-in-abstract-search-configs (#331)

--- src/igvfd/searches/configs/__init__.py
+++ src/igvfd/searches/configs/__init__.py
@@ -1,2 +1,36 @@
+from pyramid.interfaces import PHASE3_CONFIG
+
+from snovault.interfaces import TYPES
+
+from snovault.elasticsearch.searches.interfaces import SEARCH_CONFIG
+
+from snosearch.interfaces import COLUMNS
+
+
+def register_subtype_columns_in_abstract_search_configs(config):
+    item_registry = config.registry[TYPES]
+    search_config_registry = config.registry[SEARCH_CONFIG]
+    for name, item in item_registry.abstract.items():
+        subtypes = item.subtypes
+        if len(subtypes) == 1:  # No children.
+            continue
+        search_config = search_config_registry.get(name)
+        if search_config is None:
+            raise ValueError(f'Must define search config for {name}')
+        if search_config.columns:  # Manually set, skip.
+            continue
+        columns = {}
+        for subtype in subtypes:
+            subtype_search_config = search_config_registry.get(subtype)
+            columns.update(subtype_search_config.columns)
+        search_config.update(**{COLUMNS: columns})
+
+
 def includeme(config):
     config.scan()
+    config.action(
+        ('register-subtype-columns-in-abstract-search-configs',),
+        callable=register_subtype_columns_in_abstract_search_configs,
+        args=(config,),
+        order=PHASE3_CONFIG,
+    )
