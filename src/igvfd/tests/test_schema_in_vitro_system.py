f61dba7457d4e49397a4a596b01300b4d54d7012
ian-whaling <whaling.ian@gmail.com>
2023-05-10 10:52:47 -0700

    fixtures fix

--- src/igvfd/tests/test_schema_in_vitro_system.py
+++ src/igvfd/tests/test_schema_in_vitro_system.py
@@ -12,17 +12,17 @@ def test_passage_number_dependency(in_vitro_cell_line, testapp):
     assert res.status_code == 422
 
 
-def test_time_post_factors_dependency(in_vitro_differentiated_cell, treatment_chemical, testapp):
+def test_time_post_factors_dependency(in_vitro_cell_line, treatment_chemical, testapp):
     res = testapp.patch_json(
-        in_vitro_differentiated_cell['@id'],
+        in_vitro_cell_line['@id'],
         {'time_post_factors_introduction': 3}, expect_errors=True)
     assert res.status_code == 422
     res = testapp.patch_json(
-        in_vitro_differentiated_cell['@id'],
+        in_vitro_cell_line['@id'],
         {'introduced_factors': [treatment_chemical['@id']]}, expect_errors=True)
     assert res.status_code == 422
     res = testapp.patch_json(
-        in_vitro_differentiated_cell['@id'],
+        in_vitro_cell_line['@id'],
         {'time_post_factors_introduction': 3, 'time_post_factors_introduction_units': 'day', 'introduced_factors': [treatment_chemical['@id']]})
     assert res.status_code == 200
 
