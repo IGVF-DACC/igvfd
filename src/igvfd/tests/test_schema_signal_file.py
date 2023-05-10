c47ca95a81f12f7157f08e1a2fbbe51239681e5a
ian-whaling <whaling.ian@gmail.com>
2023-05-10 10:59:14 -0700

    removed tests

--- src/igvfd/tests/test_schema_signal_file.py
+++ src/igvfd/tests/test_schema_signal_file.py
@@ -32,23 +32,3 @@ def test_start_view_position_regex(testapp, signal_file):
             'start_view_position': 'chr20:54362'
         })
     assert res.status_code == 200
-
-
-def test_sequence_file_dbxrefs_regex(testapp, signal_file):
-    res = testapp.patch_json(
-        signal_file['@id'],
-        {'assembly': 'GRCm39', 'genome_annotation': 'V40'},
-        expect_errors=True
-    )
-    assert res.status_code == 422
-    res = testapp.patch_json(
-        signal_file['@id'],
-        {'assembly': 'GRCh38', 'genome_annotation': 'M30'},
-        expect_errors=True
-    )
-    assert res.status_code == 422
-    res = testapp.patch_json(
-        signal_file['@id'],
-        {'assembly': 'GRCm39', 'genome_annotation': 'M30'}
-    )
-    assert res.status_code == 200
