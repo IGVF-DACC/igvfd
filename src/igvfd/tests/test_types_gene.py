76be100cda970988395e1851803666e603892fe0
Jennifer Jou <jjou@stanford.edu>
2023-05-10 13:06:03 -0700

    Create new fixture

--- src/igvfd/tests/test_types_gene.py
+++ src/igvfd/tests/test_types_gene.py
@@ -11,7 +11,7 @@ def test_gene_mus_musculus(gene_zscan10_mm, testapp):
     assert res.json['title'] == 'Zcan10 (Mus musculus)'
 
 
-def test_gene_geneid_with_version(gene_zscan10_mm, testapp):
+def test_gene_geneid_with_version(gene_zscan10_mm, gene_CRLF2_par_y, testapp):
     testapp.patch_json(
         gene_zscan10_mm['@id'],
         {'version_number': '11'}
@@ -19,9 +19,5 @@ def test_gene_geneid_with_version(gene_zscan10_mm, testapp):
     res = testapp.get(gene_zscan10_mm['@id'])
     assert res.json['geneid_with_version'] == 'ENSMUSG00000023902.11'
 
-    testapp.patch_json(
-        gene_zscan10_mm['@id'],
-        {'geneid': 'ENSMUSG00000023902_PAR_Y'}
-    )
-    res = testapp.get(gene_zscan10_mm['@id'])
-    assert res.json['geneid_with_version'] == 'ENSMUSG00000023902.11_PAR_Y'
+    res = testapp.get(gene_CRLF2_par_y['@id'])
+    assert res.json['geneid_with_version'] == 'ENSG00000205755.3_PAR_Y'
