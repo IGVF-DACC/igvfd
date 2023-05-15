9ceee820c7a0d1f6602d646bbd7413894985a2ec
corismall <corinn.small@gmail.com>
2023-05-15 09:01:51 -0600

    changed category to internal_action

--- src/igvfd/audit/treatment.py
+++ src/igvfd/audit/treatment.py
@@ -16,4 +16,4 @@ def audit_treatment_term_id_check(value, system):
         if term_id.startswith('NTR'):
             treatment_id = value['@id']
             detail = f'Treatment term for {audit_link(treatment_id, treatment_id)} has been newly requested. Term {audit_link(term_id,term_id)} will be replaced with a CHEBI or UNIPROT term following its addition to the appropriate ontology database.'
-            yield AuditFailure('treatment term has been newly requested', detail, level='WARNING')
+            yield AuditFailure('treatment term has been newly requested', detail, level='INTERNAL_ACTION')
