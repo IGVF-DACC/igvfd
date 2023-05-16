e4ffab5781263916e3c493bcc50f3b7ea5b92b4c
corismall <35836875+corismall@users.noreply.github.com>
2023-05-15 17:57:00 -0700

    IGVF-582-ntr-ontologyterm-audit (#341)

---
+++ src/igvfd/audit/ontology_term.py
@@ -0,0 +1,21 @@
+from snovault.auditor import (
+    audit_checker,
+    AuditFailure,
+)
+from .formatter import (
+    audit_link,
+    path_to_text,
+)
+
+
+@audit_checker('OntologyTerm', frame='object')
+def audit_ntr_term_id(value, system):
+    '''
+    OntologyTerm objects with a `term_id` that starts with NTR should be flagged with an internal_action audit.
+    '''
+    if 'term_id' in value:
+        ontologyterm_id = value['@id']
+        term_id = value['term_id']
+        if term_id.startswith('NTR'):
+            detail = f'Ontology term for {audit_link(ontologyterm_id, ontologyterm_id)} has been newly requested. Term {audit_link(term_id,term_id)} will be replaced with another term_id following its addition to the appropriate ontology database.'
+            yield AuditFailure('Ontology term has been newly requested', detail, level='INTERNAL_ACTION')
---
+++ src/igvfd/tests/test_audit_ontology_term.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def test_ntr_audit(testapp, assay_term_ntr):
+    res = testapp.get(assay_term_ntr['@id'] + '@@index-data')
+    print(res.json['audit'].get('INTERNAL_ACTION', []))
+    assert any(
+        error['category'] == 'Ontology term has been newly requested'
+        for error in res.json['audit'].get('INTERNAL_ACTION', [])
+    )
