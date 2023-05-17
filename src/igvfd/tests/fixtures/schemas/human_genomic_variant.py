f49c2092ae457406b27f4b0d48b70b295de74d65
corismall <corinn.small@gmail.com>
2023-05-17 12:56:24 -0700

    add fixture

--- src/igvfd/tests/fixtures/schemas/human_genomic_variant.py
+++ src/igvfd/tests/fixtures/schemas/human_genomic_variant.py
@@ -37,3 +37,17 @@ def human_genomic_variant_v1(human_genomic_variant):
         'refseq_id': 'NC_999999i00'
     })
     return item
+
+
+@pytest.fixture
+def human_genomic_variant_match(testapp):
+    item = {
+        'ref': 'A',
+        'alt': 'A',
+        'chromosome': 'chr1',
+        'assembly': 'GRCh38',
+        'position': 1000000,
+        'rsid': 'rs100',
+        'refseq_id': 'NC_999999.00'
+    }
+    return testapp.post_json('/human_genomic_variant', item, status=201).json['@graph'][0]
