9cd6dd969884277d0db0099351b8231b2e112b1d
corismall <corinn.small@gmail.com>
2023-05-19 14:37:09 -0700

    remove print statement

--- src/igvfd/tests/test_audit_variant.py
+++ src/igvfd/tests/test_audit_variant.py
@@ -3,7 +3,6 @@ import pytest
 
 def test_audit_ref_alt_check(testapp, human_genomic_variant_match):
     res = testapp.get(human_genomic_variant_match['@id'] + '@@index-data')
-    print(res)
     assert any(
         error['category'] == 'variant ref and alt alleles match'
         for error in res.json['audit'].get('ERROR', [])
