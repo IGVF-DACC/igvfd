5ee662ab0756235953dbed7bdf42af467d1ffe4c
iyoungworth <iay@stanford.edu>
2023-05-22 14:45:28 -0700

    iterate-through-donors

--- src/igvfd/audit/whole_organism.py
+++ src/igvfd/audit/whole_organism.py
@@ -11,9 +11,20 @@ from .formatter import (
 @audit_checker('WholeOrganism', frame='object')
 def audit_whole_organism_human_taxa(value, system):
     '''Flag whole organisms associated with taxa Homo sapiens.'''
-    if value.get('taxa', '') == 'Homo sapiens':
-        detail = (
-            f'Whole organism {audit_link(path_to_text(value["@id"]), value["@id"])} '
-            f'specifies that it is of taxa Homo sapiens, this is disallowed.'
-        )
-        yield AuditFailure('incorrect taxa', detail, level='ERROR')
+    if 'donors' in value:
+        donor_ids = value.get('donors')
+        taxa_dict = {}
+        for d in donor_ids:
+            donor_object = system.get('request').embed(d + '@@object?skip_calculated=true')
+            if donor_object.get('taxa'):
+                taxa = donor_object.get('taxa')
+                if taxa not in taxa_dict:
+                    taxa_dict[taxa] = []
+                taxa_dict[taxa].append(d)
+
+        if 'Homo sapiens' in taxa_dict:
+            detail = (
+                f'Whole organism {audit_link(path_to_text(value["@id"]), value["@id"])} '
+                f'specifies that it is of taxa Homo sapiens, this is disallowed.'
+            )
+            yield AuditFailure('incorrect taxa', detail, level='ERROR')
--- src/igvfd/tests/test_audit_whole_organism.py
+++ src/igvfd/tests/test_audit_whole_organism.py
@@ -1,7 +1,17 @@
 import pytest
 
 
-def test_audit_whole_organism_human_taxa(testapp, whole_organism_human):
+def test_audit_whole_organism_human_taxa(
+        testapp, whole_organism_human, human_donor, rodent_donor):
+    res = testapp.get(whole_organism_human['@id'] + '@@index-data')
+    assert any(
+        error['category'] == 'incorrect taxa'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(whole_organism_human['@id'],
+                       {'donors': [rodent_donor['@id'],
+                                   human_donor['@id']]}
+                       )
     res = testapp.get(whole_organism_human['@id'] + '@@index-data')
     assert any(
         error['category'] == 'incorrect taxa'
