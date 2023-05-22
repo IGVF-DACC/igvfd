0e5286a4bc8455275499f88ce8ff40eec9884008
Idan <gabdank@stanford.edu>
2023-05-22 16:56:30 -0700

    add audit to tissue

--- src/igvfd/audit/tissue.py
+++ src/igvfd/audit/tissue.py
@@ -18,3 +18,15 @@ def audit_tissue_ccf_id(value, system):
             f'is missing common coordinate framework identifier (CCF ID) required for human data.'
         )
         yield AuditFailure('missing ccf_id', detail, level='NOT_COMPLIANT')
+
+
+@audit_checker('Tissue', frame='object')
+def audit_tissue_ccf_id_2(value, system):
+    '''Tissue objects must specify a common coordinate framework identifier (CCF ID) required for human data.'''
+    if ('ccf_id' not in value) and (value.get('taxa') == 'Homo sapiens'):
+        value_id = system.get('path')
+        detail = (
+            f'Tissue {audit_link(path_to_text(value_id), value_id)} '
+            f'is missing common coordinate framework identifier (CCF ID) required for human data.'
+        )
+        yield AuditFailure('missing ccf_id', detail, level='ERROR')
