7db9b05960982ce0a317825c29c1d4ebfaad0742
iyoungworth <iay@stanford.edu>
2023-05-23 10:11:37 -0700

    pr-review-changes

--- src/igvfd/audit/whole_organism.py
+++ src/igvfd/audit/whole_organism.py
@@ -11,20 +11,18 @@ from .formatter import (
 @audit_checker('WholeOrganism', frame='object')
 def audit_whole_organism_human_taxa(value, system):
     '''Flag whole organisms associated with taxa Homo sapiens.'''
-    if 'donors' in value:
+    detail = (
+        f'Whole organism {audit_link(path_to_text(value["@id"]), value["@id"])} '
+        f'specifies that it is of taxa Homo sapiens, this is disallowed.'
+    )
+    if 'taxa' in value:
+        if value['taxa'] == 'Homo sapiens':
+            yield AuditFailure('incorrect taxa', detail, level='ERROR')
+    elif 'donors' in value:
         donor_ids = value.get('donors')
-        taxa_dict = {}
+        taxa_set = set()
         for d in donor_ids:
             donor_object = system.get('request').embed(d + '@@object?skip_calculated=true')
-            if donor_object.get('taxa'):
-                taxa = donor_object.get('taxa')
-                if taxa not in taxa_dict:
-                    taxa_dict[taxa] = []
-                taxa_dict[taxa].append(d)
-
-        if 'Homo sapiens' in taxa_dict:
-            detail = (
-                f'Whole organism {audit_link(path_to_text(value["@id"]), value["@id"])} '
-                f'specifies that it is of taxa Homo sapiens, this is disallowed.'
-            )
+            taxa_set.add(donor_object.get('taxa', ''))
+        if 'Homo sapiens' in taxa_set:
             yield AuditFailure('incorrect taxa', detail, level='ERROR')
