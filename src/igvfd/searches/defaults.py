962c2737a251b123ba9a37cf09cc0c7117bab06c
Meenakshi Kagda <meenakshi@C02XR143JGH8.local>
2023-05-26 12:57:55 -0700

    fix defaults

--- src/igvfd/searches/defaults.py
+++ src/igvfd/searches/defaults.py
@@ -57,7 +57,7 @@ TOP_HITS_ITEM_TYPES = [
     'HumanGenomicVariant'
 ]
 
-DEFAULT_ITEM_TYPES == TOP_HITS_ITEM_TYPES
+DEFAULT_ITEM_TYPES = TOP_HITS_ITEM_TYPES
 
 # Top_hits_item_types interact with the search box
 # Default item types would be searchable using ?searchTerm=ABC format in the url without having to use &type=Item
