646a341f5d37c6ec0a53fc438d4b14817d387735
Otto Jolanki <ojolanki@stanford.edu>
2023-05-25 18:54:40 -0700

    no slash?

--- src/igvfd/audit_docstring.py
+++ src/igvfd/audit_docstring.py
@@ -2,10 +2,10 @@ from pyramid.view import view_config
 
 
 def includeme(config):
-    config.add_route('audit-docstring', '/audit-docstring{slash:/?}')
+    config.add_route('audit_docstring', '/audit-docstring')
     config.scan(__name__)
 
 
-@view_config(route_name='audit-docstring', request_method='GET')
+@view_config(route_name='audit_docstring', request_method='GET')
 def audit_docstring(context, request):
     return {'message': 'Audit Docstring'}
