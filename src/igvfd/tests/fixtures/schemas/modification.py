dd2f02922f456663c0f79dce5aeb106f6056e3e5
corismall <corinn.small@gmail.com>
2023-06-07 11:34:32 -0700

    new fixture

--- src/igvfd/tests/fixtures/schemas/modification.py
+++ src/igvfd/tests/fixtures/schemas/modification.py
@@ -7,7 +7,17 @@ def modification(testapp, lab, award):
         'award': award['@id'],
         'lab': lab['@id'],
         'cas': 'dCas9',
-        'modality': 'interference',
-        'cas_species': 'Streptococcus pyogenes (Sp)'
+        'modality': 'interference'
+    }
+    return testapp.post_json('/modification', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def modification_v2(testapp, lab, award):
+    item = {
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'cas': 'dCas9',
+        'modality': 'interference'
     }
     return testapp.post_json('/modification', item, status=201).json['@graph'][0]
