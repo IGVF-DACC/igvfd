329009c5b1eab645af377ecb7b48b4b516d6e250
corismall <corinn.small@gmail.com>
2023-06-07 12:47:25 -0700

    rewrite upgrade

--- src/igvfd/upgrade/modification.py
+++ src/igvfd/upgrade/modification.py
@@ -4,9 +4,5 @@ from snovault import upgrade_step
 @upgrade_step('modification', '1', '2')
 def modification_1_2(value, system):
     # https://igvf.atlassian.net/browse/IGVF-729
-    if 'cas' in value:
-        print(value['cas'])
-        print(value['cas_species'])
-        #value['cas_species'] = 'Streptococcus pyogenes (Sp)'
-    if 'schema_version' in value:
-        print(value['schema_version'])
+    if 'cas_species' not in value:
+        value['cas_species'] = 'Streptococcus pyogenes (Sp)'
