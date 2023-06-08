6bc4eee11360d96186d69fa2b5f5ab83558d2063
corismall <corinn.small@gmail.com>
2023-06-07 17:18:35 -0700

    add note

--- src/igvfd/upgrade/modification.py
+++ src/igvfd/upgrade/modification.py
@@ -6,3 +6,5 @@ def modification_1_2(value, system):
     # https://igvf.atlassian.net/browse/IGVF-729
     if 'cas_species' not in value:
         value['cas_species'] = 'Streptococcus pyogenes (Sp)'
+        value[
+            'notes'] = 'For upgrade, cas_species has been automatically designated as Streptococcus pyogenes (Sp), follow up with associated lab to check if upgrade is valid.'
