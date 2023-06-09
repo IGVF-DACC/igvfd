37b279253d17d28004c2b3474515a7d745ed8551
corismall <corinn.small@gmail.com>
2023-06-09 13:49:28 -0700

    reduced lines and fixed cas_species assignment

--- src/igvfd/upgrade/modification.py
+++ src/igvfd/upgrade/modification.py
@@ -4,9 +4,8 @@ from snovault import upgrade_step
 @upgrade_step('modification', '1', '2')
 def modification_1_2(value, system):
     # https://igvf.atlassian.net/browse/IGVF-729
-    notes = value.get('notes', '')
     if 'cas_species' not in value:
+        new_notes = value.get('notes', '')
+        new_notes += 'For upgrade, cas_species has been automatically designated as Streptococcus pyogenes (Sp), follow up with associated lab to check if upgrade is valid.'
+        value['notes'] = new_notes.strip()
         value['cas_species'] = 'Streptococcus pyogenes (Sp)'
-        notes += 'For upgrade, cas_species has been automatically designated as Streptococcus pyogenes (Sp), follow up with associated lab to check if upgrade is valid.'
-    new_notes = notes.strip()
-    value['notes'] = new_notes
