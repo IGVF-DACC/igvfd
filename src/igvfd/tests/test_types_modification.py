8d2f7139c875eb1096ed56068cc69abd86ced747
corismall <corinn.small@gmail.com>
2023-06-12 11:02:06 -0700

    add sp to second summary calc test

--- src/igvfd/tests/test_types_modification.py
+++ src/igvfd/tests/test_types_modification.py
@@ -13,7 +13,7 @@ def test_modification_summary(testapp, modification, gene_myc_hs):
         }
     )
     res = testapp.get(modification['@id'])
-    assert res.json.get('summary') == 'CRISPRa dCas9-VP64'
+    assert res.json.get('summary') == 'CRISPRa SpdCas9-VP64'
 
     testapp.patch_json(
         modification['@id'],
