f22535a89f1a259bfbe967baaede875367b93372
khine <khzlin@stanford.edu>
2023-06-15 16:52:57 -0700

    fix bracket, new mappings

--- src/igvfd/audit/biosample.py
+++ src/igvfd/audit/biosample.py
@@ -55,7 +55,7 @@ def audit_biosample_age(value, system):
         if 'lower_bound_age' and 'upper_bound_age' and 'age_units' not in value:
             value_id = system.get('path')
             detail = (
-                f'{Biosample} {audit_link(path_to_text(value_id), value_id)} '
+                f'Biosample {audit_link(path_to_text(value_id), value_id)} '
                 f'is missing upper_bound_age, lower_bound_age, and age_units.'
             )
             yield AuditFailure('missing age properties', detail, level='WARNING')
