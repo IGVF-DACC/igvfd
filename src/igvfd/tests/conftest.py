7f689448afd509caaea769536f7644f8e57bc066
khine <khzlin@stanford.edu>
2023-06-26 09:45:02 -0700

    fix comma

--- src/igvfd/tests/conftest.py
+++ src/igvfd/tests/conftest.py
@@ -49,7 +49,7 @@ pytest_plugins = [
     'igvfd.tests.fixtures.schemas.software',
     'igvfd.tests.fixtures.schemas.software_version',
     'igvfd.tests.fixtures.schemas.construct_library',
-    'igvfd.tests.fixtures.schemas.model'
+    'igvfd.tests.fixtures.schemas.model',
     'igvfd.tests.fixtures.schemas.reference_file'
 ]
 
