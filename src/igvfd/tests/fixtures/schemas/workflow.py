a17738f662e661315a8ffc648a91e3a46652ea1d
Jennifer Jou <jjou@stanford.edu>
2023-06-26 16:35:08 -0700

    Fix fixture

--- src/igvfd/tests/fixtures/schemas/workflow.py
+++ src/igvfd/tests/fixtures/schemas/workflow.py
@@ -6,7 +6,7 @@ def base_workflow(testapp, award, lab):
     item = {
         'accession': 'IGVFWF0000WRKF',
         'name': 'Base Workflow',
-        'source_url': 'https://data.igvf.org/',
+        'source_url': 'https://github.com/IGVF-DACC/igvfd',
         'award': award['@id'],
         'lab': lab['@id']
     }
