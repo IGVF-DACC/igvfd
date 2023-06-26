c9135d816d7be2228d09b4cd0eaa699e4dc27439
corismall <35836875+corismall@users.noreply.github.com>
2023-06-26 16:53:16 -0700

    IGVF-741-test-audit-print-statement (#421)

--- src/igvfd/tests/test_audit_ontology_term.py
+++ src/igvfd/tests/test_audit_ontology_term.py
@@ -3,7 +3,6 @@ import pytest
 
 def test_ntr_audit(testapp, assay_term_ntr):
     res = testapp.get(assay_term_ntr['@id'] + '@@audit')
-    print(res.json['audit'].get('INTERNAL_ACTION', []))
     assert any(
         error['category'] == 'Ontology term has been newly requested'
         for error in res.json['audit'].get('INTERNAL_ACTION', [])
