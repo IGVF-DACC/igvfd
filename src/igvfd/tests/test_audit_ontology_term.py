ca5d2ce5d664edecb6df0aac2f532458ffaa0ba0
corismall <corinn.small@gmail.com>
2023-06-26 14:32:42 -0700

    changed res

--- src/igvfd/tests/test_audit_ontology_term.py
+++ src/igvfd/tests/test_audit_ontology_term.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_ntr_audit(testapp, assay_term_ntr):
-    res = testapp.get(assay_term_ntr['@id'] + '@@index-data')
+    res = testapp.get(assay_term_ntr['@id'] + '@@audit')
     assert any(
         error['category'] == 'Ontology term has been newly requested'
         for error in res.json['audit'].get('INTERNAL_ACTION', [])
