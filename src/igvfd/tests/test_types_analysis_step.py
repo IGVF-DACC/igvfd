3697f83e9d6c5d331c8c85309ed1fe1ca021135b
Jennifer Jou <jjou@stanford.edu>
2023-06-26 16:49:10 -0700

    Fix tests

--- src/igvfd/tests/test_types_analysis_step.py
+++ src/igvfd/tests/test_types_analysis_step.py
@@ -2,6 +2,6 @@ import pytest
 
 
 def test_analysis_step_name_calcprop(testapp, analysis_step):
-    assert analysis_step['@id'] == '/analysis-steps/base-analysis-step-v-1/'
+    assert analysis_step['@id'] == '/analysis-steps/IGVFWF0000WRKF-base-analysis-step/'
     assert analysis_step['name'] == 'IGVFWF0000WRKF-base-analysis-step'
     assert analysis_step['major_version'] == 1
