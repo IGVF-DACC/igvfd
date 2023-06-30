4c18928cab74369a1f70af4eaeabc919092d4eb6
corismall <corinn.small@gmail.com>
2023-06-30 16:35:38 -0700

    add test

---
+++ src/igvfd/tests/test_upgrade_reference_file.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def test_reference_file_upgrade_2_3(upgrader, reference_file_v2):
+    value = upgrader.upgrade('reference_file', reference_file_v2, current_version='2', target_version='3')
+    assert 'source' not in value
+    assert 'source_url' in value
+    assert value['schema_version'] == '3'
