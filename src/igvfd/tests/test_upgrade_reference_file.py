9017b20b2111d3719200d8cf1f8dc59695a1d55a
corismall <35836875+corismall@users.noreply.github.com>
2023-07-11 11:37:38 -0700

    IGVF-794-rename-source-in-reference-file (#429)

---
+++ src/igvfd/tests/test_upgrade_reference_file.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def test_reference_file_upgrade_2_3(upgrader, reference_file_v2):
+    source = reference_file_v2['source']
+    value = upgrader.upgrade('reference_file', reference_file_v2, current_version='2', target_version='3')
+    assert source == value['source_url']
+    assert 'source' not in value
+    assert 'source_url' in value
+    assert value['schema_version'] == '3'
--- src/igvfd/upgrade/file.py
+++ src/igvfd/upgrade/file.py
@@ -9,3 +9,11 @@ def file_2b_3(value, system):
     notes += f' PlatformTerm added via upgrade; verify before removing note.'
     value['notes'] = notes.strip()
     return
+
+
+@upgrade_step('reference_file', '2', '3')
+def ref_file_2_3(value, system):
+    # https://igvf.atlassian.net/browse/IGVF-794
+    if 'source' in value:
+        value['source_url'] = value['source']
+        del value['source']
