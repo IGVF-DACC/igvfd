ea81c15f7766e6b1dba152e22b7e8bd5a0bc1908
jenjou <42983888+jenjou@users.noreply.github.com>
2023-07-18 17:40:44 -0700

    IGVF-660-audit-curated-set-taxa (#445)

--- src/igvfd/commands/make_audit_docstring_json.py
+++ src/igvfd/commands/make_audit_docstring_json.py
@@ -6,6 +6,7 @@ import igvfd.audit.formatter
 import igvfd.audit.human_donor
 import igvfd.audit.in_vitro_system
 import igvfd.audit.item
+import igvfd.audit.curated_set
 import igvfd.audit.measurement_set
 import igvfd.audit.curated_set
 import igvfd.audit.ontology_term
@@ -28,6 +29,7 @@ AUDIT_MODULES_TO_PROCESS = [
     igvfd.audit.human_donor,
     igvfd.audit.in_vitro_system,
     igvfd.audit.item,
+    igvfd.audit.curated_set,
     igvfd.audit.measurement_set,
     igvfd.audit.curated_set,
     igvfd.audit.ontology_term,
--- src/igvfd/tests/test_audit_curated_set.py
+++ src/igvfd/tests/test_audit_curated_set.py
@@ -20,3 +20,24 @@ def test_audit_donors_mismatch(
         error['category'] == 'inconsistent donors metadata'
         for error in res.json['audit'].get('ERROR', [])
     )
+
+
+def test_audit_related_multiome_datasets(
+    testapp,
+    curated_set_genome,
+    human_donor,
+    in_vitro_cell_line
+):
+    testapp.patch_json(
+        curated_set_genome['@id'],
+        {
+            'taxa': 'Homo sapiens',
+            'samples': [in_vitro_cell_line['@id']],
+            'donors': [human_donor['@id']]
+        }
+    )
+    res = testapp.get(curated_set_genome['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'inconsistent taxa metadata'
+        for error in res.json['audit'].get('ERROR', [])
+    )
