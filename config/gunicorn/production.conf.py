b243159e3872c1fd83ccf7bd3f021fb8bd36bb04
Keenan Graham <ksgraham@stanford.edu>
2023-07-24 13:44:34 -0700

    Update worker number and point to sandbox snapshot

--- config/gunicorn/production.conf.py
+++ config/gunicorn/production.conf.py
@@ -1,5 +1,5 @@
 bind = '0.0.0.0:6543'
-workers = 4
+workers = 8
 threads = 1
 accesslog = '-'
 access_log_format = '{\"host\":"%(h)s", \"user\":"%(u)s", \"time\":"%(t)s", \"statusline\":"%(r)s", \"status\":%(s)s, \"response_length\":%(b)s, \"referer\":"%(f)s", \"user_agent\":"%(a)s", \"x_stats\":"%({X-Stats}o)s"}'
