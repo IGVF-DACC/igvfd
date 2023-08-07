e2faa6ecaadb8f04dd7d6fe06f68486e9efa5f4a
KAndreeva <andreva@stanford.edu>
2023-08-07 14:46:35 -0400

    IGVF-916 Added unit test for summary.

--- src/igvfd/tests/test_types_modification.py
+++ src/igvfd/tests/test_types_modification.py
@@ -27,3 +27,29 @@ def test_modification_summary(testapp, modification, gene_myc_hs):
     )
     res = testapp.get(modification['@id'])
     assert res.json.get('summary') == 'CRISPR localizing SpCas13-ANTI-FLAG fused to MYC'
+
+    testapp.patch_json(
+        modification['@id'],
+        {
+            'cas': 'SpG',
+            'cas_species': 'Streptococcus pyogenes (Sp)',
+            'modality': 'localizing',
+            'fused_domain': 'ANTI-FLAG',
+            'tagged_protein': gene_myc_hs['@id']
+        }
+    )
+    res = testapp.get(modification['@id'])
+    assert res.json.get('summary') == 'CRISPR localizing SpG-ANTI-FLAG fused to MYC'
+
+    testapp.patch_json(
+        modification['@id'],
+        {
+            'cas': 'SpRY',
+            'cas_species': 'Streptococcus pyogenes (Sp)',
+            'modality': 'localizing',
+            'fused_domain': 'ANTI-FLAG',
+            'tagged_protein': gene_myc_hs['@id']
+        }
+    )
+    res = testapp.get(modification['@id'])
+    assert res.json.get('summary') == 'CRISPR localizing SpRY-ANTI-FLAG fused to MYC'
