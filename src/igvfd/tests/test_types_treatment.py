bbd8024a2a00519311f0e6408384eca88a794c49
ian-whaling <whaling.ian@gmail.com>
2023-08-16 14:23:55 -0700

    test fixes

--- src/igvfd/tests/test_types_treatment.py
+++ src/igvfd/tests/test_types_treatment.py
@@ -3,9 +3,9 @@ import pytest
 
 def test_treatment_calculated(treatment_chemical, testapp):
     res = testapp.get(treatment_chemical['@id'])
-    assert res.json['title'] == 'Treated with 10 mM lactate for 1 hour'
+    assert res.json['title'] == 'Treatment of 10 mM lactate for 1 hour'
 
 
 def test_treatment_no_duration_calculated(treatment_protein, testapp):
     res = testapp.get(treatment_protein['@id'])
-    assert res.json['title'] == 'Treated with 10 ng/mL G-CSF'
+    assert res.json['title'] == 'Treatment of 10 ng/mL G-CSF'
