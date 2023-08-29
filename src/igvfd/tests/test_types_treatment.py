9a55fed4864caf69acaadc394a5d12bee6fc78fb
ian-whaling <whaling.ian@gmail.com>
2023-08-29 10:57:59 -0700

    test fixes

--- src/igvfd/tests/test_types_treatment.py
+++ src/igvfd/tests/test_types_treatment.py
@@ -1,10 +1,10 @@
 import pytest
 
 
-def test_treatment_title(treatment_chemical, treatment_protein, depletion_treatment, testapp):
+def test_treatment_summary(treatment_chemical, treatment_protein, depletion_treatment, testapp):
     res = testapp.get(treatment_chemical['@id'])
-    assert res.json['title'] == 'Treatment of 10 mM lactate for 1 hour'
+    assert res.json['summary'] == 'Treatment of 10 mM lactate for 1 hour'
     res = testapp.get(treatment_protein['@id'])
-    assert res.json['title'] == 'Treatment of 10 ng/mL G-CSF'
+    assert res.json['summary'] == 'Treatment of 10 ng/mL G-CSF'
     res = testapp.get(depletion_treatment['@id'])
-    assert res.json['title'] == 'Depletion of penicillin for 3 minutes'
+    assert res.json['summary'] == 'Depletion of penicillin for 3 minutes'
