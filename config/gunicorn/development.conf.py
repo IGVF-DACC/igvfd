fdc560371873a630ccd064da802db7f79c70cdd8
Otto Jolanki <ojolanki@stanford.edu>
2023-09-01 10:16:42 -0700

    log x-stats derived values as integers

--- config/gunicorn/development.conf.py
+++ config/gunicorn/development.conf.py
@@ -3,4 +3,4 @@ workers = 7
 threads = 1
 accesslog = '-'
 logger_class = 'igvfd.logging.gunicornlogger.MyGunicornLogger'
-access_log_format = '{\"host\":"%(h)s", \"user\":"%(u)s", \"time\":"%(t)s", \"statusline\":"%(r)s", \"status\":%(s)s, \"response_length\":%(b)s, \"referer\":"%(f)s", \"user_agent\":"%(a)s", \"db_count\":"%(db_count)s", \"db_time\":"%(db_time)s", \"es_count\":"%(es_count)s", \"es_time\":"%(es_time)s", \"rss_begin\": "%(rss_begin)s", \"rss_end\": "%(rss_end)s", \"rss_change\": "%(rss_change)s", \"wsgi_begin\":"%(wsgi_begin)s",  \"wsgi_end\":"%(wsgi_end)s", \"wsgi_time\":"%(wsgi_time)s"}'
+access_log_format = '{\"host\":"%(h)s", \"user\":"%(u)s", \"time\":"%(t)s", \"statusline\":"%(r)s", \"status\":%(s)s, \"response_length\":%(b)s, \"referer\":"%(f)s", \"user_agent\":"%(a)s", \"db_count\":"%(db_count)i", \"db_time\":"%(db_time)i", \"es_count\":"%(es_count)i", \"es_time\":"%(es_time)i", \"rss_begin\": "%(rss_begin)i", \"rss_end\": "%(rss_end)i", \"rss_change\": "%(rss_change)i", \"wsgi_begin\":"%(wsgi_begin)i",  \"wsgi_end\":"%(wsgi_end)i", \"wsgi_time\":"%(wsgi_time)i"}'
