e672c34fa48f1faab41759885bf56c03988172f0
ian-whaling <whaling.ian@gmail.com>
2023-08-31 21:43:01 -0700

    add scope, targeted_loci, targeted_genes

--- src/igvfd/tests/fixtures/schemas/prediction_set.py
+++ src/igvfd/tests/fixtures/schemas/prediction_set.py
@@ -10,3 +10,14 @@ def base_prediction_set(testapp, lab, award, in_vitro_cell_line):
         'samples': [in_vitro_cell_line['@id']]
     }
     return testapp.post_json('/prediction_set', item).json['@graph'][0]
+
+
+@pytest.fixture
+def prediction_set_functional_effect(testapp, lab, award, multiplexed_sample):
+    item = {
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'file_set_type': 'functional effect',
+        'samples': [multiplexed_sample['@id']]
+    }
+    return testapp.post_json('/prediction_set', item).json['@graph'][0]
