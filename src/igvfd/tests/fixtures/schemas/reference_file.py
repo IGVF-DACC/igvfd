494e1e97c11ea5b587ea1dc89d598f882ac69829
ian-whaling <whaling.ian@gmail.com>
2023-09-01 10:37:34 -0700

    mappings

--- src/igvfd/tests/fixtures/schemas/reference_file.py
+++ src/igvfd/tests/fixtures/schemas/reference_file.py
@@ -15,6 +15,19 @@ def reference_file(testapp, lab, award, analysis_set_with_sample):
 
 
 @pytest.fixture
+def reference_file_two(testapp, lab, award, analysis_set_with_sample):
+    item = {
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'md5sum': '7f1987dea86105dd9d2582c0a91c1111',
+        'file_format': 'gtf',
+        'file_set': analysis_set_with_sample['@id'],
+        'content_type': 'transcriptome reference'
+    }
+    return testapp.post_json('/reference_file', item, status=201).json['@graph'][0]
+
+
+@pytest.fixture
 def reference_file_v2(reference_file):
     item = reference_file.copy()
     item.update({
