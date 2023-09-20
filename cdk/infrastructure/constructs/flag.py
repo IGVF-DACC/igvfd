c6be47a2346566d4ecd92972aa55e82b1bc31f4c
Keenan Graham <ksgraham@stanford.edu>
2023-09-20 13:45:41 -0700

    Add custom resource to clean up hosted configuration versions

--- cdk/infrastructure/constructs/flag.py
+++ cdk/infrastructure/constructs/flag.py
@@ -2,7 +2,8 @@ import json
 
 from aws_cdk import App
 from aws_cdk import Stack
-from aws_cdk import RemovalPolicy
+from aws_cdk import CustomResource
+from aws_cdk import Duration
 
 from aws_cdk.aws_appconfig import CfnApplication
 from aws_cdk.aws_appconfig import CfnEnvironment
@@ -11,12 +12,25 @@ from aws_cdk.aws_appconfig import CfnDeploymentStrategy
 from aws_cdk.aws_appconfig import CfnHostedConfigurationVersion
 from aws_cdk.aws_appconfig import CfnDeployment
 
+
+from aws_cdk.aws_iam import PolicyStatement
+from aws_cdk.aws_iam import Role
+
+from aws_cdk.custom_resources import Provider
+
+from aws_cdk.aws_lambda_python_alpha import PythonFunction
+
+from aws_cdk.aws_lambda import Runtime
+
+from aws_cdk.aws_logs import RetentionDays
+
 from constructs import Construct
 
 from dataclasses import dataclass
 
 from typing import Any
 from typing import Dict
+from typing import cast
 
 
 @dataclass
@@ -54,6 +68,7 @@ class FeatureFlagService(Construct):
         self._define_raw_flags()
         self._define_configuration_version()
         self._define_deployment()
+        self._define_configuration_version_cleaner()
 
     def _define_application(self) -> None:
         self.application = CfnApplication(
@@ -118,9 +133,6 @@ class FeatureFlagService(Construct):
             content_type='application/json',
             content=self.raw_flags,
         )
-        self.configuration_version.apply_removal_policy(
-            policy=RemovalPolicy.DESTROY
-        )
 
     def _define_deployment(self) -> None:
         self.deployment = CfnDeployment(
@@ -131,4 +143,47 @@ class FeatureFlagService(Construct):
             configuration_version=self.configuration_version.ref,
             deployment_strategy_id=self.deployment_strategy.ref,
             environment_id=self.environment.ref,
+        )
+
+    def _define_configuration_version_cleaner(self) -> None:
+        # CloudFormation doesn't track new AppConfig HostedConfigurationVersions
+        # when toggling AppConfig flags in console. This custom resource
+        # deletes all of the configuration versions when the stack is deleted.
+        # Otherwise the stack fails to delete.
+        delete_all_hosted_configuration_versions_on_clean_up = PythonFunction(
+            self,
+            'DeleteAllHostedConfigurationVersionsOnCleanUp',
+            entry='infrastructure/runtime/lambdas/flag',
+            runtime=Runtime.PYTHON_3_11,
+            index='main.py',
+            handler='custom_resource_handler',
+            timeout=Duration.seconds(60),
+        )
+        lambda_role = cast(
+            Role,
+            delete_all_hosted_configuration_versions_on_clean_up.role
+        )
+        lambda_role.add_to_policy(
+            PolicyStatement(
+                actions=[
+                    'appconfig:ListHostedConfigurationVersions',
+                    'appconfig:DeleteHostedConfigurationVersion',
+                ],
+                resources=['*'],
+            )
+        )
+        provider = Provider(
+            self,
+            'Provider',
+            on_event_handler=delete_all_hosted_configuration_versions_on_clean_up,
+            log_retention=RetentionDays.ONE_MONTH,
+        )
+        CustomResource(
+            self,
+            'HostedConfigurationVersionCleaner',
+            service_token=provider.service_token,
+            properties={
+                'application_id': self.application.ref,
+                'configuration_profile_id': self.configuration_profile.ref,
+            }
         )
