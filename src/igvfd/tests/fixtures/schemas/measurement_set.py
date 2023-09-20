dea4204202fe1b89cd69eebf729b9df28e541264
ian-whaling <whaling.ian@gmail.com>
2023-09-20 10:43:27 -0700

    introduce dependency

--- src/igvfd/tests/fixtures/schemas/measurement_set.py
+++ src/igvfd/tests/fixtures/schemas/measurement_set.py
@@ -2,33 +2,36 @@ import pytest
 
 
 @pytest.fixture
-def measurement_set(testapp, lab, award, assay_term_starr):
+def measurement_set(testapp, lab, award, assay_term_starr, tissue):
     item = {
         'award': award['@id'],
         'lab': lab['@id'],
-        'assay_term': assay_term_starr['@id']
+        'assay_term': assay_term_starr['@id'],
+        'samples': [tissue['@id']]
     }
     return testapp.post_json('/measurement_set', item).json['@graph'][0]
 
 
 @pytest.fixture
-def measurement_set_multiome(testapp, lab, award, assay_term_atac):
+def measurement_set_multiome(testapp, lab, award, assay_term_atac, tissue):
     item = {
         'award': award['@id'],
         'lab': lab['@id'],
         'assay_term': assay_term_atac['@id'],
-        'multiome_size': 2
+        'multiome_size': 2,
+        'samples': [tissue['@id']]
     }
     return testapp.post_json('/measurement_set', item).json['@graph'][0]
 
 
 @pytest.fixture
-def measurement_set_multiome_2(testapp, lab, award, assay_term_rna):
+def measurement_set_multiome_2(testapp, lab, award, assay_term_rna, in_vitro_cell_line):
     item = {
         'award': award['@id'],
         'lab': lab['@id'],
         'assay_term': assay_term_rna['@id'],
-        'multiome_size': 2
+        'multiome_size': 2,
+        'samples': [in_vitro_cell_line['@id']]
     }
     return testapp.post_json('/measurement_set', item).json['@graph'][0]
 
@@ -65,10 +68,11 @@ def measurement_set_v4(measurement_set):
 
 
 @pytest.fixture
-def measurement_set_mpra(testapp, lab, award, assay_term_mpra):
+def measurement_set_mpra(testapp, lab, award, assay_term_mpra, primary_cell):
     item = {
         'award': award['@id'],
         'lab': lab['@id'],
-        'assay_term': assay_term_mpra['@id']
+        'assay_term': assay_term_mpra['@id'],
+        'samples': primary_cell['@id']
     }
     return testapp.post_json('/measurement_set', item).json['@graph'][0]
--- src/igvfd/tests/test_schema_prediction_set.py
+++ src/igvfd/tests/test_schema_prediction_set.py
@@ -38,3 +38,38 @@ def test_dependencies_construct_library(testapp, gene_myc_hs, base_prediction_se
          'targeted_genes': [gene_myc_hs['@id']]
          })
     assert res.status_code == 200
+
+
+def test_sample_donor_dependency(testapp, lab, award, tissue, human_donor):
+    item = {
+        'lab': lab['@id'],
+        'award': award['@id'],
+        'file_set_type': 'pathogenicity'
+    }
+    response = testapp.post_json('/prediction_set', item, expect_errors=True)
+    assert response.status_code == 422
+    item = {
+        'lab': lab['@id'],
+        'award': award['@id'],
+        'file_set_type': 'pathogenicity',
+        'samples': [tissue['@id']]
+    }
+    response = testapp.post_json('/prediction_set', item)
+    assert response.status_code == 201
+    item = {
+        'lab': lab['@id'],
+        'award': award['@id'],
+        'file_set_type': 'pathogenicity',
+        'donors': [human_donor['@id']]
+    }
+    response = testapp.post_json('/prediction_set', item)
+    assert response.status_code == 201
+    item = {
+        'lab': lab['@id'],
+        'award': award['@id'],
+        'file_set_type': 'pathogenicity',
+        'samples': [tissue['@id']],
+        'donors': [human_donor['@id']]
+    }
+    response = testapp.post_json('/prediction_set', item, expect_errors=True)
+    assert response.status_code == 422
