2fbbda93752253677fd3786d6ad5ba3eee75e32e
ian-whaling <whaling.ian@gmail.com>
2023-09-29 16:43:33 -0700

    analysis set

---
+++ src/igvfd/tests/test_types_analysis_set.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+def test_calculated_donors(testapp, analysis_set_base, primary_cell, human_donor, in_vitro_cell_line, rodent_donor):
+    testapp.patch_json(
+        analysis_set_base['@id'],
+        {
+            'samples': [primary_cell['@id']]
+        }
+    )
+    res = testapp.get(analysis_set_base['@id'])
+    assert set(res.json.get('donors')) == {human_donor['@id']}
+    testapp.patch_json(
+        analysis_set_base['@id'],
+        {
+            'samples': [in_vitro_cell_line['@id']]
+        }
+    )
+    res = testapp.get(analysis_set_base['@id'])
+    assert set(res.json.get('donors')) == {rodent_donor['@id']}
