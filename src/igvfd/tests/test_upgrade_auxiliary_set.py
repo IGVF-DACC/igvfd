2055296f599fe313d75a256e074aa4843f914db0
iyoungworth <iay@stanford.edu>
2023-10-06 13:22:30 -0400

    replace-test

--- src/igvfd/tests/test_upgrade_auxiliary_set.py
+++ src/igvfd/tests/test_upgrade_auxiliary_set.py
@@ -11,6 +11,16 @@ def test_auxiliary_set_upgrade_1_2(upgrader, auxiliary_set_v1):
     assert 'references' not in value
 
 
+def test_auxiliary_set_upgrade_2_3(upgrader, auxiliary_set_v2):
+    old_auxiliary_type = auxiliary_set_v2['auxiliary_type']
+    value = upgrader.upgrade(
+        'auxiliary_set', auxiliary_set_v2,
+        current_version='2', target_version='3')
+    assert value['schema_version'] == '3'
+    assert 'file_set_type' in value and value['file_set_type'] == old_auxiliary_type
+    assert 'auxiliary_type' not in value
+
+
 def test_auxiliary_set_upgrade_3_4(upgrader, auxiliary_set_v3):
     value = upgrader.upgrade(
         'auxiliary_set', auxiliary_set_v3,
