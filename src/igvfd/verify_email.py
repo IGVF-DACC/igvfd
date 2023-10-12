5e04c1ec5a3ebd3bbf804d359267e9b1ced8b731
Otto Jolanki <ojolanki@stanford.edu>
2023-10-12 14:33:23 -0700

    fetch without @@object

--- src/igvfd/verify_email.py
+++ src/igvfd/verify_email.py
@@ -16,7 +16,7 @@ def includeme(config):
 def email_verification(context, request):
     if request.authenticated_userid:
         email = request.GET.get('email')
-        user_to_verify = request.embed(f'/users/{email}', '@@object')
+        user_to_verify = request.embed(f'/users/{email}')
         viewing_groups = user_to_verify.get('viewing_groups', [])
         verified_user = 'IGVF' in viewing_groups
         return {
