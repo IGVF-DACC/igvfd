2f421075e6d0a5e8ffd22326dfb9d9e5e48ec41d
Otto Jolanki <ojolanki@stanford.edu>
2023-10-12 15:04:23 -0700

    get @@raw

--- src/igvfd/verify_email.py
+++ src/igvfd/verify_email.py
@@ -17,7 +17,7 @@ def email_verification(context, request):
     if request.authenticated_userid:
         email = request.GET.get('email')
         try:
-            user_to_verify = request.embed(f'/users/{email}')
+            user_to_verify = request.embed(f'/users/{email}', '@@raw')
             viewing_groups = user_to_verify.get('viewing_groups', [])
             verified_user = 'IGVF' in viewing_groups
         except KeyError:
