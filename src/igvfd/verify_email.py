bb52eb43226a0b73a2f2ee62cff096cd4c27a81c
Otto Jolanki <ojolanki@stanford.edu>
2023-10-12 09:32:41 -0700

    test query parameter

--- src/igvfd/verify_email.py
+++ src/igvfd/verify_email.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_config
 from pyramid.security import NO_PERMISSION_REQUIRED
 
+from snovault.interfaces import STORAGE
+
 
 def includeme(config):
     config.scan(__name__, categories=None)
@@ -14,8 +16,12 @@ def includeme(config):
 )
 def email_verification(context, request):
     if request.authenticated_userid:
+        #session = request.registry[STORAGE].DBSession()
+        #statement = select(User).filter_by(email=email)
+        email = request.GET.get['email']
         return {'foo': 'bar',
-                'user': request.authenticated_userid
+                'user': request.authenticated_userid,
+                'email_to_check': email
                 }
     else:
         return {'no': 'way'}
