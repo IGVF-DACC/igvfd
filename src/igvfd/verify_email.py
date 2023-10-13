e6b235877f1d8f383315717d87da94ce1ad1d8dc
Otto Jolanki <ojolanki@stanford.edu>
2023-10-13 08:57:21 -0700

    check if permission is there in the request

--- src/igvfd/verify_email.py
+++ src/igvfd/verify_email.py
@@ -1,5 +1,7 @@
 from pyramid.httpexceptions import HTTPForbidden
 from pyramid.view import view_config
+from pyramid.security import Allowed
+from pyramid.security import Denied
 
 
 def includeme(config):
@@ -16,6 +18,8 @@ def email_verification(context, request):
     if request.authenticated_userid:
         email = request.GET.get('email')
         try:
+            has_view_permission = request.has_permission('view')
+            is_allowed = isinstance(has_view_permission, Allowed)
             user_to_verify = request.embed(f'/users/{email}', '@@object')
             viewing_groups = user_to_verify.get('viewing_groups', [])
             verified_user = 'IGVF' in viewing_groups
@@ -25,7 +29,9 @@ def email_verification(context, request):
             'email': email,
             'verified': verified_user,
             'viewing_groups': viewing_groups,
-            'user': user_to_verify
+            'has_view_permission': has_view_permission,
+            'is_allowed': is_allowed,
+            'user': user_to_verify,
         }
     else:
         raise HTTPForbidden('You are not authorized to perform email address verification.')
