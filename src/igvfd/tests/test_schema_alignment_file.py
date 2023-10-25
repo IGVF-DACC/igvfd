a077c8c40e9a742d0b6708ee209683000b061457
ian-whaling <whaling.ian@gmail.com>
2023-10-25 16:15:44 -0700

    test fixed

--- src/igvfd/tests/test_schema_alignment_file.py
+++ src/igvfd/tests/test_schema_alignment_file.py
@@ -3,6 +3,7 @@ import pytest
 
 # This tests the regex that disallows submission of any pattern. Remove this test once dbxrefs are added for alignment files.
 def test_restricted_dbxrefs(testapp, alignment_file):
-    testapp.patch_json(
+    res = testapp.patch_json(
         alignment_file['@id'],
         {'dbxrefs': ['']}, expect_errors=True)
+    assert res.status_code == 422
