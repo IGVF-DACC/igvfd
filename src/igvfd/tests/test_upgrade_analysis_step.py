4e5b7226f605e284764c424521384698ea9bdd00
Jennifer Jou <jjou@stanford.edu>
2023-10-30 16:58:12 -0700

    Remove extra line in test

--- src/igvfd/tests/test_upgrade_analysis_step.py
+++ src/igvfd/tests/test_upgrade_analysis_step.py
@@ -12,6 +12,5 @@ def test_analysis_step_upgrade_1_2(upgrader, analysis_step_v1):
 
 def test_analysis_step_upgrade_2_3(upgrader, analysis_step_v2):
     value = upgrader.upgrade('analysis_step', analysis_step_v2, current_version='2', target_version='3')
-    assert 'parents' not in value
     assert value['lab'] == '/labs/j-michael-cherry/'
     assert value['award'] == '/awards/HG012012/'
