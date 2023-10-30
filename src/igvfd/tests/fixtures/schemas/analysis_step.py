a1b388c834c10e96393352e24646e6b78464d71b
jenjou <42983888+jenjou@users.noreply.github.com>
2023-10-30 16:09:51 -0700

    IGVF-1212-minitems-analysis-step (#643)

--- src/igvfd/tests/fixtures/schemas/analysis_step.py
+++ src/igvfd/tests/fixtures/schemas/analysis_step.py
@@ -12,3 +12,16 @@ def analysis_step(testapp, base_workflow):
         'workflow': base_workflow['@id']
     }
     return testapp.post_json('/analysis_step', item).json['@graph'][0]
+
+
+@pytest.fixture
+def analysis_step_v1(analysis_step):
+    item = analysis_step.copy()
+    item.update({
+        'schema_version': '1',
+        'parents': [],
+        'input_content_types': [],
+        'output_content_types': [],
+        'analysis_step_types': [],
+    })
+    return item
