bd5fa8fb23ef1dc1b0a0575bd31953f5508442b0
Jin wook Lee <leepc12@gmail.com>
2023-11-01 17:30:36 -0700

    testing demo with subsampled databsae

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -60,7 +60,7 @@ config: Dict[str, Any] = {
                         'construct_id': 'Postgres',
                         'on': True,
                         'props': {
-                            'snapshot_source_db_identifier': 'test-wrn-395-10-31-2023',
+                            'snapshot_arn': 'arn:aws:rds:us-west-2:109189702753:snapshot:subsampled-wrn-395-11-01-2023',
                             'allocated_storage': 10,
                             'max_allocated_storage': 20,
                             'instance_type': InstanceType.of(
