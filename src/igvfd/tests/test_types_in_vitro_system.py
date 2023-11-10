b07e812580a45cbd76ba70a5ea88f0448b3bee3c
iyoungworth <iay@stanford.edu>
2023-11-10 12:23:06 -0500

    IGVF-1124-mention-cls-in-sample-summaries

--- src/igvfd/tests/test_types_in_vitro_system.py
+++ src/igvfd/tests/test_types_in_vitro_system.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_summary(testapp, in_vitro_cell_line, in_vitro_differentiated_cell, human_donor, rodent_donor, biomarker_CD243_absent, biomarker_CD243_high, sample_term_lymphoblastoid, sample_term_endothelial_cell, sample_term_embryoid_body, sample_term_brown_adipose_tissue, treatment_protein, depletion_treatment):
+def test_summary(testapp, in_vitro_cell_line, in_vitro_differentiated_cell, human_donor, rodent_donor, biomarker_CD243_absent, biomarker_CD243_high, sample_term_lymphoblastoid, sample_term_endothelial_cell, sample_term_embryoid_body, sample_term_brown_adipose_tissue, treatment_protein, depletion_treatment, construct_library_set_reporter):
     res = testapp.get(in_vitro_cell_line['@id'])
     assert res.json.get('summary') == 'K562 cell line, male, Mus musculus strain1'
     testapp.patch_json(
@@ -75,3 +75,28 @@ def test_summary(testapp, in_vitro_cell_line, in_vitro_differentiated_cell, huma
     res = testapp.get(in_vitro_cell_line['@id'])
     assert res.json.get(
         'summary') == f'virtual embryoid body induced to endothelial cell of vascular tree for 3 weeks (PKR-456), mixed sex, Homo sapiens and Mus musculus strain1 (sorting details: some detail about sorting) characterized by high level of CD243, negative detection of CD243, depleted of penicillin for 3 minutes, treated with 10 ng/mL G-CSF'
+    testapp.patch_json(
+        in_vitro_cell_line['@id'],
+        {
+            'donors': [human_donor['@id'], rodent_donor['@id']],
+            'time_post_change': 5,
+            'time_post_change_units': 'minute',
+            'cell_fate_change_treatments': [treatment_protein['@id']],
+            'targeted_sample_term': sample_term_brown_adipose_tissue['@id'],
+            'construct_library_sets': [construct_library_set_reporter['@id']]
+        }
+    )
+    res = testapp.get(in_vitro_cell_line['@id'])
+    assert res.json.get(
+        'summary') == 'virtual embryoid body induced to brown adipose tissue for 5 minutes (PKR-456), mixed sex, Homo sapiens and Mus musculus strain1 (sorting details: some detail about sorting) characterized by high level of CD243, negative detection of CD243, depleted of penicillin for 3 minutes, treated with 10 ng/mL G-CSF, modified with a reporter library'
+    testapp.patch_json(
+        in_vitro_differentiated_cell['@id'],
+        {
+            'moi': 2,
+            'nucleic_acid_delivery': 'transfection',
+            'construct_library_sets': [construct_library_set_reporter['@id']]
+        }
+    )
+    res = testapp.get(in_vitro_differentiated_cell['@id'])
+    assert res.json.get(
+        'summary') == 'K562 differentiated cell specimen induced to brown adipose tissue for 5 minutes, Homo sapiens transfected with a reporter library (MOI of 2)'
