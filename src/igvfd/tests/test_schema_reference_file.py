133d12da702f9bb2bf071e184431237c31e3d17e
Keenan Graham <ksgraham@stanford.edu>
2023-11-30 11:25:41 -0800

    Make consistent

--- src/igvfd/tests/test_schema_reference_file.py
+++ src/igvfd/tests/test_schema_reference_file.py
@@ -36,9 +36,9 @@ def test_transcriptome_annotation_dependency(testapp, reference_file):
 
 
 def test_schema_reference_file_validation_error_detail_only_allowed_when_upload_status_is_invalidated(testapp, reference_file):
-    # Given a file with upload_status: pending.
+    # Given a file with upload_status: pending:
     testapp.patch_json(reference_file['@id'], {'upload_status': 'pending'}, status=200)
-    # When validation_error_details added, then patch fails.
+    # When validation_error_details added, then patch fails:
     testapp.patch_json(
         reference_file['@id'],
         {
@@ -46,9 +46,9 @@ def test_schema_reference_file_validation_error_detail_only_allowed_when_upload_
         },
         status=422
     )
-    # Given a file with upload_status: invalidated.
+    # Given a file with upload_status: invalidated:
     testapp.patch_json(reference_file['@id'], {'upload_status': 'invalidated'}, status=200)
-    # When validation_error_details added, then patch works.
+    # When validation_error_details added, then patch works:
     testapp.patch_json(
         reference_file['@id'],
         {
@@ -96,9 +96,9 @@ def test_schema_reference_file_regeneratiing_upload_credentials_leads_to_valid_s
 
 
 def test_schema_reference_file_regenerating_upload_credentials_fails_if_upload_status_is_validated(testapp, reference_file):
-    # Given a file with upload_status: validated.
+    # Given a file with upload_status: validated:
     testapp.patch_json(reference_file['@id'], {'upload_status': 'validated', 'file_size': 123}, status=200)
-    # Then regenerating upload credentials fails.
+    # Then regenerating upload credentials fails:
     testapp.post_json(reference_file['@id'] + '@@upload', {}, status=403)
 
 
@@ -114,6 +114,6 @@ def test_schema_reference_file_regenerating_upload_credentials_on_invalid_file_c
     )
     # When credentials are regenerated:
     r = testapp.post_json(reference_file['@id'] + '@@upload', {}, status=200)
-    # Then upload_status is pending and validation_error_details are removed.
+    # Then upload_status is pending and validation_error_details are removed:
     assert r.json['@graph'][0]['upload_status'] == 'pending'
     assert 'validation_error_detail' not in r.json['@graph'][0]
