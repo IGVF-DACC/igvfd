e4fb5abf82c5b94e0c43fb54944a4f7ffbbecffb
Idan <gabdank@stanford.edu>
2023-11-30 08:58:16 -0800

    updating arn

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -60,7 +60,7 @@ config: Dict[str, Any] = {
                         'construct_id': 'Postgres',
                         'on': True,
                         'props': {
-                            'snapshot_source_db_identifier': DEV_DATABASE_IDENTIFIER,
+                            'snapshot_arn': 'arn:aws:rds:us-west-2:109189702753:snapshot:final-idan-snapshot',
                             'allocated_storage': 10,
                             'max_allocated_storage': 20,
                             'instance_type': InstanceType.of(
