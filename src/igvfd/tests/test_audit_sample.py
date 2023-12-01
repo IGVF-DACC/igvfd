35ac6562aaabfc2997b8d56bd189c4bbee54f75e
khine <khzlin@stanford.edu>
2023-12-01 15:07:43 -0800

    add test

--- src/igvfd/tests/test_audit_sample.py
+++ src/igvfd/tests/test_audit_sample.py
@@ -52,10 +52,24 @@ def test_audit_sample_virtual_donor_check(
             'donors': [human_donor['@id'], rodent_donor['@id']]
         }
     )
-    res = testapp.get(tissue['@id'] + '@@index-data')
+    tissue_res = testapp.get(tissue['@id'] + '@@index-data')
+    human_donor_res = testapp.get(human_donor['@id'] + '@@index-data')
+    rodent_donor_res = testapp.get(rodent_donor['@id'] + '@@index-data')
     assert any(
         error['category'] == 'inconsistent sample metadata'
-        for error in res.json['audit'].get('ERROR', [])
+        for error in tissue_res.json['audit'].get('ERROR', [])
+    )
+    tissue_link = '{' + tissue_res.json['object'].get('accession') + '|' + tissue_res.json['object'].get('@id') + '}'
+    human_donor_link = '{' + human_donor_res.json['object'].get(
+        'accession') + '|' + human_donor_res.json['object'].get('@id') + '}'
+    rodent_donor_link = '{' + rodent_donor_res.json['object'].get(
+        'accession') + '|' + rodent_donor_res.json['object'].get('@id') + '}'
+    assert any(
+        error['detail'] == f'The sample {tissue_link} is linked to virtual donor(s): {human_donor_link}, {rodent_donor_link}'
+        for error in tissue_res.json['audit'].get('ERROR', [])
+    ) or any(
+        error['detail'] == f'The sample {tissue_link} is linked to virtual donor(s): {rodent_donor_link}, {human_donor_link}'
+        for error in tissue_res.json['audit'].get('ERROR', [])
     )
 
 
