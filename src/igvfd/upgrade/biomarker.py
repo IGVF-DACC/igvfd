daed1955281d8b6ba4e2284daf9547103e97d594
Meenakshi Kagda <meenakshi@C02XR143JGH8.local>
2023-12-03 00:34:36 -0800

    upgrade biomarker

--- src/igvfd/upgrade/biomarker.py
+++ src/igvfd/upgrade/biomarker.py
@@ -5,3 +5,11 @@ from snovault import upgrade_step
 def biomarker_1_2(value, system):
     # https://igvf.atlassian.net/browse/IGVF-649
     value['status'] = 'in progress'
+
+
+@upgrade_step('biomarker', '2', '3')
+def biomarker_2_3(value, system):
+    # https://igvf.atlassian.net/browse/IGVF-1170
+    if 'description' in value:
+        if value['description'] == '':
+            del value['description']
