4debdc731d3276815c07bb3853dd325e48747cf6
Meenakshi Kagda <meenakshi@C02XR143JGH8.local>
2023-12-03 17:10:12 -0800

    test upgrade document

--- src/igvfd/tests/test_upgrade_document.py
+++ src/igvfd/tests/test_upgrade_document.py
@@ -6,3 +6,9 @@ def test_document_upgrade_1_2(upgrader, document_v1):
     assert 'urls' not in value
     assert 'aliases' not in value
     assert value['schema_version'] == '2'
+
+
+def test_document_upgrade_1_2(upgrader, document_v2):
+    value = upgrader.upgrade('document', document_v2, current_version='2', target_version='3')
+    assert value['description'] == 'Default upgrade text: please add details about document in description.'
+    assert value['schema_version'] == '3'
