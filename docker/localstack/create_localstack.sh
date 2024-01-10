914e87d652bf02150d30148b392ce72a77b0f758
Keenan Graham <ksgraham@stanford.edu>
2024-01-10 13:07:41 -0800

    Create local DLQs

--- docker/localstack/create_localstack.sh
+++ docker/localstack/create_localstack.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+echo 'Creating DLQ'
+
+TRANSACTION_DLQ_URL=$(awslocal sqs create-queue --queue-name transaction-dead-letter-queue --query 'QueueUrl' --output text)
+TRANSACTION_DLQ_ARN=$(awslocal sqs get-queue-attributes --queue-url $TRANSACTION_DLQ_URL --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
+
+echo $TRANSACTION_DLQ_URL
+
+INVALIDATION_DLQ_URL=$(awslocal sqs create-queue --queue-name invalidation-dead-letter-queue --query 'QueueUrl' --output text)
+INVALIDATION_DLQ_ARN=$(awslocal sqs get-queue-attributes --queue-url $INVALIDATION_DLQ_URL --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
+
+echo $INVALIDATION_DLQ_URL
+
 echo 'Creating SQS'
 
-awslocal sqs create-queue --queue-name transaction-queue --attributes VisibilityTimeout=60
-awslocal sqs create-queue --queue-name invalidation-queue --attributes VisibilityTimeout=120
-awslocal s3api create-bucket --bucket igvf-files-local --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+awslocal sqs create-queue --queue-name transaction-queue --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"'$TRANSACTION_DLQ_ARN'\",\"maxReceiveCount\":\"3\"}", "VisibilityTimeout": "60"}'
+awslocal sqs create-queue --queue-name invalidation-queue --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"'$INVALIDATION_DLQ_ARN'\",\"maxReceiveCount\":\"3\"}", "VisibilityTimeout": "120"}'
