a0dfe925d3e023bf9d5fb8acf9629e552b0e92d9
jenjou <42983888+jenjou@users.noreply.github.com>
2024-02-05 09:45:45 -0800

    IGVF-1434-remove-nih-audit (#766)

--- src/igvfd/tests/test_audit_biosample.py
+++ src/igvfd/tests/test_audit_biosample.py
@@ -1,42 +1,6 @@
 import pytest
 
 
-def test_audit_biosample_nih_institutional_certification(
-    testapp,
-    primary_cell,
-    human_donor,
-    rodent_donor,
-):
-    res = testapp.get(primary_cell['@id'] + '@@audit')
-    assert any(
-        error['category'] == 'missing nih_institutional_certification'
-        for error in res.json['audit'].get('ERROR', [])
-    )
-    testapp.patch_json(
-        primary_cell['@id'],
-        {
-            'donors': [human_donor['@id'],
-                       rodent_donor['@id']]
-        }
-    )
-    res = testapp.get(primary_cell['@id'] + '@@audit')
-    assert any(
-        error['category'] == 'missing nih_institutional_certification'
-        for error in res.json['audit'].get('ERROR', [])
-    )
-    testapp.patch_json(
-        primary_cell['@id'],
-        {
-            'nih_institutional_certification': 'NIC00042'
-        }
-    )
-    res = testapp.get(primary_cell['@id'] + '@@audit')
-    assert all(
-        error['category'] != 'missing nih_institutional_certification'
-        for error in res.json['audit'].get('ERROR', [])
-    )
-
-
 def test_audit_biosample_taxa_check(testapp, tissue, rodent_donor, human_donor, human_male_donor):
     '''A Biosample that has donors of different taxa should not have calculated taxa property and trigger audit.'''
 
