0d2bb73fc9f6c01ffcbf5b412179e8fcc645be04
khine <khzlin@stanford.edu>
2024-03-22 10:14:23 -0700

    take out calc property

--- src/igvfd/types/open_reading_frame.py
+++ src/igvfd/types/open_reading_frame.py
@@ -1,11 +1,9 @@
 from snovault import (
-    calculated_property,
     collection,
     load_schema,
 )
 from .base import (
-    Item,
-    paths_filtered_by_status
+    Item
 )
 from snovault.util import Path
 
@@ -25,22 +23,3 @@ class OpenReadingFrame(Item):
     embedded_with_frame = [
         Path('gene', include=['@id', 'symbol']),
     ]
-    rev = {
-        'gene_symbol': ('Gene', 'symbol')
-    }
-
-    @calculated_property(schema={
-        'title': 'Gene Symbol',
-        'description': 'Gene symbol approved by the official nomenclature.',
-        'type': 'array',
-        'uniqueItems': True,
-        'minItems': 1,
-        'items': {
-            'title': 'Gene Symbol',
-            'type': ['array', 'object'],
-            'linkFrom': 'Gene.symbol',
-        },
-        'notSubmittable': True
-    })
-    def gene_symbol(self, request, gene_symbol):
-        return paths_filtered_by_status(request, gene_symbol)
