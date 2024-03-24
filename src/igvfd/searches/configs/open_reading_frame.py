311bc5194d230e40b4a0047e64e36fa193e07052
khine <khzlin@stanford.edu>
2024-03-23 21:17:42 -0700

    fix mapping

--- src/igvfd/searches/configs/open_reading_frame.py
+++ src/igvfd/searches/configs/open_reading_frame.py
@@ -7,24 +7,9 @@ from snovault.elasticsearch.searches.configs import search_config
 def open_reading_frame():
     return {
         'facets': {
-            'gene': {
-                'title': 'Gene'
-            },
             'status': {
                 'title': 'Status'
-            },
-            'audit.ERROR.category': {
-                'title': 'Audit Category: Error'
-            },
-            'audit.NOT_COMPLIANT.category': {
-                'title': 'Audit Category: Not Compliant'
-            },
-            'audit.WARNING.category': {
-                'title': 'Audit Category: Warning'
-            },
-            'audit.INTERNAL_ACTION.category': {
-                'title': 'Audit Category: Internal Action'
-            },
+            }
         },
         'columns': {
             'ord_id': {
