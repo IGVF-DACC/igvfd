19884376705098f9c0ea844a738410da67cbb950
Idan <gabdank@stanford.edu>
2024-04-09 10:16:03 -0700

    added missing audit documentation

--- src/igvfd/audit/file.py
+++ src/igvfd/audit/file.py
@@ -6,13 +6,22 @@ from snovault.auditor import (
 
 @audit_checker('File', frame='object')
 def audit_file_controlled_access_file_in_correct_anvil_workspace(value, system):
+    '''
+    [
+        {
+            "audit_description": "All controlled access files have to be moved from lab submission AnVIL workspace to DACC AnVIL workspace.",
+            "audit_category": "incorrect anvil workspace",
+            "audit_level": "INTERNAL_ACTION"
+        }
+    ]
+    '''
     if value.get('controlled_access', False) is False:
         return
     if value.get('upload_status') != 'pending':
         return
     detail = (
         f'Move controlled-access file {value["@id"]} '
-        f'from submission Anvil workspace to protected Anvil workspace. '
+        f'from submission AnVIL workspace to protected AnVIL workspace. '
         f'Source={value["anvil_source_url"]} '
         f'Destination={value["anvil_destination_url"]}'
     )
