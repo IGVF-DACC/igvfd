d2d4d33f9528143f6c5f5fac386e9113172f73fe
ian-whaling <whaling.ian@gmail.com>
2024-05-01 13:59:26 -0700

    audit created

--- src/igvfd/audit/analysis_set.py
+++ src/igvfd/audit/analysis_set.py
@@ -77,3 +77,57 @@ def audit_input_file_sets_derived_from(value, system):
             f'which are not in `input_file_sets`.'
         )
         yield AuditFailure('missing input file set', f'{detail} {description}', level='ERROR')
+
+
+@audit_checker('AnalysisSet', frame='object')
+def audit_analysis_set_samples(value, system):
+    '''
+    [
+        {
+            "audit_description": "With the exception of multiplexed data, analysis sets are expected to specify all the samples from its input file sets.",
+            "audit_category": "missing samples",
+            "audit_level": "WARNING"
+        },
+        {
+            "audit_description": "With the exception of multiplexed data, analysis sets are expected to specify only the samples from its input file sets.",
+            "audit_category": "unexpected samples",
+            "audit_level": "WARNING"
+        }
+    ]
+    '''
+    missing_description = get_audit_description(audit_analysis_set_samples, index=0)
+    unexpected_description = get_audit_description(audit_analysis_set_samples, index=1)
+    detail = ''
+    input_file_sets = value.get('input_file_sets')
+    samples = value.get('samples')
+    if input_file_sets:
+        if samples:
+            input_file_sets_samples = []
+            for input_file_set in input_file_sets:
+                input_file_set_object = system.get('request').embed(input_file_set + '@@object?skip_calculated=true')
+                input_file_sets_samples.append(input_file_set_object.get('samples'))
+            if not([input_file_sets_sample for input_file_sets_sample in input_file_sets_samples if input_file_sets_sample.startswith('/multiplexed-samples/')]):
+                if set(samples).issubset(set(input_file_sets_samples)) and set(samples) != set(input_file_sets_samples):
+                    missing_samples = list(set(input_file_sets_samples) - set(samples))
+                    missing_samples = ', '.join([audit_link(path_to_text(missing_sample), missing_sample)
+                                                for missing_sample in missing_samples])
+                    detail = (
+                        f'Analysis set {audit_link(path_to_text(value["@id"]),value["@id"])} '
+                        f'does not specify samples {missing_samples} from its `input_file_sets`. '
+                    )
+                    yield AuditFailure('missing samples', f'{detail} {missing_description}', level='WARNING')
+                if (set(samples) - set(input_file_sets_samples)):
+                    unexpected_samples = list(set(input_file_sets_samples) - set(samples))
+                    unexpected_samples = ', '.join(
+                        [audit_link(path_to_text(unexpected_sample), unexpected_sample) for unexpected_sample in unexpected_samples])
+                    detail = (
+                        f'Analysis set {audit_link(path_to_text(value["@id"]),value["@id"])} '
+                        f'specifies unexpected samples {unexpected_samples} not from its `input_file_sets`.'
+                    )
+                    yield AuditFailure('unexpected samples', f'{detail} {unexpected_description}', level='WARNING')
+        else:
+            detail = (
+                f'Analysis set {audit_link(path_to_text(value["@id"]),value["@id"])} '
+                f'has `input_file_sets`, but no `samples`.'
+            )
+            yield AuditFailure('missing samples', f'{detail} {missing_description}', level='WARNING')
