93dfe6ec363722cb21c811ac25f4e7c6050236ba
ian-whaling <whaling.ian@gmail.com>
2024-05-03 15:50:13 -0700

    test fixed

--- src/igvfd/tests/test_audit_in_vitro_system.py
+++ src/igvfd/tests/test_audit_in_vitro_system.py
@@ -4,12 +4,17 @@ import pytest
 def test_audit_targeted_sample_term(
     testapp,
     in_vitro_cell_line,
-    sample_term_K562
+    sample_term_K562,
+    treatment_chemical
 ):
     # In vitro systems should not have the same sample_term specified in targeted_sample_term and sample_terms.
     testapp.patch_json(
         in_vitro_cell_line['@id'],
-        {'targeted_sample_term': sample_term_K562['@id']}
+        {'targeted_sample_term': sample_term_K562['@id'],
+         'cell_fate_change_treatments': [treatment_chemical['@id']],
+         'time_post_change': 10,
+         'time_post_change_units': 'minute',
+         'classifications': ['differentiated cell specimen']}
     )
     res = testapp.get(in_vitro_cell_line['@id'] + '@@audit')
     assert any(
