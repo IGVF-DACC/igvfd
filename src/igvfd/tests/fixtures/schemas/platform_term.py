4537be0abdf231ef1ce4f1a89ca4b19756bf19d6
jenjou <42983888+jenjou@users.noreply.github.com>
2024-05-13 10:02:04 -0700

    IGVF-1629-seq-kit-typo (#894)

--- src/igvfd/tests/fixtures/schemas/platform_term.py
+++ src/igvfd/tests/fixtures/schemas/platform_term.py
@@ -18,7 +18,7 @@ def platform_term_NovaSeq(testapp):
         'sequencing_kits': ['NovaSeq 6000 SP Reagent Kit v1.5',
                             'NovaSeq 6000 S1 Reagent Kit v1.5',
                             'NovaSeq 6000 S2 Reagent Kit v1.5',
-                            'NovaSeq 6000 S4 Reagent Kit V1.5']
+                            'NovaSeq 6000 S4 Reagent Kit v1.5']
     }
     return testapp.post_json('/platform_term', item, status=201).json['@graph'][0]
 
@@ -29,5 +29,15 @@ def platform_term_v1(platform_term_HiSeq):
     item.update({
         'schema_version': '1',
         'description': ''
+    })
+    return item
+
+
+@pytest.fixture
+def platform_term_v3(platform_term_HiSeq):
+    item = platform_term_HiSeq.copy()
+    item.update({
+        'schema_version': '3',
+        'sequencing_kits': ['NovaSeq 6000 S4 Reagent Kit V1.5']
     })
     return item
--- src/igvfd/tests/test_upgrade_platform_term.py
+++ src/igvfd/tests/test_upgrade_platform_term.py
@@ -5,3 +5,10 @@ def test_assay_term_upgrade_1_2(upgrader, platform_term_v1):
     value = upgrader.upgrade('platform_term', platform_term_v1, current_version='1', target_version='2')
     assert 'description' not in value
     assert value['schema_version'] == '2'
+
+
+def test_assay_term_upgrade_3_4(upgrader, platform_term_v3):
+    value = upgrader.upgrade('platform_term', platform_term_v3, current_version='3', target_version='4')
+    assert 'NovaSeq 6000 S4 Reagent Kit V1.5' not in value['sequencing_kits']
+    assert 'NovaSeq 6000 S4 Reagent Kit v1.5' in value['sequencing_kits']
+    assert value['schema_version'] == '4'
