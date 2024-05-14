d83b74af823f24caebba8a2ea37a2a0f29d16f2e
Shengcheng Dong <shengchd@umich.edu>
2024-05-13 17:16:18 -0700

    IGVF-1108-audit-biomarker-name (#898)

--- src/igvfd/tests/test_audit_biosample.py
+++ src/igvfd/tests/test_audit_biosample.py
@@ -31,3 +31,25 @@ def test_audit_biosample_taxa_check(testapp, tissue, rodent_donor, human_donor, 
         error['category'] != 'inconsistent donor taxa'
         for error in res.json['audit'].get('ERROR', [])
     )
+
+
+def test_audit_biomarker_name(testapp, primary_cell, biomarker_CD243_absent, biomarker_CD243_high, biomarker_CD1e_low):
+    testapp.patch_json(primary_cell['@id'],
+                       {'biomarkers': [biomarker_CD243_absent['@id'],
+                                       biomarker_CD243_high['@id'],
+                                       biomarker_CD1e_low['@id']]}
+                       )
+    res = testapp.get(primary_cell['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'inconsistent biomarkers'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(primary_cell['@id'],
+                       {'biomarkers': [biomarker_CD243_high['@id'],
+                                       biomarker_CD1e_low['@id']]}
+                       )
+    res = testapp.get(primary_cell['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'inconsistent biomarkers'
+        for error in res.json['audit'].get('ERROR', [])
+    )
