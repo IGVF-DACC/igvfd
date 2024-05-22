f05d0fd67deaad432c1bf5d5507aaf3624a3a6be
ian-whaling <whaling.ian@gmail.com>
2024-05-22 10:17:33 -0700

    audit adjusted

--- src/igvfd/audit/file.py
+++ src/igvfd/audit/file.py
@@ -46,16 +46,22 @@ def audit_upload_status(value, system):
             "audit_description": "Files are expected to be validated or deposited.",
             "audit_category": "unvalidated upload status",
             "audit_level": "ERROR"
+        },
+        {
+            "audit_description": "External files are expected to be validated or deposited.",
+            "audit_category": "unvalidated upload status",
+            "audit_level": "WARNING"
         }
     ]
     '''
-    description = get_audit_description(audit_upload_status)
     upload_status = value.get('upload_status')
     if upload_status not in ['validated', 'deposited']:
         if value.get('external'):
             audit_level = 'WARNING'
+            description = get_audit_description(audit_upload_status, index=1)
         else:
             audit_level = 'ERROR'
+            description = get_audit_description(audit_upload_status, index=0)
         detail = (
             f'File {audit_link(path_to_text(value["@id"]), value["@id"])} has `upload_status` {upload_status}.'
         )
