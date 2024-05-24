92ad5b294e1aac39ef2a6e440e97c2dfbb515f9d
ian-whaling <whaling.ian@gmail.com>
2024-05-23 18:45:33 -0700

    audit created

--- src/igvfd/audit/analysis_set.py
+++ src/igvfd/audit/analysis_set.py
@@ -17,19 +17,67 @@ def audit_input_file_sets(value, system):
             "audit_description": "Primary analysis sets are expected to have at least one measurement set as an input file set.",
             "audit_category": "missing measurement set",
             "audit_level": "WARNING"
+        },
+        {
+            "audit_description": "Analysis sets with input measurement sets are expected to also specify the associated construct library sets as inputs.",
+            "audit_category": "missing construct library set",
+            "audit_level": "WARNING"
+        },
+        {
+            "audit_description": "Analysis sets with input measurement sets are expected to also specify the associated auxiliary sets as inputs.",
+            "audit_category": "missing auxiliary set",
+            "audit_level": "WARNING"
         }
     ]
     '''
-    description = get_audit_description(audit_input_file_sets)
+    description_measurement_set = get_audit_description(audit_input_file_sets, index=0)
+    description_construct_libray_set = get_audit_description(audit_input_file_sets, index=1)
+    description_auxiliary_set = get_audit_description(audit_input_file_sets, index=2)
     detail = ''
-    if value.get('file_set_type') == 'primary analysis':
-        if not(any(file_set.startswith('/measurement-sets/') for file_set in value['input_file_sets'])):
+    input_file_sets = value.get('input_file_sets')
+    input_measurement_sets = [file_set.startswith('/measurement-sets/') for file_set in input_file_sets]
+    if not(input_measurement_sets):
+        if value.get('file_set_type') == 'primary analysis':
             detail = (
                 f'Analysis set {audit_link(path_to_text(value["@id"]),value["@id"])} '
                 f'is a primary analysis, but does not specify any measurement sets in '
                 f'`input_file_sets`.'
             )
-            yield AuditFailure('missing measurement set', f'{detail} {description}', level='WARNING')
+            yield AuditFailure('missing measurement set', f'{detail} {description_measurement_set}', level='WARNING')
+    else:
+        missing_construct_library_sets = []
+        missing_auxiliary_sets = []
+        for measurement_set in input_measurement_sets:
+            measurement_set_object = system.get('request').embed(measurement_set + '@@embedded?skip_calculated=true')
+            samples = measurement_set_object.get('samples')
+            for sample in samples:
+                construct_library_sets = sample.get('construct_library_sets')
+                for construct_library_set in construct_library_sets:
+                    if construct_library_set not in missing_construct_library_sets and construct_library_set not in input_file_sets:
+                        missing_construct_library_sets.append(construct_library_set)
+            auxiliary_sets = measurement_set_object.get('auxiliary_sets')
+            auxiliary_sets = [auxiliary_set['@id'] for auxiliary_set in auxiliary_sets]
+            for auxiliary_set in auxiliary_sets:
+                if auxiliary_set not in missing_auxiliary_sets and auxiliary_set not in input_file_sets:
+                    missing_auxiliary_sets.append(auxiliary_set)
+        if missing_construct_library_sets:
+            missing_construct_library_sets = ', '.join([audit_link(path_to_text(
+                construct_library_set), construct_library_set) for construct_library_set in missing_construct_library_sets])
+            detail = (
+                f'Analysis set {audit_link(path_to_text(value["@id"]),value["@id"])} '
+                f'does not link to construct library set(s) in `input_file_sets`: {construct_library_sets} '
+                f'that are associated with its input measurement sets. '
+            )
+            yield AuditFailure('missing construct library set', f'{detail} {description_construct_libray_set}', level='WARNING')
+        if missing_auxiliary_sets:
+            missing_auxiliary_sets = ', '.join(
+                [audit_link(path_to_text(auxiliary_set), auxiliary_set) for auxiliary_set in missing_auxiliary_sets])
+            detail = (
+                f'Analysis set {audit_link(path_to_text(value["@id"]),value["@id"])} '
+                f'does not link to auxiliary set(s) in `input_file_sets`: {missing_auxiliary_sets} '
+                f'that are associated with its input measurement sets. '
+            )
+            yield AuditFailure('missing auxiliary set', f'{detail} {description_auxiliary_set}', level='WARNING')
 
 
 @audit_checker('AnalysisSet', frame='object')
