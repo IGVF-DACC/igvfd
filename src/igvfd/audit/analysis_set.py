ea1c01c987a4475d7d30e7e0125dd27cd7013258
ian-whaling <whaling.ian@gmail.com>
2024-06-04 12:26:44 -0700

    removed audit & test

--- src/igvfd/audit/analysis_set.py
+++ src/igvfd/audit/analysis_set.py
@@ -10,29 +10,6 @@ from .formatter import (
 
 
 @audit_checker('AnalysisSet', frame='object')
-def audit_input_file_sets(value, system):
-    '''
-    [
-        {
-            "audit_description": "Primary analysis sets are expected to have at least one measurement set as an input file set.",
-            "audit_category": "missing measurement set",
-            "audit_level": "WARNING"
-        }
-    ]
-    '''
-    description = get_audit_description(audit_input_file_sets)
-    detail = ''
-    if value.get('file_set_type') == 'primary analysis':
-        if not(any(file_set.startswith('/measurement-sets/') for file_set in value['input_file_sets'])):
-            detail = (
-                f'Analysis set {audit_link(path_to_text(value["@id"]),value["@id"])} '
-                f'is a primary analysis, but does not specify any measurement sets in '
-                f'`input_file_sets`.'
-            )
-            yield AuditFailure('missing measurement set', f'{detail} {description}', level='WARNING')
-
-
-@audit_checker('AnalysisSet', frame='object')
 def audit_input_file_sets_derived_from(value, system):
     '''
     [
