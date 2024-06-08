be2b35d92d6fe97459d33050f2a718bdb6b6ef8d
Idan <gabdank@stanford.edu>
2024-06-07 17:20:08 -0700

    adding upgrade with test

--- src/igvfd/tests/test_upgrade_analysis_set.py
+++ src/igvfd/tests/test_upgrade_analysis_set.py
@@ -26,3 +26,10 @@ def test_analysis_set_upgrade_4_5(upgrader, analysis_set_v4):
     value = upgrader.upgrade('analysis_set', analysis_set_v4, current_version='4', target_version='5')
     assert 'description' not in value
     assert value['schema_version'] == '5'
+
+
+def test_analysis_set_upgrade_6_7(upgrader, analysis_set_v6):
+    value = upgrader.upgrade('analysis_set', analysis_set_v6, current_version='6', target_version='7')
+    assert 'file_set_type' == 'principal analysis'
+    assert value['schema_version'] == '7'
+    assert value['notes'].endswith('file_set_type was primary analysis and has been updated to be principal analysis.')
