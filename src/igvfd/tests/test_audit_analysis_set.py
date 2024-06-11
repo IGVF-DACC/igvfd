f81ffcae371162d6c51337a4a7a6fa00059c9691
Ian Whaling <78115078+ian-whaling@users.noreply.github.com>
2024-06-11 07:44:08 -0700

    IGVF-1675-remove-analysis-audit (#934)

--- src/igvfd/tests/test_audit_analysis_set.py
+++ src/igvfd/tests/test_audit_analysis_set.py
@@ -1,37 +1,6 @@
 import pytest
 
 
-def test_audit_inconsistent_input_file_sets(
-    testapp,
-    analysis_set_base,
-    curated_set_transcriptome,
-    measurement_set
-):
-    testapp.patch_json(
-        analysis_set_base['@id'],
-        {
-            'file_set_type': 'primary analysis',
-            'input_file_sets': [curated_set_transcriptome['@id']]
-        }
-    )
-    res = testapp.get(analysis_set_base['@id'] + '@@audit')
-    assert any(
-        error['category'] == 'missing measurement set'
-        for error in res.json['audit'].get('WARNING', [])
-    )
-    testapp.patch_json(
-        analysis_set_base['@id'],
-        {
-            'input_file_sets': [curated_set_transcriptome['@id'], measurement_set['@id']]
-        }
-    )
-    res = testapp.get(analysis_set_base['@id'] + '@@audit')
-    assert all(
-        error['category'] != 'missing measurement set'
-        for error in res.json['audit'].get('WARNING', [])
-    )
-
-
 def test_audit_missing_input_file_set(
     testapp,
     analysis_set_base,
