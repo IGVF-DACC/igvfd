3ebe4e1e004b55b9257e9a3c204c93886bf811b9
khine <khzlin@stanford.edu>
2024-06-18 14:55:09 -0700

    rewrite test

--- src/igvfd/upgrade/crispr_modification.py
+++ src/igvfd/upgrade/crispr_modification.py
@@ -8,16 +8,17 @@ def crispr_modification_1_2(value, system):
         'ZIM3': 'ZIM3-KRAB',
         'VPR': 'VP64-p65-Rta (VPR)',
     }
+    sentence_end = ''
     if 'fused_domain' in value:
         if value['fused_domain'] == 'KRAB':
             old_fused_domain = 'KRAB'
             value.pop('fused_domain')
             sentence_end = 'removed'
-        else:
+        elif value['fused_domain'] in old_to_new:
             old_fused_domain = value['fused_domain']
             sentence_end = f'renamed to be {old_to_new[old_fused_domain]}'
-            if old_fused_domain in old_to_new:
-                value['fused_domain'] = old_to_new[old_fused_domain]
+            value['fused_domain'] = old_to_new[old_fused_domain]
+    if sentence_end != '':
         notes = value.get('notes', '')
         notes += f'Fused_domain enum {old_fused_domain} has been {sentence_end}.'
         value['notes'] = notes.strip()
