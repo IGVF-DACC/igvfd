f33a10f0fe5c9ea408ba1c021d18b5ea62aa6294
Meenakshi Kagda <meenakshi@C02XR143JGH8.local>
2024-06-25 00:23:41 -0700

    add test for treatment

--- src/igvfd/tests/test_types_treatment.py
+++ src/igvfd/tests/test_types_treatment.py
@@ -8,3 +8,14 @@ def test_treatment_summary(treatment_chemical, treatment_protein, depletion_trea
     assert res.json['summary'] == 'Treatment of 10 ng/mL G-CSF'
     res = testapp.get(depletion_treatment['@id'])
     assert res.json['summary'] == 'Depletion of penicillin for 3 minutes'
+
+
+def test_samples_treated(testapp, in_vitro_system_v20, treatment_v6):
+    testapp.patch_json(
+        in_vitro_system_v20['@id'],
+        {
+            'treatments': [treatment_v6['@id']]
+        }
+    )
+    res = testapp.get(treatment_v6['@id'])
+    assert res.json.get('samples_treated', []) == [in_vitro_system_v20['@id']]
