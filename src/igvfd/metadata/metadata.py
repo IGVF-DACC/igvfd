07ae5561dd022404a1b1ce07ffdb1c642cc0d3f0
Keenan Graham <ksgraham@stanford.edu>
2024-07-03 11:50:51 -0700

    Filter files without href

--- src/igvfd/metadata/metadata.py
+++ src/igvfd/metadata/metadata.py
@@ -225,6 +225,7 @@ class MetadataReport:
         conditions = [
             not file_matches_file_params(file_, self.positive_file_param_set),
             not file_satisfies_inequality_constraints(file_, self.positive_file_inequalities),
+            'href' not in file_,
         ]
         return any(conditions)
 
