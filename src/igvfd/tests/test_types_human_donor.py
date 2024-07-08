26d63c9e96d69d66ffe3ceedbe81d70f9f789ac6
Jennifer Jou <jjou@stanford.edu>
2024-07-08 10:43:00 -0700

    fix test

--- src/igvfd/tests/test_types_human_donor.py
+++ src/igvfd/tests/test_types_human_donor.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_human_donor_summary(testapp, human_donor):
     res = testapp.get(human_donor['@id'])
-    assert res.json.get('summary') == 'unspecified'
+    assert res.json.get('summary') == res.json.get('uuid')
 
     testapp.patch_json(
         human_donor['@id'],
