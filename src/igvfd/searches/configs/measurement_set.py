9140f7aa6120c7e9fb79824279b34686d48f0d0f
Jennifer Jou <jjou@stanford.edu>
2024-07-08 18:22:39 -0700

    Change from using gene name to symbol in facet

--- src/igvfd/searches/configs/measurement_set.py
+++ src/igvfd/searches/configs/measurement_set.py
@@ -40,7 +40,7 @@ def measurement_set():
             'sequencing_library_types': {
                 'title': 'Library Material'
             },
-            'targeted_genes.name': {
+            'targeted_genes.symbol': {
                 'title': 'Assay Targeted Genes'
             },
             'collections': {
@@ -92,7 +92,7 @@ def measurement_set():
                     'preferred_assay_title',
                     'samples.modifications.modality',
                     'file_set_type',
-                    'targeted_genes.name'
+                    'targeted_genes.symbol'
                 ],
             },
             {
@@ -163,7 +163,7 @@ def measurement_set():
             'sequencing_library_types': {
                 'title': 'Sequencing Library Types'
             },
-            'targeted_genes.name': {
+            'targeted_genes.symbol': {
                 'title': 'Assay Targeted Genes'
             },
             'protocols': {
