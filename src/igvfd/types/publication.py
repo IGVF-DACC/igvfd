e31568c3ad8c056d561b78d66571375460badd3c
ian-whaling <whaling.ian@gmail.com>
2024-07-09 21:12:23 -0700

    mappings

--- src/igvfd/types/publication.py
+++ src/igvfd/types/publication.py
@@ -1,12 +1,13 @@
 from snovault import (
     collection,
     load_schema,
-    calculated_property
+    calculated_property,
 )
 from snovault.util import Path
 from .base import (
     Item,
-    datetime
+    datetime,
+    paths_filtered_by_status
 )
 
 
@@ -26,6 +27,12 @@ class Publication(Item):
         Path('lab', include=['@id', 'title']),
         Path('submitted_by', include=['@id', 'title']),
     ]
+
+    rev = {
+        'samples': ('Sample', 'publications'),
+        'donors': ('Donor', 'publications'),
+        'file_sets': ('FileSet', 'publications')
+    }
 
     set_status_up = []
     set_status_down = []
@@ -48,3 +55,51 @@ class Publication(Item):
     def publication_year(self, date_published):
         year = datetime.strptime(date_published, '%Y-%m-%d').year
         return year
+
+    @calculated_property(schema={
+        'title': 'Samples',
+        'type': 'array',
+        'description': 'The samples associated with this publication.',
+        'minItems': 1,
+        'uniqueItems': True,
+        'items': {
+            'title': 'Sample',
+            'type': ['string', 'object'],
+            'linkFrom': 'Sample.publications',
+        },
+        'notSubmittable': True
+    })
+    def samples(self, request, samples):
+        return paths_filtered_by_status(request, samples)
+
+    @calculated_property(schema={
+        'title': 'Donors',
+        'type': 'array',
+        'description': 'The donors associated with this publication.',
+        'minItems': 1,
+        'uniqueItems': True,
+        'items': {
+            'title': 'Donor',
+            'type': ['string', 'object'],
+            'linkFrom': 'Donor.publications',
+        },
+        'notSubmittable': True
+    })
+    def donors(self, request, donors):
+        return paths_filtered_by_status(request, donors)
+
+    @calculated_property(schema={
+        'title': 'File Sets',
+        'type': 'array',
+        'description': 'The file sets associated with this publication.',
+        'minItems': 1,
+        'uniqueItems': True,
+        'items': {
+            'title': 'FileSet',
+            'type': ['string', 'object'],
+            'linkFrom': 'FileSet.publications',
+        },
+        'notSubmittable': True
+    })
+    def file_sets(self, request, file_sets):
+        return paths_filtered_by_status(request, file_sets)
