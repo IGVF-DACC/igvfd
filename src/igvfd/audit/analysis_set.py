e77bf6b025aba2c9f75ccbd24ff236d1b3b5d475
Ian Whaling <78115078+ian-whaling@users.noreply.github.com>
2024-07-16 10:13:18 -0700

    IGVF-1787-audit-no-step-version (#990)

--- src/igvfd/audit/analysis_set.py
+++ src/igvfd/audit/analysis_set.py
@@ -146,3 +146,30 @@ def audit_analysis_set_samples(value, system):
                 f'has `input_file_sets`, but no `samples`.'
             )
             yield AuditFailure('missing samples', f'{detail} {missing_description}', level='WARNING')
+
+
+@audit_checker('AnalysisSet', frame='object')
+def audit_analysis_set_files_missing_analysis_step_version(value, system):
+    '''
+    [
+        {
+            "audit_description": "Analysis set files are expected to specify analysis step version.",
+            "audit_category": "missing analysis step version",
+            "audit_level": "WARNING"
+        }
+    ]
+    '''
+    description = get_audit_description(audit_analysis_set_files_missing_analysis_step_version, index=0)
+    files = value.get('files')
+    files_with_missing_asv = []
+    for file in files:
+        file_object = system.get('request').embed(file + '@@object?skip_calculated=true')
+        if not (file_object.get('analysis_step_version', '')):
+            files_with_missing_asv.append(file)
+    if files_with_missing_asv:
+        files_with_missing_asv = ', '.join([audit_link(path_to_text(file), file) for file in files_with_missing_asv])
+        detail = (
+            f'Analysis set {audit_link(path_to_text(value["@id"]),value["@id"])} '
+            f'links to file(s) {files_with_missing_asv} that are missing `analysis_step_version`.'
+        )
+        yield AuditFailure('missing analysis step version', f'{detail} {description}', level='WARNING')
