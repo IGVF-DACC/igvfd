5f168af85aa5677a3e4396bc1be3aad39b04af03
Keenan Graham <ksgraham@stanford.edu>
2024-07-30 13:21:51 -0700

    Spin up from prod data

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -60,7 +60,7 @@ config: Dict[str, Any] = {
                         'construct_id': 'Postgres',
                         'on': True,
                         'props': {
-                            'snapshot_source_db_identifier': DEV_DATABASE_IDENTIFIER,
+                            'snapshot_arn': 'arn:aws:rds:us-west-2:035226225042:snapshot:igvfd-main-productiondeploystage-postgres97b73533-ikxchjbjh9yk-2024-07-30-13-45',
                             'allocated_storage': 10,
                             'max_allocated_storage': 20,
                             'instance_type': InstanceType.of(
