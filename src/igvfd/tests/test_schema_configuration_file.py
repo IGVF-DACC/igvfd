6d3ba239a7f8832642600818d942fb9878dd9e11
ian-whaling <whaling.ian@gmail.com>
2024-07-31 10:28:14 -0700

    dependency + test

--- src/igvfd/tests/test_schema_configuration_file.py
+++ src/igvfd/tests/test_schema_configuration_file.py
@@ -9,5 +9,13 @@ def test_seqspec_of_dependency(testapp, configuration_file_json, sequence_file):
     res = testapp.patch_json(
         configuration_file_json['@id'],
         {'content_type': 'seqspec',
-         'seqspec_of': [sequence_file['@id']]})
+         'seqspec_of': [sequence_file['@id']],
+         'file_format': 'yaml'})
     assert res.status_code == 200
+
+
+def test_seqspec_file_format_depdendency(testapp, configuration_file_seqspec):
+    res = testapp.patch_json(
+        configuration_file_seqspec['@id'],
+        {'file_format': 'json'}, expect_errors=True)
+    assert res.status_code == 422
