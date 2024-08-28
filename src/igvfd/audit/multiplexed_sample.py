ea5c03271e7185db890aefc50849b05d1b88cfdc
ian-whaling <whaling.ian@gmail.com>
2024-08-28 10:57:16 -0700

    audit + tests + mappings

---
+++ src/igvfd/audit/multiplexed_sample.py
@@ -0,0 +1,29 @@
+from snovault.auditor import (
+    audit_checker,
+    AuditFailure,
+)
+from .formatter import (
+    audit_link,
+    path_to_text,
+    get_audit_description
+)
+
+
+@audit_checker('MultiplexedSample', frame='object')
+def audit_multiplexed_sample_no_barcode_map(value, system):
+    '''
+    [
+        {
+            "audit_description": "Multiplexed samples that are demultiplexed with barcodes are expected to specify a barcode sample map.",
+            "audit_category": "missing barcode sample map",
+            "audit_level": "NOT_COMPLIANT"
+        }
+    ]
+    '''
+    description = get_audit_description(audit_multiplexed_sample_no_barcode_map, index=0)
+    if not (value.get('barcode_sample_map')):
+        detail = (
+            f'Multiplexed sample {audit_link(path_to_text(value["@id"]), value["@id"])} '
+            f'has no `barcode_sample_map`.'
+        )
+        yield AuditFailure('missing barcode sample map', f'{detail} {description}', level='NOT_COMPLIANT')
---
+++ src/igvfd/tests/test_audit_multiplexed_sample.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+def test_audit_missing_barcode_sample_map(
+    testapp,
+    multiplexed_sample,
+    tabular_file
+):
+    res = testapp.get(multiplexed_sample['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'missing barcode sample map'
+        for error in res.json['audit'].get('NOT_COMPLIANT', [])
+    )
+    testapp.patch_json(
+        multiplexed_sample['@id'],
+        {'barcode_sample_map': tabular_file['@id']}
+    )
+    res = testapp.get(multiplexed_sample['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing barcode sample map'
+        for error in res.json['audit'].get('NOT_COMPLIANT', [])
+    )
