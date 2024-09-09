7492d394e9241343a81caefba1c94df695e66933
Meenakshi Kagda <meenakshi@C02XR143JGH8.local>
2024-09-09 14:21:55 -0700

    update audits

--- src/igvfd/audit/multiplexed_sample.py
+++ src/igvfd/audit/multiplexed_sample.py
@@ -14,16 +14,16 @@ def audit_multiplexed_sample_no_barcode_map(value, system):
     '''
     [
         {
-            "audit_description": "Multiplexed samples that use barcodes for demultiplexing are expected to specify a barcode sample map.",
-            "audit_category": "missing barcode sample map",
+            "audit_description": "Multiplexed samples that use barcodes for demultiplexing are expected to specify a barcode map.",
+            "audit_category": "missing barcode map",
             "audit_level": "WARNING"
         }
     ]
     '''
     audit_message = get_audit_message(audit_multiplexed_sample_no_barcode_map, index=0)
-    if not (value.get('barcode_sample_map')):
+    if not (value.get('barcode_map')):
         detail = (
             f'Multiplexed sample {audit_link(path_to_text(value["@id"]), value["@id"])} '
-            f'has no `barcode_sample_map`.'
+            f'has no `barcode_map`.'
         )
         yield AuditFailure(audit_message.get('audit_category', ''), f'{detail} {audit_message.get("audit_description", "")}', level=audit_message.get('audit_level', ''))
