d6abd6fe1e2913265814367eeeed140af7352230
ian-whaling <whaling.ian@gmail.com>
2024-09-09 17:47:17 -0700

    audit + test

---
+++ src/igvfd/tests/test_audit_tabular_file.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+def test_audit_missing_file_format_specifications(
+    testapp,
+    tabular_file,
+    experimental_protocol_document
+):
+    res = testapp.get(tabular_file['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'missing file format specifications'
+        for error in res.json['audit'].get('NOT_COMPLIANT', [])
+    )
+    testapp.patch_json(
+        tabular_file['@id'],
+        {'file_format_specifications': experimental_protocol_document['@id']}
+    )
+    res = testapp.get(multiplexed_sample['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing file format specifications'
+        for error in res.json['audit'].get('NOT_COMPLIANT', [])
+    )
