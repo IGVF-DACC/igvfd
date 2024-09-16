50a7dc70d06ab3cdb888bac63314652f06f2fa2c
Jennifer Jou <jjou@stanford.edu>
2024-09-16 13:34:51 -0700

    fix test

--- src/igvfd/tests/test_types_model_set.py
+++ src/igvfd/tests/test_types_model_set.py
@@ -3,4 +3,4 @@ import pytest
 
 def test_summary(testapp, model_set_no_input):
     res = testapp.get(model_set_no_input['@id'])
-    assert res.json.get('summary') == 'predictive model v0.0.1 neural network targeting genes'
+    assert res.json.get('summary') == 'predictive model v0.0.1 neural network predicting genes'
