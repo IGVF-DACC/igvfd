b05814ae8df6a1b0f03ee47ef835a8b0110cc66f
Meenakshi Kagda <meenakshi@C02XR143JGH8.local>
2024-09-17 13:46:49 -0700

    update symbols

--- src/igvfd/types/sample.py
+++ src/igvfd/types/sample.py
@@ -206,8 +206,8 @@ class Biosample(Sample):
         Path('sample_terms', include=['@id', 'term_name']),
         Path('disease_terms', include=['@id', 'term_name']),
         Path('treatments', include=['@id', 'purpose', 'treatment_type', 'summary', 'status', 'treatment_term_name']),
-        Path('modifications', include=['@id', 'modality', 'summary',
-             'status', 'fused_domain', '@type', 'tagged_proteins', 'cas']),
+        Path('modifications.tagged_proteins', include=['@id', 'modality', 'summary',
+             'status', 'fused_domain', '@type', 'symbol', 'cas']),
         Path('institutional_certificates', include=['@id', 'certificate_identifier'])
     ]
 
