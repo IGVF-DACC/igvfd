c4f3204f07d65197cd7860a7e0073cf6e79c371b
Meenakshi Kagda <meenakshi@C02XR143JGH8.local>
2024-09-26 15:25:43 -0700

    fixture again

--- src/igvfd/tests/fixtures/schemas/matrix_file.py
+++ src/igvfd/tests/fixtures/schemas/matrix_file.py
@@ -95,7 +95,7 @@ def matrix_file_v6(testapp, lab, award, measurement_set, reference_file):
             reference_file['@id']
         ],
         'schema_version': '6',
-        'dimension1': 'genomic position',
-        'dimension2': ['genomic position']
+        'dimension1': 'cell',
+        'dimension2': ['gene']
     }
     return item
