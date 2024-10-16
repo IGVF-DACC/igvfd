a025ed0c54e7e9283d069ca41d29210d1bf1942b
ian-whaling <whaling.ian@gmail.com>
2024-10-16 12:59:03 -0700

    removed duplicate fixture

--- src/igvfd/tests/fixtures/schemas/analysis_set.py
+++ src/igvfd/tests/fixtures/schemas/analysis_set.py
@@ -48,22 +48,6 @@ def intermediate_analysis_set(
 
 
 @pytest.fixture
-def principal_analysis_set(
-    testapp,
-    award,
-    lab,
-    measurement_set
-):
-    item = {
-        'award': award['@id'],
-        'lab': lab['@id'],
-        'input_file_sets': [measurement_set['@id']],
-        'file_set_type': 'principal analysis'
-    }
-    return testapp.post_json('/analysis_set', item, status=201).json['@graph'][0]
-
-
-@pytest.fixture
 def analysis_set_v1(analysis_set_base, curated_set_genome, human_donor, in_vitro_cell_line):
     item = analysis_set_base.copy()
     item.update({
