1d5a2a7864c6278b4199ca97b52922d422a07f26
Keenan Graham <ksgraham@stanford.edu>
2024-10-28 13:37:36 -0700

    Try adding local bucket

--- docker/localstack/create_localstack.sh
+++ docker/localstack/create_localstack.sh
@@ -16,3 +16,11 @@ echo 'Creating SQS'
 
 awslocal sqs create-queue --queue-name transaction-queue --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"'$TRANSACTION_DLQ_ARN'\",\"maxReceiveCount\":\"3\"}", "VisibilityTimeout": "60"}'
 awslocal sqs create-queue --queue-name invalidation-queue --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"'$INVALIDATION_DLQ_ARN'\",\"maxReceiveCount\":\"3\"}", "VisibilityTimeout": "120"}'
+
+echo 'Creating S3 buckets'
+
+S3_BUCKET=$(awslocal s3api create-bucket --bucket igvf-files-local --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2 --query 'Location' --output text)
+RESTRICTED_S3_BUCKET=$(awslocal s3api create-bucket --bucket igvf-restricted-files-local --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2 --query 'Location' --output text)
+
+echo $S3_BUCKET
+echo $RESTRICTED_S3_BUCKET
