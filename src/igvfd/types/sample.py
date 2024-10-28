0aec22d618181028b9cc80d25f984a97aca9ab53
ian-whaling <whaling.ian@gmail.com>
2024-10-28 11:39:56 -0700

    fixed modification embedding

--- src/igvfd/types/sample.py
+++ src/igvfd/types/sample.py
@@ -89,9 +89,9 @@ class Sample(Item):
              'status', 'treatment_term_name', 'depletion']),
         Path('biomarkers.gene', include=['@id', 'name', 'classification', 'quantification', 'gene', 'symbol']),
         Path('modifications.tagged_protein', include=[
-             '@id', 'tagged_protein', 'modality', 'fused_domain', '@type', 'symbol', 'cas', 'cas_species']),
+             '@id', 'tagged_protein', 'tagged_proteins', 'modality', 'fused_domain', '@type', 'symbol', 'cas', 'cas_species', 'degron_system']),
         Path('modifications.tagged_proteins', include=[
-             '@id', 'tagged_proteins', 'modality', '@type', 'symbol', 'degron_system']),
+             '@id', 'symbol']),
         Path('institutional_certificates', include=['@id', 'certificate_identifier']),
         Path('construct_library_sets.associated_phenotypes', include=[
              '@id', 'accession', 'file_set_type', 'term_name', 'associated_phenotypes'])
