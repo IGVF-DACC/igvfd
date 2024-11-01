087426bd6424043f58dfbca4d9760a7f08c4b77f
ian-whaling <whaling.ian@gmail.com>
2024-11-01 14:42:22 -0700

    test fixed

--- src/igvfd/tests/test_types_analysis_set.py
+++ src/igvfd/tests/test_types_analysis_set.py
@@ -242,7 +242,8 @@ def test_analysis_set_sample_summary(testapp, principal_analysis_set, measuremen
         }
     )
     res = testapp.get(principal_analysis_set['@id']).json
-    assert res.get('sample_summary', '') == 'K562 differentiated cell specimen induced to endothelial cell of vascular tree, at 1 time point(s) post change, differentiated with treatment(s), modified with Degron and CRISPR modifications, transfected with a guide library, sorted on expression of MYC'
+    print(res.get('sample_summary', ''))
+    assert res.get('sample_summary', '') == 'K562 differentiated cell specimen induced to endothelial cell of vascular tree, at 1 time point(s) post change, differentiated with treatment(s), modified with CRISPR and Degron modifications, transfected with a guide library, sorted on expression of MYC'
 
 
 def test_functional_assay_mechanisms(testapp, analysis_set_base, measurement_set, measurement_set_with_functional_assay_mechanisms, phenotype_term_from_go, phenotype_term_myocardial_infarction):
