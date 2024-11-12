9f9663c767b6fc1731fc1e7633f9c80e29047ea4
keenangraham <ksgraham@stanford.edu>
2024-11-11 18:01:56 -0800

    point-to-snovault-v15.1.0 (#1180)

--- src/igvfd/tests/indexing/conftest.py
+++ src/igvfd/tests/indexing/conftest.py
@@ -20,17 +20,17 @@ def make_test_app(app):
 
 
 def wait_for_indexing_poll(testapp):
-    double_check_number = 3
+    double_check_number = 5
     while True:
         print('Waiting for indexing', double_check_number)
         is_indexing = bool(testapp.get('/indexer-info').json['is_indexing'])
         if is_indexing:
-            double_check_number = 3
+            double_check_number = 5
         else:
             double_check_number -= 1
         if double_check_number <= 0:
             break
-        time.sleep(10)
+        time.sleep(11)
 
 
 def wait_for_opensearch(app):
