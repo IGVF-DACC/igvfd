f2f14fcd413d850305f11484ef85363b119dd6a6
Keenan Graham <ksgraham@stanford.edu>
2024-11-14 13:10:24 -0800

    Try inc keepalive

--- config/gunicorn/development.conf.py
+++ config/gunicorn/development.conf.py
@@ -3,5 +3,6 @@ workers = 9
 threads = 1
 timeout = 90
 accesslog = '-'
+keepalive = 60
 logger_class = 'igvfd.logging.gunicornlogger.MyGunicornLogger'
 access_log_format = '{\"host\":"%(h)s", \"user\":"%(u)s", \"time\":"%(t)s", \"statusline\":"%(r)s", \"status\":%(s)s, \"response_length\":%(b)s, \"referer\":"%(f)s", \"user_agent\":"%(a)s", \"db_count\":%(db_count)d, \"db_time\":%(db_time)d, \"es_count\":%(es_count)d, \"es_time\":%(es_time)d, \"rss_begin\": %(rss_begin)d, \"rss_end\": %(rss_end)d, \"rss_change\": %(rss_change)d, \"wsgi_begin\":%(wsgi_begin)d,  \"wsgi_end\":%(wsgi_end)d, \"wsgi_time\":%(wsgi_time)d}'
