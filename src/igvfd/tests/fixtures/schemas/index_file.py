544bb1123009c22701fad5c25d1b968d464bc727
Jennifer Jou <jjou@stanford.edu>
2024-11-15 17:38:44 -0800

    Fix fixtures

--- src/igvfd/tests/fixtures/schemas/index_file.py
+++ src/igvfd/tests/fixtures/schemas/index_file.py
@@ -7,10 +7,9 @@ def index_file_tbi(testapp, lab, award, principal_analysis_set, tabular_file):
         'award': award['@id'],
         'lab': lab['@id'],
         'md5sum': '5ac7f55b8735dd430df19a01b08bb548',
-        'file_format': 'tsv',
+        'file_format': 'tbi',
         'file_set': principal_analysis_set['@id'],
         'content_type': 'index',
-        'assembly': 'GRCh38',
         'controlled_access': False,
         'derived_from': [tabular_file['@id']]
     }
@@ -23,10 +22,9 @@ def index_file_bai(testapp, lab, award, principal_analysis_set, alignment_file):
         'award': award['@id'],
         'lab': lab['@id'],
         'md5sum': '07f195481361a5c3a5ac0cf5501b09c',
-        'file_format': 'tsv',
+        'file_format': 'bai',
         'file_set': principal_analysis_set['@id'],
         'content_type': 'index',
-        'assembly': 'GRCh38',
         'controlled_access': False,
         'derived_from': [alignment_file['@id']]
     }
