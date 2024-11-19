641f33ec3449599c4a694d4d1a488fd830ffb876
Jennifer Jou <jjou@stanford.edu>
2024-11-18 17:22:46 -0800

    Move audits to own file, fix test

--- src/igvfd/tests/test_audit_file.py
+++ src/igvfd/tests/test_audit_file.py
@@ -125,22 +125,3 @@ def test_audit_external_reference_files(testapp, reference_file):
         audit['category'] != 'missing dbxrefs'
         for audit in res.json['audit'].get('WARNING', {})
     )
-
-
-def test_audit_index_files(testapp, reference_file, index_file_tbi):
-    res = testapp.get(index_file_tbi['@id'] + '@@audit')
-    assert all(
-        audit['category'] != 'incorrect indexed file'
-        for audit in res.json['audit'].get('ERROR', {})
-    )
-    testapp.patch_json(
-        index_file_tbi['@id'],
-        {
-            'derived_from': [reference_file['@id']]
-        }
-    )
-    res = testapp.get(index_file_tbi['@id'] + '@@audit')
-    assert any(
-        audit['category'] == 'incorrect indexed file'
-        for audit in res.json['audit'].get('ERROR', {})
-    )
