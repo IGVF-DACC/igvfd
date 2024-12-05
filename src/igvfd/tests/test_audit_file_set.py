2abb540d0ead4200f528bb207fe37e902a827545
ian-whaling <whaling.ian@gmail.com>
2024-12-05 14:41:25 -0800

    fixed another test

--- src/igvfd/tests/test_audit_file_set.py
+++ src/igvfd/tests/test_audit_file_set.py
@@ -27,11 +27,11 @@ def test_audit_missing_files(
 
 def test_audit_input_file_set_for(
     testapp,
-    measurement_set,
+    measurement_set_mpra,
     analysis_set_base,
     sequence_file
 ):
-    res = testapp.get(measurement_set['@id'] + '@@audit')
+    res = testapp.get(measurement_set_mpra['@id'] + '@@audit')
     assert all(
         error['category'] != 'missing analysis'
         for error in res.json['audit'].get('NOT_COMPLIANT', [])
@@ -39,10 +39,10 @@ def test_audit_input_file_set_for(
     testapp.patch_json(
         sequence_file['@id'],
         {
-            'file_set': measurement_set['@id']
+            'file_set': measurement_set_mpra['@id']
         }
     )
-    res = testapp.get(measurement_set['@id'] + '@@audit')
+    res = testapp.get(measurement_set_mpra['@id'] + '@@audit')
     assert any(
         error['category'] == 'missing analysis'
         for error in res.json['audit'].get('NOT_COMPLIANT', [])
@@ -50,10 +50,10 @@ def test_audit_input_file_set_for(
     testapp.patch_json(
         analysis_set_base['@id'],
         {
-            'input_file_sets': [measurement_set['@id']]
+            'input_file_sets': [measurement_set_mpra['@id']]
         }
     )
-    res = testapp.get(measurement_set['@id'] + '@@audit')
+    res = testapp.get(measurement_set_mpra['@id'] + '@@audit')
     assert all(
         error['category'] != 'missing analysis'
         for error in res.json['audit'].get('NOT_COMPLIANT', [])
