7bc751f969e732286932e000c98a7560d560ebe5
Keenan Graham <ksgraham@stanford.edu>
2024-12-12 16:38:55 -0800

    Add local deduplication queue

--- docker/dedup/run-once.sh
+++ docker/dedup/run-once.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running once"
-./dedup -numWorkers=${NUM_WORKERS} -queueURL=${QUEUE_URL}
+./dedup -numWorkers=${NUM_WORKERS} -queueURL=${QUEUE_URL} -storageQueueURL=${STORAGE_QUEUE_URL}
