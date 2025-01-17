66a4d34fe28090eb15d97bc456f5b96f45e3b5c2
ian-whaling <whaling.ian@gmail.com>
2025-01-17 10:58:33 -0800

    description fixed

--- src/igvfd/audit/file_set.py
+++ src/igvfd/audit/file_set.py
@@ -155,7 +155,7 @@ def audit_inconsistent_seqspec(value, system):
     '''
     [
         {
-            "audit_description": "Sequence files in a file set from the same sequencing set, are expected to link to the same seqspec file, which should be unique to that set sequencing set. A sequencing set is defined by the combination of flowcell ID, lane, and index for non-single cell data, or flowcell ID, lane, index, and sequencing run for single cell data.",
+            "audit_description": "Sequence files in a file set from the same sequencing set are expected to link to the same seqspec file, which should be unique to that sequencing set. A sequencing set is defined by the combination of flowcell ID, lane, and index for non-single cell data, or flowcell ID, lane, index, and sequencing run for single cell data.",
             "audit_category": "inconsistent sequence specifications",
             "audit_level": "ERROR"
         }
