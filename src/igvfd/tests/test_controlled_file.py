8b5331440680adbb8ba32e3c1337da2d9bcb51b0
Keenan Graham <ksgraham@stanford.edu>
2025-03-11 14:33:30 -0700

    Add test

--- src/igvfd/tests/test_controlled_file.py
+++ src/igvfd/tests/test_controlled_file.py
@@ -25,6 +25,16 @@ def submitter_testapp(submitter, app, external_tx, zsa_savepoints):
     return _remote_user_testapp(app, submitter['uuid'])
 
 
+@pytest.fixture
+def viewing_group_member_testapp(viewing_group_member, app, external_tx, zsa_savepoints):
+    return _remote_user_testapp(app, viewing_group_member['uuid'])
+
+
+@pytest.fixture
+def verified_member_testapp(verified_member, app, external_tx, zsa_savepoints):
+    return _remote_user_testapp(app, verified_member['uuid'])
+
+
 def test_submitter_post_controlled_access_file(submitter_testapp, controlled_sequence_file):
     submitter_testapp.post_json('/sequence_file', controlled_sequence_file, status=201)
 
@@ -55,3 +65,58 @@ def test_wrangler_patch_controlled_access_file(wrangler_testapp, controlled_sequ
     res = wrangler_testapp.get(controlled_sequence_file_object['@id'])
     controlled_access = {'controlled_access': False}
     wrangler_testapp.patch_json(res.json['@id'], controlled_access, status=422)
+
+
+def test_controlled_file_only_viewing_group_members_can_download_controlled_access_file(testapp, anontestapp, authenticated_testapp, submitter_testapp, viewing_group_member_testapp, verified_member_testapp, controlled_access_alignment_file, alignment_file):
+    res = testapp.get(alignment_file['@id'])
+    assert res.json['controlled_access'] is False
+    assert 's3_uri' in res.json
+    assert 'href' in res.json
+    assert 'anvil_url' not in res.json
+    res = testapp.get(controlled_access_alignment_file['@id'])
+    assert res.json['controlled_access'] is True
+    assert res.json['status'] == 'in progress'
+    assert 's3_uri' in res.json
+    assert 'href' in res.json
+    assert 'anvil_url' not in res.json
+    # Get upload only submitter or admin
+    testapp.get(controlled_access_alignment_file['@id'] + '@@upload', status=200)
+    submitter_testapp.get(controlled_access_alignment_file['@id'] + '@@upload', status=200)
+    authenticated_testapp.get(controlled_access_alignment_file['@id'] + '@@upload', status=403)
+    anontestapp.get(controlled_access_alignment_file['@id'] + '@@upload', status=403)
+    # Post upload only submitter or admin
+    testapp.post_json(controlled_access_alignment_file['@id'] + '@@upload', {}, status=200)
+    submitter_testapp.post_json(controlled_access_alignment_file['@id'] + '@@upload', {}, status=200)
+    # Not viewing group or anon.
+    authenticated_testapp.post_json(controlled_access_alignment_file['@id'] + '@@upload', {}, status=403)
+    viewing_group_member_testapp.post_json(controlled_access_alignment_file['@id'] + '@@upload', {}, status=403)
+    verified_member_testapp.post_json(controlled_access_alignment_file['@id'] + '@@upload', {}, status=403)
+    # Download in progress only if viewing group.
+    testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=307)
+    authenticated_testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=307)
+    submitter_testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=307)
+    viewing_group_member_testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=307)
+    # Not anon or verified user.
+    anontestapp.get(controlled_access_alignment_file['@id'] + '@@download', status=403)
+    verified_member_testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=403)
+    testapp.patch_json(
+        controlled_access_alignment_file['@id'],
+        {
+            'status': 'released',
+            'release_timestamp':  '2024-03-06T12:34:56Z',
+            'upload_status': 'validated',
+        },
+        status=200
+    )
+    res = testapp.get(controlled_access_alignment_file['@id'])
+    assert res.json['controlled_access'] is True
+    assert res.json['status'] == 'released'
+    # Download released
+    testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=307)
+    authenticated_testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=307)
+    submitter_testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=307)
+    viewing_group_member_testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=307)
+    # Not anon or verified user.
+    anontestapp.get(controlled_access_alignment_file['@id'] + '@@download', status=403)
+    r = verified_member_testapp.get(controlled_access_alignment_file['@id'] + '@@download', status=403)
+    assert r.json['detail'] == 'Downloading controlled-access file not allowed.'
