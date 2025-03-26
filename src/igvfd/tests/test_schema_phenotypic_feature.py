17bda237bb8d7fd5d4aae696d61ca9385594dd36
Keenan Graham <ksgraham@stanford.edu>
2025-03-26 10:25:53 -0700

    Fix

--- src/igvfd/tests/test_schema_phenotypic_feature.py
+++ src/igvfd/tests/test_schema_phenotypic_feature.py
@@ -8,7 +8,7 @@ def test_phenotypic_feature_required_lab_award(
     item = {
         'feature': phenotype_term_ncit_feature['@id']
     }
-    testapp.post_json('/phenotypic_feature', item, expect_errors=True)
+    res = testapp.post_json('/phenotypic_feature', item, expect_errors=True)
     assert res.status_code == 422
 
 
