e452f23bc93a53213d96296d968c3fa8f50043fa
ian-whaling <whaling.ian@gmail.com>
2025-03-26 13:32:40 -0700

    adjusted audit

--- src/igvfd/tests/test_audit_tabular_file.py
+++ src/igvfd/tests/test_audit_tabular_file.py
@@ -13,7 +13,17 @@ def test_audit_missing_file_format_specifications(
     )
     testapp.patch_json(
         tabular_file['@id'],
-        {'file_format_specifications': [experimental_protocol_document['@id']]}
+        {'file_format': 'vcf'}
+    )
+    res = testapp.get(tabular_file['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing file format specifications'
+        for error in res.json['audit'].get('NOT_COMPLIANT', [])
+    )
+    testapp.patch_json(
+        tabular_file['@id'],
+        {'file_format_specifications': [experimental_protocol_document['@id']],
+         'file_format': 'tsv'}
     )
     res = testapp.get(tabular_file['@id'] + '@@audit')
     assert all(
