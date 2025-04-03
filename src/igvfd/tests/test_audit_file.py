7273e06efad4b097e74a75b3356e64afcd178236
Meenakshi Kagda <meenakshikagda@Meenakshis-MacBook-Pro.local>
2025-04-03 12:30:24 -0700

    move audits

--- src/igvfd/tests/test_audit_file.py
+++ src/igvfd/tests/test_audit_file.py
@@ -94,32 +94,6 @@ def test_audit_file_format_specifications(testapp, matrix_file, experimental_pro
     )
 
 
-def test_audit_external_identifiers(testapp, sequence_file):
-    testapp.patch_json(
-        sequence_file['@id'],
-        {
-            'externally_hosted': True,
-            'external_host_url': 'http://test_url'
-        }
-    )
-    res = testapp.get(sequence_file['@id'] + '@@audit')
-    assert any(
-        audit['category'] == 'missing dbxrefs'
-        for audit in res.json['audit'].get('NOT_COMPLIANT', {})
-    )
-    testapp.patch_json(
-        sequence_file['@id'],
-        {
-            'dbxrefs': ['SRA:SRR20474123']
-        }
-    )
-    res = testapp.get(sequence_file['@id'] + '@@audit')
-    assert all(
-        audit['category'] != 'missing dbxrefs'
-        for audit in res.json['audit'].get('NOT_COMPLIANT', {})
-    )
-
-
 def test_audit_external_reference_files(testapp, reference_file):
     testapp.patch_json(
         reference_file['@id'],
