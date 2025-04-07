fbd1bf6f7ecfab55049d56b1a8348d669f26b6c3
Otto Jolanki <ojolanki@stanford.edu>
2025-04-07 12:25:58 -0700

    item types to constants.py

--- cdk/infrastructure/constructs/dashboards/backend.py
+++ cdk/infrastructure/constructs/dashboards/backend.py
@@ -20,66 +20,7 @@ from typing import Any
 from typing import List
 
 from infrastructure.config import Config
-
-ITEM_TYPES = [
-    'access_key',
-    'alignment_file',
-    'analysis_set',
-    'analysis_step',
-    'analysis_step_version',
-    'assay_term',
-    'auxiliary_set',
-    'award',
-    'biomarker',
-    'configuration_file',
-    'construct_library_set',
-    'crispr_modification',
-    'curated_set',
-    'degron_modification',
-    'document',
-    'gene',
-    'genome_browser_annotation_file',
-    'human_donor',
-    'image',
-    'image_file',
-    'in_vitro_system',
-    'index_file',
-    'institutional_certificate',
-    'lab',
-    'matrix_file',
-    'measurement_set',
-    'model_file',
-    'model_set',
-    'mpra_quality_metric',
-    'multiplexed_sample',
-    'open_reading_frame',
-    'page',
-    'perturb_seq_quality_metric',
-    'phenotype_term',
-    'phenotypic_feature',
-    'platform_term',
-    'prediction_set',
-    'primary_cell',
-    'publication',
-    'reference_file',
-    'rodent_donor',
-    'sample_term',
-    'sequence_file',
-    'signal_file',
-    'single_cell_atac_seq_quality_metric',
-    'single_cell_rna_seq_quality_metric',
-    'software',
-    'software_version',
-    'source',
-    'starr_seq_quality_metric',
-    'tabular_file',
-    'technical_sample',
-    'tissue',
-    'treatment',
-    'user',
-    'whole_organism',
-    'workflow',
-]
+from infrastructure.constructs.dashboards.constants import ITEM_TYPES
 
 
 @dataclass
@@ -93,66 +34,6 @@ class BackendDashboard(Construct):
     props: BackendDashboardProps
     _widgets: List[IWidget]
     _item_type_indexing_metrics: List[IMetric]
-
-    ITEM_TYPES: List[str] = [
-        'access_key',
-        'alignment_file',
-        'analysis_set',
-        'analysis_step',
-        'analysis_step_version',
-        'assay_term',
-        'auxiliary_set',
-        'award',
-        'biomarker',
-        'configuration_file',
-        'construct_library_set',
-        'crispr_modification',
-        'curated_set',
-        'degron_modification',
-        'document',
-        'gene',
-        'genome_browser_annotation_file',
-        'human_donor',
-        'image',
-        'image_file',
-        'in_vitro_system',
-        'index_file',
-        'institutional_certificate',
-        'lab',
-        'matrix_file',
-        'measurement_set',
-        'model_file',
-        'model_set',
-        'mpra_quality_metric',
-        'multiplexed_sample',
-        'open_reading_frame',
-        'page',
-        'perturb_seq_quality_metric',
-        'phenotype_term',
-        'phenotypic_feature',
-        'platform_term',
-        'prediction_set',
-        'primary_cell',
-        'publication',
-        'reference_file',
-        'rodent_donor',
-        'sample_term',
-        'sequence_file',
-        'signal_file',
-        'single_cell_atac_seq_quality_metric',
-        'single_cell_rna_seq_quality_metric',
-        'software',
-        'software_version',
-        'source',
-        'starr_seq_quality_metric',
-        'tabular_file',
-        'technical_sample',
-        'tissue',
-        'treatment',
-        'user',
-        'whole_organism',
-        'workflow',
-    ]
 
     def __init__(
             self,
@@ -198,7 +79,7 @@ class BackendDashboard(Construct):
         return item_type_indexing_metric
 
     def _define_item_type_indexing_metrics(self) -> None:
-        for item_type in self.ITEM_TYPES:
+        for item_type in ITEM_TYPES:
             self._item_type_indexing_metrics.append(self._define_item_type_indexing_metric(item_type))
 
     def _define_item_type_indexing_widget(self) -> None:
