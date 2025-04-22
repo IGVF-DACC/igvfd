16db40df3a1d6c120eca4f5bc1079d7ab22b29e3
khine <khzlin@stanford.edu>
2025-04-22 12:16:14 -0700

    code review

--- src/igvfd/tests/test_audit_index_file.py
+++ src/igvfd/tests/test_audit_index_file.py
@@ -25,7 +25,7 @@ def test_audit_index_files(testapp, reference_file, index_file_tbi, tabular_file
         }
     )
     res = testapp.get(index_file_tbi['@id'] + '@@audit')
-    assert not any(
-        audit['category'] == 'unexpected indexed file'
+    assert all(
+        audit['category'] != 'unexpected indexed file'
         for audit in res.json['audit'].get('ERROR', {})
     )
