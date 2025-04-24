9802786468b884f65412a9d31687bc5f1dd44770
Ian Whaling <ian.whaling@gmail.com>
2025-04-24 10:38:59 -0700

    calculate assay titles fix

--- src/igvfd/types/file_set.py
+++ src/igvfd/types/file_set.py
@@ -1481,35 +1481,32 @@ class ConstructLibrarySet(FileSet):
     def applied_to_samples(self, request, applied_to_samples):
         return paths_filtered_by_status(request, applied_to_samples)
 
-    @calculated_property(
-        define=True,
-        condition='applied_to_samples',
-        schema={
-            'title': 'Assay Titles',
-            'description': 'Title(s) of assay that produced data this construct library set was used in.',
-            'type': 'array',
-            'minItems': 1,
-            'uniqueItems': True,
-            'items': {
-                'title': 'Assay Title',
-                'description': 'Title of assay that produced data this construct library set was used in.',
-                'type': 'string'
-            },
-            'notSubmittable': True,
-        }
-    )
-    def assay_titles(self, request, applied_to_samples=[]):
-        assay_titles = set()
-        for sample in applied_to_samples:
-            sample_object = request.embed(
-                sample, '@@object_with_select_calculated_properties?field=file_sets')
-            for file_set in sample_object.get('file_sets', []):
-                file_set_object = request.embed(
-                    file_set, '@@object_with_select_calculated_properties?field=preferred_assay_title')
-                preferred_assay_title = file_set_object.get('preferred_assay_title')
-                if preferred_assay_title:
-                    assay_titles.add(preferred_assay_title)
-        return list(assay_titles)
+    # @calculated_property(
+    #     define=True,
+    #     condition='file_sets',
+    #     schema={
+    #         'title': 'Assay Titles',
+    #         'description': 'Title(s) of assay that produced data this construct library set was used in.',
+    #         'type': 'array',
+    #         'minItems': 1,
+    #         'uniqueItems': True,
+    #         'items': {
+    #             'title': 'Assay Title',
+    #             'description': 'Title of assay that produced data this construct library set was used in.',
+    #             'type': 'string'
+    #         },
+    #         'notSubmittable': True,
+    #     }
+    # )
+    # def assay_titles(self, request, file_sets=[]):
+    #     assay_titles = set()
+    #     for file_set in file_sets.get('file_sets', []):
+    #         if file_set.startswith('/measurement-sets/'):
+    #             file_set_object = request.embed(file_set, '@@skip_calculated=true')
+    #             preferred_assay_title = file_set_object.get('preferred_assay_title')
+    #             if preferred_assay_title:
+    #                 assay_titles.add(preferred_assay_title)
+    #     return list(assay_titles)
 
     @calculated_property(
         schema={
@@ -1526,6 +1523,7 @@ class ConstructLibrarySet(FileSet):
         pheno_terms = []
         pheno_phrase = ''
         preposition = ''
+        pool_phrase = ''
         criteria = []
         criteria = criteria + selection_criteria
 
@@ -1597,21 +1595,26 @@ class ConstructLibrarySet(FileSet):
             else:
                 pheno_phrase = f' associated with {len(pheno_terms)} phenotypes'
 
-        # special case for SUPERSTARR libraries to display 1000 genomes IDs
-        if assay_titles and 'SUPERSTARR' in assay_titles:
-            thousand_genomes_ids = set()
-            for integrated_content_file in integrated_content_files:
-                integrated_content_file_object = request.embed(integrated_content_file, '@@object?skip_calculated=true')
-                file_set_object = request.embed(integrated_content_file_object.get(
-                    'file_set'), '@@object?skip_calculated=true')
-                donors = file_set_object.get('donors', [])
-                for donor in donors:
-                    donor_object = request.embed(donor, '@@object?skip_calculated=true')
-                    dbxrefs = donor_object.get('dbxrefs')
-                    for dbxref in dbxrefs:
-                        if dbxref.startswith('IGSR'):
-                            thousand_genomes_id = dbxref.split(':')[1]
-                            thousand_genomes_ids.add(thousand_genomes_id)
+        # special case for STARR-seq libraries to display 1000 genomes IDs
+        # if assay_titles and 'STARR-seq' in assay_titles:
+        #     thousand_genomes_ids = set()
+        #     for integrated_content_file in integrated_content_files:
+        #         integrated_content_file_object = request.embed(integrated_content_file, '@@object?skip_calculated=true')
+        #         print(integrated_content_file_object)
+        #         file_set_object = request.embed(integrated_content_file_object['file_set'], '@@object?skip_calculated=true')
+        #         print(file_set_object)
+        #         donors = file_set_object.get('donors', [])
+        #         for donor in donors:
+        #             donor_object = request.embed(donor, '@@object?skip_calculated=true')
+        #             print(donor_object)
+        #             dbxrefs = donor_object.get('dbxrefs', [])
+        #             for dbxref in dbxrefs:
+        #                 if dbxref.startswith('IGSR'):
+        #                     thousand_genomes_id = dbxref.split(':')[1]
+        #                     thousand_genomes_ids.add(thousand_genomes_id)
+        #     if thousand_genomes_ids:
+        #         thousand_genomes_ids = ', '.join(list(thousand_genomes_ids))
+        #         pool_phrase = f' pooled from 1000 Genomes donors: {thousand_genomes_ids}'
 
         if file_set_type == 'expression vector library':
             if 'genes' in criteria:
@@ -1627,4 +1630,4 @@ class ConstructLibrarySet(FileSet):
                 preposition = ''
             else:
                 preposition = ' in'
-            return f'{library_type} targeting {selections}{preposition}{target_phrase}{pheno_phrase}'
+            return f'{library_type} targeting {selections}{preposition}{target_phrase}{pheno_phrase}{pool_phrase}'
