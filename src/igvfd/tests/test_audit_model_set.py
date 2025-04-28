5f8215435782a0af443aefa87a46a7d2687ffe95
Ian Whaling <78115078+ian-whaling@users.noreply.github.com>
2025-04-28 10:20:12 -0700

    IGVF-2505-audit-missing-input (#1495)

--- src/igvfd/tests/test_audit_model_set.py
+++ src/igvfd/tests/test_audit_model_set.py
@@ -58,3 +58,118 @@ def test_audit_missing_analysis_step_version_model_set(
         error['category'] != 'missing analysis step version'
         for error in res.json['audit'].get('WARNING', [])
     )
+
+
+def test_audit_missing_input_file_set(
+    testapp,
+    model_set_no_input,
+    analysis_set_base,
+    model_file,
+    signal_file
+):
+    res = testapp.get(model_set_no_input['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing input file set'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        signal_file['@id'],
+        {
+            'file_set': analysis_set_base['@id']
+        }
+    )
+    testapp.patch_json(
+        model_file['@id'],
+        {
+            'file_set': model_set_no_input['@id'],
+            'derived_from': [signal_file['@id']]
+        }
+    )
+    res = testapp.get(model_set_no_input['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'missing input file set'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        signal_file['@id'],
+        {
+            'file_set': model_set_no_input['@id']
+        }
+    )
+    res = testapp.get(model_set_no_input['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing input file set'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+
+
+def test_audit_missing_derived_from(
+    testapp,
+    model_set_no_input,
+    model_file,
+    signal_file
+):
+    testapp.patch_json(
+        model_file['@id'],
+        {
+            'file_set': model_set_no_input['@id']
+        }
+    )
+    res = testapp.get(model_set_no_input['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'missing derived from'
+        for error in res.json['audit'].get('WARNING', [])
+    )
+    testapp.patch_json(
+        model_file['@id'],
+        {
+            'derived_from': [signal_file['@id']]
+        }
+    )
+    res = testapp.get(model_set_no_input['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing derived from'
+        for error in res.json['audit'].get('WARNING', [])
+    )
+
+
+def test_audit_unexpected_input_file_set(
+    testapp,
+    model_set_no_input,
+    model_file,
+    signal_file,
+    measurement_set_multiome
+):
+    testapp.patch_json(
+        model_file['@id'],
+        {
+            'file_set': model_set_no_input['@id']
+        }
+    )
+    testapp.patch_json(
+        model_set_no_input['@id'],
+        {
+            'input_file_sets': [measurement_set_multiome['@id']]
+        }
+    )
+    res = testapp.get(model_set_no_input['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'unexpected input file set'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        model_file['@id'],
+        {
+            'derived_from': [signal_file['@id']]
+        }
+    )
+    testapp.patch_json(
+        signal_file['@id'],
+        {
+            'file_set': measurement_set_multiome['@id']
+        }
+    )
+    res = testapp.get(model_set_no_input['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'unexpected input file set'
+        for error in res.json['audit'].get('ERROR', []))
--- src/igvfd/tests/test_audit_prediction_set.py
+++ src/igvfd/tests/test_audit_prediction_set.py
@@ -29,3 +29,118 @@ def test_audit_missing_analysis_step_version_prediction_set(
         error['category'] != 'missing analysis step version'
         for error in res.json['audit'].get('NOT_COMPLIANT', [])
     )
+
+
+def test_audit_missing_input_file_set(
+    testapp,
+    base_prediction_set,
+    analysis_set_base,
+    tabular_file,
+    signal_file
+):
+    res = testapp.get(base_prediction_set['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing input file set'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        signal_file['@id'],
+        {
+            'file_set': analysis_set_base['@id']
+        }
+    )
+    testapp.patch_json(
+        tabular_file['@id'],
+        {
+            'file_set': base_prediction_set['@id'],
+            'derived_from': [signal_file['@id']]
+        }
+    )
+    res = testapp.get(base_prediction_set['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'missing input file set'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        signal_file['@id'],
+        {
+            'file_set': base_prediction_set['@id']
+        }
+    )
+    res = testapp.get(base_prediction_set['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing input file set'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+
+
+def test_audit_missing_derived_from(
+    testapp,
+    base_prediction_set,
+    tabular_file,
+    signal_file
+):
+    testapp.patch_json(
+        tabular_file['@id'],
+        {
+            'file_set': base_prediction_set['@id']
+        }
+    )
+    res = testapp.get(base_prediction_set['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'missing derived from'
+        for error in res.json['audit'].get('WARNING', [])
+    )
+    testapp.patch_json(
+        tabular_file['@id'],
+        {
+            'derived_from': [signal_file['@id']]
+        }
+    )
+    res = testapp.get(base_prediction_set['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing derived from'
+        for error in res.json['audit'].get('WARNING', [])
+    )
+
+
+def test_audit_unexpected_input_file_set(
+    testapp,
+    base_prediction_set,
+    tabular_file,
+    signal_file,
+    measurement_set_multiome
+):
+    testapp.patch_json(
+        tabular_file['@id'],
+        {
+            'file_set': base_prediction_set['@id']
+        }
+    )
+    testapp.patch_json(
+        base_prediction_set['@id'],
+        {
+            'input_file_sets': [measurement_set_multiome['@id']]
+        }
+    )
+    res = testapp.get(base_prediction_set['@id'] + '@@audit')
+    assert any(
+        error['category'] == 'unexpected input file set'
+        for error in res.json['audit'].get('ERROR', [])
+    )
+    testapp.patch_json(
+        tabular_file['@id'],
+        {
+            'derived_from': [signal_file['@id']]
+        }
+    )
+    testapp.patch_json(
+        signal_file['@id'],
+        {
+            'file_set': measurement_set_multiome['@id']
+        }
+    )
+    res = testapp.get(base_prediction_set['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'unexpected input file set'
+        for error in res.json['audit'].get('ERROR', []))
