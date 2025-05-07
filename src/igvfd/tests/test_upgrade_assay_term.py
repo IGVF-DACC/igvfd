62c171f4a4938de76e36060901e6487ec9830f0d
Ian Whaling <ian.whaling@gmail.com>
2025-05-07 15:38:18 -0400

    assay term ugprade fixed

--- src/igvfd/tests/test_upgrade_assay_term.py
+++ src/igvfd/tests/test_upgrade_assay_term.py
@@ -70,11 +70,12 @@ def test_assay_term_upgrade_10_11(upgrader, assay_term_v10):
 
 def test_assay_term_upgrade_11_12(upgrader, assay_term_v11):
     value = upgrader.upgrade('assay_term', assay_term_v11, current_version='11', target_version='12')
-<<<<<<< HEAD
     assert 'definition' not in value
     assert 'comment' not in value
     assert value['schema_version'] == '12'
-=======
-    assert value['schema_version'] == '12'
-    assert set(value.get('preferred_assay_titles')) == {'STARR-seq'}
->>>>>>> 2fa4cdd3 (calculate assay titles fix)
+
+
+def test_assay_term_upgrade_12_13(upgrader, assay_term_v12):
+    value = upgrader.upgrade('assay_term', assay_term_v12, current_version='12', target_version='13')
+    assert 'SUPERSTARR' not in value['preferred_assay_titles'] and 'STARR-seq' in value['preferred_assay_titles']
+    assert value['schema_version'] == '13'
