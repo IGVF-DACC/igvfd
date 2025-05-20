d82d9ab4c932520c177bbe22d5d2f85636d219eb
Zhewei Shen <zwshen@stanford.edu>
2025-05-20 16:31:50 -0700

    asv on workflow and workflow rev link on asv

---
+++ src/igvfd/audit/workflow.py
@@ -0,0 +1,27 @@
+from snovault.auditor import (
+    audit_checker,
+    AuditFailure,
+)
+from .formatter import (
+    audit_link,
+    path_to_text,
+    get_audit_message
+)
+
+
+@audit_checker('Workflow', frame='object')
+def audit_workflow_without_asvs(value, system):
+    '''
+    [
+        {
+            "audit_description": "Workflows are expected to be associated with one or more analysis step version(s).",
+            "audit_category": "missing analysis step versions",
+            "audit_level": "WARNING"
+        }
+    ]
+    '''
+    # Audit 1: missing analysis step versions
+    missing_asv_msg = get_audit_message(audit_workflow_without_asvs, index=0)
+    if 'analysis_step_versions' not in value:
+        detail = f'Workflow {audit_link(path_to_text(value["@id"]), value["@id"])} does not have any analysis step version.'
+        yield AuditFailure(missing_asv_msg.get('audit_category', ''), f'{detail} {missing_asv_msg.get("audit_description", "")}', level=missing_asv_msg.get('audit_level', ''))
--- src/igvfd/schemas/changelogs/workflow.md
+++ src/igvfd/schemas/changelogs/workflow.md
@@ -6,6 +6,7 @@
 
 ### Minor changes since schema version 5
 
+* Add `analysis_step_versions`.
 * Add `preview_timestamp`.
 * Update `aliases` regex to add `igvf-dacc-processing-pipeline` as a namespace.
 * Update `aliases` regex to add `steven-gazal` as a namespace.
