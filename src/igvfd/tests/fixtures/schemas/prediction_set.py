64926744315858b46442e8c6aecb5ccc497f3462
Meenakshi Kagda <meenakshikagda@Meenakshis-MacBook-Pro.local>
2025-06-09 13:04:44 -0700

    update tests

--- src/igvfd/tests/fixtures/schemas/prediction_set.py
+++ src/igvfd/tests/fixtures/schemas/prediction_set.py
@@ -6,7 +6,7 @@ def base_prediction_set(testapp, lab, award, in_vitro_cell_line):
     item = {
         'award': award['@id'],
         'lab': lab['@id'],
-        'file_set_type': 'pathogenicity',
+        'file_set_type': 'functional effect',
         'samples': [in_vitro_cell_line['@id']]
     }
     return testapp.post_json('/prediction_set', item).json['@graph'][0]
