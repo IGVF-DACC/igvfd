a02787e1d5ec5b511970ae8e3956311905731201
Zhewei Shen <zwshen@stanford.edu>
2025-06-14 21:10:04 -0700

    add targeted_sample_term to mux samp sum

--- src/igvfd/tests/test_types_multiplexed_sample.py
+++ src/igvfd/tests/test_types_multiplexed_sample.py
@@ -1,10 +1,13 @@
 import pytest
 
 
-def test_summary(testapp, multiplexed_sample, tissue, in_vitro_cell_line, human_tissue, primary_cell, sample_term_endothelial_cell, in_vitro_differentiated_cell, in_vitro_organoid, sample_term_brown_adipose_tissue, sample_term_lymphoblastoid):
+def test_multiplexed_sample_summary(testapp, multiplexed_sample, tissue, in_vitro_cell_line, human_tissue, primary_cell, sample_term_endothelial_cell, in_vitro_differentiated_cell, in_vitro_organoid, sample_term_brown_adipose_tissue, sample_term_lymphoblastoid):
+    # Test: 2 samp types, same donor
     res = testapp.get(multiplexed_sample['@id'])
     assert res.json.get(
         'summary') == 'multiplexed K562 and adrenal gland, 1 donor, 2 samples'
+
+    # Test: 2 samp types, different donors
     testapp.patch_json(
         multiplexed_sample['@id'],
         {
@@ -14,6 +17,8 @@ def test_summary(testapp, multiplexed_sample, tissue, in_vitro_cell_line, human_
     res = testapp.get(multiplexed_sample['@id'])
     assert res.json.get(
         'summary') == 'multiplexed K562 and adrenal gland, 2 donors, 3 samples'
+
+    # Test: 4 samp types, different donors, 4 samples
     testapp.patch_json(
         multiplexed_sample['@id'],
         {
@@ -23,6 +28,8 @@ def test_summary(testapp, multiplexed_sample, tissue, in_vitro_cell_line, human_
     res = testapp.get(multiplexed_sample['@id'])
     assert res.json.get(
         'summary') == 'multiplexed K562, adrenal gland and pluripotent stem cell, 2 donors, 4 samples'
+
+    # Test: 4 samp types, different donors, 6 samples, 2 targeted sample terms
     testapp.patch_json(
         multiplexed_sample['@id'],
         {
@@ -56,7 +63,24 @@ def test_summary(testapp, multiplexed_sample, tissue, in_vitro_cell_line, human_
     )
     res = testapp.get(multiplexed_sample['@id'])
     assert res.json.get(
-        'summary') == 'multiplexed K562, adrenal gland, brown adipose tissue, endothelial cell of vascular tree and 1 more, 2 donors, 6 samples'
+        'summary') == 'multiplexed K562, adrenal gland, brown adipose tissue, endothelial cell of vascular tree and 1 more, 2 donors, 6 samples, induced to adrenal gland and brown adipose tissue'
+
+    # Test: 4 samp types, different donors, 6 samples, 1 targeted sample term
+    testapp.patch_json(
+        multiplexed_sample['@id'],
+        {
+            'multiplexed_samples': [
+                tissue['@id'],
+                in_vitro_cell_line['@id'],
+                human_tissue['@id'],
+                primary_cell['@id'],
+                in_vitro_differentiated_cell['@id']
+            ]
+        }
+    )
+    res = testapp.get(multiplexed_sample['@id'])
+    assert res.json.get(
+        'summary') == 'multiplexed K562, adrenal gland, brown adipose tissue, endothelial cell of vascular tree and pluripotent stem cell, 2 donors, 5 samples, induced to brown adipose tissue'
 
 
 def test_taxa(testapp, multiplexed_sample_mixed_species, multiplexed_sample):
