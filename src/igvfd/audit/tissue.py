a05634ee9c67334a74fc668594116188044d336c
Jennifer Jou <jjou@stanford.edu>
2025-06-19 17:01:42 -0700

    summary calc fixes

--- src/igvfd/audit/tissue.py
+++ src/igvfd/audit/tissue.py
@@ -13,12 +13,12 @@ def audit_tissue_ccf_id(value, system):
     '''
     [
         {
-            "audit_description": "Human tissues are expected to specify a common coordinate framework identifier (CCF ID).",
+            "audit_description": "Human tissues/organs are expected to specify a common coordinate framework identifier (CCF ID).",
             "audit_category": "missing CCF ID",
             "audit_level": "INTERNAL_ACTION"
         },
         {
-            "audit_description": "Non-human tissues are not expected to specify a common coordinate framework identifier (CCF ID).",
+            "audit_description": "Non-human tissues/organs are not expected to specify a common coordinate framework identifier (CCF ID).",
             "audit_category": "unexpected CCF ID",
             "audit_level": "INTERNAL_ACTION"
         }
@@ -29,14 +29,14 @@ def audit_tissue_ccf_id(value, system):
     if ('ccf_id' not in value) and (any(donor.startswith('/human-donors/') for donor in value.get('donors'))):
         value_id = system.get('path')
         detail = (
-            f'Tissue {audit_link(path_to_text(value_id), value_id)} '
+            f'Tissue/Organ {audit_link(path_to_text(value_id), value_id)} '
             f'is missing a `ccf_id`.'
         )
         yield AuditFailure(audit_message_human_tissue.get('audit_category', ''), f'{detail} {audit_message_human_tissue.get("audit_description", "")}', level=audit_message_human_tissue.get('audit_level', ''))
     if ('ccf_id' in value) and (value.get('taxa', '') != 'Homo sapiens'):
         value_id = system.get('path')
         detail = (
-            f'Tissue {audit_link(path_to_text(value_id), value_id)} '
+            f'Tissue/Organ {audit_link(path_to_text(value_id), value_id)} '
             f'has a `ccf_id` but is associated with a non-human donor.'
         )
         yield AuditFailure(audit_message_non_human_tissue.get('audit_category', ''), f'{detail} {audit_message_non_human_tissue.get("audit_description", "")}', level=audit_message_non_human_tissue.get('audit_level', ''))
