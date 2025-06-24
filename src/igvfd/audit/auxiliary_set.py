8349d3390ede1a01b61a3db26712b081cef1ac58
Ian Whaling <78115078+ian-whaling@users.noreply.github.com>
2025-06-24 10:06:51 -0700

    IGVF-2836-audit-no-barcode-map (#1595)

--- src/igvfd/audit/auxiliary_set.py
+++ src/igvfd/audit/auxiliary_set.py
@@ -29,8 +29,44 @@ def audit_missing_measurement_sets(value, system):
         yield AuditFailure(audit_message.get('audit_category', ''), f'{detail} {audit_message.get("audit_description", "")}', level=audit_message.get('audit_level', ''))
 
 
+def audit_missing_barcode_map(value, system):
+    '''
+    [
+        {
+            "audit_description": "Cell hashing barcode sequencing auxiliary sets are expected to link to a barcode to hashtag mapping.",
+            "audit_category": "missing barcode map",
+            "audit_level": "NOT_COMPLIANT"
+        },
+        {
+            "audit_description": "Cell hashing barcode sequencing auxiliary sets are expected to link to only link to tabular file with content type of barcode to hashtag mapping.",
+            "audit_category": "inconsistent barcode map",
+            "audit_level": "ERROR"
+        }
+    ]
+    '''
+    audit_message_missing = get_audit_message(audit_missing_barcode_map, index=0)
+    audit_message_inconsistent = get_audit_message(audit_missing_barcode_map, index=1)
+    if value['file_set_type'] == 'cell hashing barcode sequencing':
+        barcode_map = value.get('barcode_map', '')
+        if not (barcode_map):
+            detail = (
+                f'Auxiliary set {audit_link(path_to_text(value["@id"]), value["@id"])} has '
+                f'no `barcode_map`.'
+            )
+            yield AuditFailure(audit_message_missing.get('audit_category', ''), f'{detail} {audit_message_missing.get("audit_description", "")}', level=audit_message_missing.get('audit_level', ''))
+        else:
+            barcode_map_object = system.get('request').embed(barcode_map + '@@object?skip_calculated=true')
+            if barcode_map_object['content_type'] != 'barcode to hashtag mapping':
+                detail = (
+                    f'Auxiliary set {audit_link(path_to_text(value["@id"]), value["@id"])} links '
+                    f'to `barcode_map` {audit_link(path_to_text(barcode_map), barcode_map)} that is not a barcode to hashtag mapping.'
+                )
+                yield AuditFailure(audit_message_inconsistent.get('audit_category', ''), f'{detail} {audit_message_inconsistent.get("audit_description", "")}', level=audit_message_inconsistent.get('audit_level', ''))
+
+
 function_dispatcher_auxiliary_set_object = {
-    'audit_missing_measurement_sets': audit_missing_measurement_sets
+    'audit_missing_measurement_sets': audit_missing_measurement_sets,
+    'audit_missing_barcode_map': audit_missing_barcode_map
 }
 
 
