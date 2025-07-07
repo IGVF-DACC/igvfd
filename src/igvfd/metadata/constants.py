619f4b6f914905c1ef2dc5ef607dab568ca6cb69
khine <khzlin@stanford.edu>
2025-07-07 13:29:52 -0700

    IGVF-2225-file-column-embed

--- src/igvfd/metadata/constants.py
+++ src/igvfd/metadata/constants.py
@@ -18,12 +18,13 @@ METADATA_COLUMN_TO_FIELDS_MAPPING = OrderedDict(
         ('File accession', ['files.accession']),
         ('File id', ['files.@id']),
         ('File format', ['files.file_format']),
+        ('File format type', ['files.file_format_type']),
         ('File content', ['files.content_type']),
+        ('File content', ['files.summary']),
         ('Fileset accession', ['accession']),
-        ('Fileset classification', ['@type']),
         ('Fileset type', ['file_set_type']),
-        ('Preferred assay titles', ['preferred_assay_titles']),
-        ('Assay titles', ['assay_titles']),
+        ('Preferred assay titles', ['files.preferred_assay_titles']),
+        ('Assay titles', ['files.assay_titles']),
         ('Donor(s)', ['donors.accession']),
         ('Sample(s)', ['samples.accession']),
         ('Sample term name', ['samples.sample_terms.term_name']),
@@ -35,9 +36,20 @@ METADATA_COLUMN_TO_FIELDS_MAPPING = OrderedDict(
         ('File assembly', ['files.assembly']),
         ('File transcriptome annotation', ['files.transcriptome_annotation']),
         ('File controlled access', ['files.controlled_access']),
+        ('File anvil_url', ['files.anvil_url']),
         ('File md5sum', ['files.md5sum']),
         ('File derived from', ['files.derived_from']),
         ('File upload status', ['files.upload_status']),
+        ('File cell type annotation', ['files.cell_type_annotation']),
+        ('File sequencing run', ['files.sequencing_run']),
+        ('File flowcell ID', ['files.flowcell_id']),
+        ('File lane', ['files.lane']),
+        ('File mean read length', ['files.mean_read_length']),
+        ('File seqspecs', ['files.seqspecs']),
+        ('File seqspec document', ['files.seqspec_document']),
+        ('File sequencing kit', ['files.sequencing_kit']),
+        ('File sequencing platform', ['files.sequencing_platform'])
+        ('File workflow name', ['files.workflow.name'])
     ]
 )
 
