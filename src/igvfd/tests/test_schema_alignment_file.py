709288fb59f6248a4a6bafba4f460277dcb2d286
Jennifer Jou <jjou@stanford.edu>
2025-07-16 19:35:11 -0700

    Remove submitted assembly and transcriptome annotation

--- src/igvfd/tests/test_schema_alignment_file.py
+++ src/igvfd/tests/test_schema_alignment_file.py
@@ -7,36 +7,3 @@ def test_restricted_dbxrefs(testapp, alignment_file):
         alignment_file['@id'],
         {'dbxrefs': ['']}, expect_errors=True)
     assert res.status_code == 422
-
-
-def test_assembly_transcriptome_dependency(testapp, alignment_file):
-    res = testapp.patch_json(
-        alignment_file['@id'],
-        {'assembly': 'mm10',
-         'transcriptome_annotation': 'GENCODE M25'})
-    assert res.status_code == 200
-    res = testapp.patch_json(
-        alignment_file['@id'],
-        {'assembly': 'mm10',
-         'transcriptome_annotation': 'GENCODE M17'})
-    assert res.status_code == 200
-    res = testapp.patch_json(
-        alignment_file['@id'],
-        {'assembly': 'GRCm39',
-         'transcriptome_annotation': 'GENCODE M17'}, expect_errors=True)
-    assert res.status_code == 422
-    res = testapp.patch_json(
-        alignment_file['@id'],
-        {'assembly': 'GRCh38, mm10',
-         'transcriptome_annotation': 'GENCODE 32, GENCODE M23'})
-    assert res.status_code == 200
-    res = testapp.patch_json(
-        alignment_file['@id'],
-        {'assembly': 'GRCh38',
-         'transcriptome_annotation': 'GENCODE 32, GENCODE M23'}, expect_errors=True)
-    assert res.status_code == 422
-    res = testapp.patch_json(
-        alignment_file['@id'],
-        {'assembly': 'GRCh38',
-         'transcriptome_annotation': 'GENCODE 28'})
-    assert res.status_code == 200
