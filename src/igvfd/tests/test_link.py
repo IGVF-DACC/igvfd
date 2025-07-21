555a207f3f449441b53e0c8ffd0930479ccc8973
Mingjie Li <mingjiecn@gmail.com>
2025-07-21 17:23:46 -0500

    fix test

--- src/igvfd/tests/test_link.py
+++ src/igvfd/tests/test_link.py
@@ -72,7 +72,7 @@ def test_links_reverse(content, testapp, session):
     # DELETED sources are hidden from the list.
     target = targets[1]
     res = testapp.get('/testing-link-targets/%s/' % target['name'])
-    assert 'reverse' not in res.json
+    assert res.json['reverse'] == []
 
 
 def test_links_quoted_ids(content, testapp, session):
