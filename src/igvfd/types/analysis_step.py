96760fae68c2414ff736cbc42ce9848f5abeec7c
Keenan Graham <ksgraham@stanford.edu>
2025-07-25 18:28:47 -0700

    Remove workflow from analysis step and use UUID as name

--- src/igvfd/types/analysis_step.py
+++ src/igvfd/types/analysis_step.py
@@ -16,7 +16,7 @@ from pyramid.traversal import (
 
 @collection(
     name='analysis-steps',
-    unique_key='analysis_step:name',
+    unique_key='analysis_step:uuid',
     properties={
         'title': 'Analysis Steps',
         'description': 'Listing of analysis steps',
@@ -29,7 +29,6 @@ class AnalysisStep(Item):
         Path('award', include=['@id', 'component']),
         Path('lab', include=['@id', 'title']),
         Path('parents', include=['@id', 'title', 'status']),
-        Path('workflow', include=['@id', 'accession', 'name', 'status']),
         Path('submitted_by', include=['@id', 'title']),
         Path('analysis_step_versions.software_versions', include=[
              '@id', 'analysis_step_versions', 'software_versions', 'name'])
@@ -41,33 +40,6 @@ class AnalysisStep(Item):
 
     set_status_up = []
     set_status_down = []
-
-    def unique_keys(self, properties):
-        keys = super(AnalysisStep, self).unique_keys(properties)
-        keys.setdefault('analysis_step:name', []).append(self._name(properties))
-        return keys
-
-    @calculated_property(schema={
-        'title': 'Name',
-        'type': 'string',
-        'description': 'Full name of the analysis step.',
-        'comment': 'Do not submit. Value is automatically assigned by the server.',
-        'notSubmittable': True,
-        'uniqueKey': True
-    })
-    def name(self):
-        return self.__name__
-
-    @property
-    def __name__(self):
-        properties = self.upgrade_properties()
-        return self._name(properties)
-
-    def _name(self, properties):
-        root = find_root(self)
-        workflow_uuid = properties['workflow']
-        workflow = root.get_by_uuid(workflow_uuid)
-        return u'{}-{}'.format(workflow.upgrade_properties()['accession'], properties['step_label'])
 
     @calculated_property(schema={
         'title': 'Analysis Step Versions',
