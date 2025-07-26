f0fd077427462ff9d18fa93b9942a8bbddd8cfb3
Keenan Graham <ksgraham@stanford.edu>
2025-07-25 18:28:47 -0700

    Remove workflow from analysis step and use UUID as name

--- src/igvfd/types/workflow.py
+++ src/igvfd/types/workflow.py
@@ -22,37 +22,16 @@ class Workflow(Item):
     item_type = 'workflow'
     name_key = 'accession'
     schema = load_schema('igvfd:schemas/workflow.json')
-    rev = {
-        'analysis_steps': ('AnalysisStep', 'workflow')
-    }
+
     embedded_with_frame = [
         Path('award', include=['@id', 'component']),
         Path('lab', include=['@id', 'title']),
         Path('submitted_by', include=['@id', 'title']),
         Path('standards_page', include=['@id', 'title', 'status']),
         Path('publications', include=['@id', 'publication_identifiers', 'status']),
-        Path('analysis_steps.analysis_step_versions.software_versions.software',
-             include=['@id', 'title', 'name', 'analysis_step_types', 'output_content_types', 'input_content_types', 'analysis_step_versions', 'software_versions', 'software', 'status'])
     ]
 
     set_status_up = [
-        'analysis_steps',
         'standards_page'
     ]
     set_status_down = []
-
-    @calculated_property(schema={
-        'title': 'Analysis Steps',
-        'type': 'array',
-        'description': 'The analysis steps which are part of this workflow.',
-        'minItems': 1,
-        'uniqueItems': True,
-        'items': {
-            'title': 'Analysis Step',
-            'type': 'string',
-            'linkFrom': 'AnalysisStep.workflow',
-        },
-        'notSubmittable': True
-    })
-    def analysis_steps(self, request, analysis_steps):
-        return paths_filtered_by_status(request, analysis_steps)
