30c8ccd798368d449d56442b0175135d6eb74fca
Keenan Graham <ksgraham@stanford.edu>
2025-07-28 11:02:42 -0700

    Fix test

--- src/igvfd/tests/test_audit_workflow.py
+++ src/igvfd/tests/test_audit_workflow.py
@@ -1,30 +1,30 @@
 import pytest
 
 
-def test_audit_workflow_without_asvs(testapp, base_workflow, analysis_step_version, analysis_step_version_2):
+def test_audit_workflow_without_asvs(testapp, base_workflow_no_asv, analysis_step_version, analysis_step_version_2):
     # Test: workflow without analysis step versions (flag)
-    res = testapp.get(base_workflow['@id'] + '@@audit')
+    res = testapp.get(base_workflow_no_asv['@id'] + '@@audit')
     assert any(
         error['category'] == 'missing analysis step versions'
         for error in res.json['audit'].get('WARNING', [])
     )
 
     # Test: workflow with one analysis step version (no flag)
-    testapp.patch_json(base_workflow['@id'], {
+    testapp.patch_json(base_workflow_no_asv['@id'], {
         'analysis_step_versions': [analysis_step_version['@id']]
     })
-    res = testapp.get(base_workflow['@id'] + '@@audit')
+    res = testapp.get(base_workflow_no_asv['@id'] + '@@audit')
     assert all(
         error['category'] != 'missing analysis step versions'
         for error in res.json['audit'].get('WARNING', [])
     )
 
     # Test: workflow with two analysis step version (no flag)
-    testapp.patch_json(base_workflow['@id'],
+    testapp.patch_json(base_workflow_no_asv['@id'],
                        {'analysis_step_versions': [analysis_step_version['@id'],
                                                    analysis_step_version_2['@id']]}
                        )
-    res = testapp.get(base_workflow['@id'] + '@@audit')
+    res = testapp.get(base_workflow_no_asv['@id'] + '@@audit')
     assert all(
         error['category'] != 'missing analysis step versions'
         for error in res.json['audit'].get('WARNING', [])
