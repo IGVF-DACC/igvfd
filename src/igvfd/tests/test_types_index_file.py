6f2318fe47e02703c7b8acfdc8a35dc5b2436dce
Jennifer Jou <jjou@stanford.edu>
2025-08-01 09:51:01 -0700

    Address code review

--- src/igvfd/tests/test_types_index_file.py
+++ src/igvfd/tests/test_types_index_file.py
@@ -1,19 +1,19 @@
 import pytest
 
 
-# Test is temporarily commented out and should be restored in IGVF-2897.
-# def test_types_index_file_inherited_properties(testapp, index_file_bai, alignment_file):
-#     testapp.patch_json(
-#         alignment_file['@id'],
-#         {
-#             'transcriptome_annotation': 'GENCODE 43'
-#         }
-#     )
-#     res = testapp.get(index_file_bai['@id'])
-#     assert res.json.get('assembly') == 'GRCh38'
-#     assert res.json.get('transcriptome_annotation') == 'GENCODE 43'
-#     assert res.json.get('filtered') is False
-#     assert res.json.get('redacted') is False
+def test_types_index_file_inherited_properties(testapp, index_file_bai, alignment_file, reference_file):
+    testapp.patch_json(
+        reference_file['@id'],
+        {
+            'assembly': 'GRCh38',
+            'transcriptome_annotation': 'GENCODE 43'
+        }
+    )
+    res = testapp.get(index_file_bai['@id'])
+    assert res.json.get('assembly') == 'GRCh38'
+    assert res.json.get('transcriptome_annotation') == 'GENCODE 43'
+    assert res.json.get('filtered') is False
+    assert res.json.get('redacted') is False
 
 
 def test_types_index_file_summary(testapp, index_file_bai, alignment_file):
