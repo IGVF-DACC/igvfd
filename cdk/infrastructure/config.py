174cb0e4ca84728d63cbf3a2727f9ee3be1ffe12
Keenan Graham <ksgraham@stanford.edu>
2025-12-04 16:42:48 -0800

    Just 14_19

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -69,7 +69,7 @@ config: Dict[str, Any] = {
                                 InstanceClass.BURSTABLE3,
                                 InstanceSize.MEDIUM,
                             ),
-                            'engine_version': PostgresEngineVersion.VER_16_10
+                            'engine_version': PostgresEngineVersion.VER_14_19
                         },
                     }
                 ],
