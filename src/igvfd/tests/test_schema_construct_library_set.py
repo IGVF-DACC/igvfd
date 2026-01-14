7e1825bbd00fc20847a17158bf796858a70b10d6
Shengcheng Dong <shengchd@umich.edu>
2026-01-13 19:30:22 -0800

    IGVF-3211-add-dependency-interactor (#1853)

--- src/igvfd/tests/test_schema_construct_library_set.py
+++ src/igvfd/tests/test_schema_construct_library_set.py
@@ -18,7 +18,7 @@ def test_post_construct_library_set(award, lab, testapp):
 def test_dependencies_construct_library_set(award, lab, testapp, gene_myc_hs,
                                             construct_library_set_genome_wide,
                                             base_expression_construct_library_set,
-                                            construct_library_set_reporter, tabular_file):
+                                            construct_library_set_reporter, tabular_file, orf_foxp):
     res = testapp.patch_json(
         construct_library_set_genome_wide['@id'],
         {'scope': 'genes'
@@ -138,5 +138,30 @@ def test_dependencies_construct_library_set(award, lab, testapp, gene_myc_hs,
             'control_types': ['reference transduction'],
             'selection_criteria': ['TF binding sites'],
             'control_file_sets': [base_expression_construct_library_set['@id']]
+        })
+    assert res.status_code == 201
+    # either small_scale_gene_list or large_scale_gene_list is accepted when scope = 'interactors'
+    res = testapp.post_json(
+        '/construct_library_set',
+        {
+            'lab': lab['@id'],
+            'award': award['@id'],
+            'file_set_type': 'expression vector library',
+            'scope': 'interactors',
+            'selection_criteria': ['protein interactors'],
+            'orf_list': [orf_foxp['@id']],
+            'large_scale_gene_list': tabular_file['@id']
+        })
+    assert res.status_code == 201
+    res = testapp.post_json(
+        '/construct_library_set',
+        {
+            'lab': lab['@id'],
+            'award': award['@id'],
+            'file_set_type': 'expression vector library',
+            'scope': 'interactors',
+            'selection_criteria': ['protein interactors'],
+            'orf_list': [orf_foxp['@id']],
+            'small_scale_gene_list': [gene_myc_hs['@id']]
         })
     assert res.status_code == 201
