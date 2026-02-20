890ebcd0d23150fc754c0906fa90b1b34602fa39
shengchd <shengchd@umich.edu>
2026-02-20 12:17:38 -0800

    fix test 2

--- src/igvfd/tests/test_upgrade_software_version.py
+++ src/igvfd/tests/test_upgrade_software_version.py
@@ -49,6 +49,6 @@ def test_software_version_upgrade_6_7(upgrader, software_version_v6):
 def test_software_version_upgrade_7_8(upgrader, software_version_v7):
     value = upgrader.upgrade('software_version', software_version_v7, current_version='7', target_version='8')
     assert value['schema_version'] == '8'
-    assert value['source_url'] == 'https://temp-url.com/d31294875092e76ebb061eadc7998585'
+    assert value['source_url'] == 'https://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.4.4/'
     assert 'download_id' not in value
     assert value['notes'] == 'download_id d31294875092e76ebb061eadc7998585 was removed from an upgrade.'
