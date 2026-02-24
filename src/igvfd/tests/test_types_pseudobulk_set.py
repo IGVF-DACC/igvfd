b062519bc70a0e5a3cd2281fbab8370701faee8a
Jennifer Jou <jjou@stanford.edu>
2026-02-23 16:13:10 -0800

    Fix comments in test code

--- src/igvfd/tests/test_types_pseudobulk_set.py
+++ src/igvfd/tests/test_types_pseudobulk_set.py
@@ -2,10 +2,10 @@ import pytest
 
 
 def test_pseudobulk_set_summary(testapp, pseudobulk_set_base, in_vitro_cell_line):
-    # With no input_file_sets and no files present, summary is based on analysis file_set_type only.
+    # Case 1: Pseudobulk's source biosample is not a cell line.
     res = testapp.get(pseudobulk_set_base['@id']).json
     assert res.get('summary', '') == 'Pseudobulk of adrenal gland endothelial cell of vascular tree'
-    # When there are files, but no input_file_sets, summary says Unspecified.
+    # Case 2: Pseudobulk's source biosample is a cell line. Also, include cell_qualifier.
     testapp.patch_json(
         pseudobulk_set_base['@id'],
         {
