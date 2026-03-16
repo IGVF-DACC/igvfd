0b8cb0ac17f3c1d6277ac15e0f67f95f405108de
khine <khzlin@stanford.edu>
2026-03-15 23:29:45 -0700

    IGVF-3347-pseudobulk-ui

--- src/igvfd/types/file_set.py
+++ src/igvfd/types/file_set.py
@@ -2174,6 +2174,7 @@ class PseudobulkSet(FileSet):
     schema = load_schema('igvfd:schemas/pseudobulk_set.json')
     embedded_with_frame = FileSet.embedded_with_frame + [
         Path('input_file_sets', include=['@id', 'accession', 'aliases', 'file_set_type', 'status']),
+        Path('workflows', include=['@id', 'accession', 'name', 'uniform_pipeline', 'status', 'workflow_version']),
         Path('cell_type', include=['@id', 'term_name', 'status']),
         Path(
             'samples.sample_terms',
