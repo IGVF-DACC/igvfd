2ab7bf58c5d1a7811f070d3167117f004ac3cbb3
Jennifer Jou <jjou@stanford.edu>
2026-03-27 15:09:17 -0700

    Add assay title properties to Curated Set

--- src/igvfd/tests/test_schema_curated_set.py
+++ src/igvfd/tests/test_schema_curated_set.py
@@ -28,3 +28,23 @@ def test_patch_curated_set(award, lab, curated_set_genome, testapp):
         curated_set_genome['@id'],
         {'taxa': 'Homo sapiens'})
     assert res.status_code == 200
+
+
+def test_curated_set_assay_term(award, lab, curated_set_genome, assay_term_starr, testapp):
+    res = testapp.patch_json(
+        curated_set_genome['@id'],
+        {'preferred_assay_titles': ['SHARE-seq']}, expect_errors=True)
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        curated_set_genome['@id'],
+        {'assay_term': assay_term_starr['@id']}, expect_errors=True)
+    assert res.status_code == 422
+    res = testapp.patch_json(
+        curated_set_genome['@id'],
+        {
+            'preferred_assay_titles': ['SHARE-seq'],
+            'assay_term': assay_term_starr['@id'],
+            'file_set_type': 'external sequencing data'
+        }
+    )
+    assert res.status_code == 200
