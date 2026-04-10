6579278a6ee106238e4b6d87ba3f841ed21e15fc
Keenan Graham <ksgraham@stanford.edu>
2026-04-10 10:50:05 -0700

    Update variables

--- src/igvfd/metadata/recurse.py
+++ src/igvfd/metadata/recurse.py
@@ -6,39 +6,39 @@ from igvfd.metadata.constants import RECURSE_FILE_FIELDS
 from pyramid.view import view_config
 
 
-def extract_file_sets_and_files(file_set, can_downstream):
-    file_sets = set()
+def extract_related_file_sets_and_files(full_file_set, can_downstream):
+    related_file_sets = set()
     files = set()
-    file_set_type = file_set['@type'][0]
+    file_set_type = full_file_set['@type'][0]
     for link_field in RECURSE_FILE_SET_FIELDS.get(file_set_type, []):
-        if link_field not in file_set:
+        if link_field not in full_file_set:
             continue
-        value = file_set[link_field]
+        value = full_file_set[link_field]
         if value:
             if isinstance(value, list):
-                file_sets.update((v, False) for v in value)
+                related_file_sets.update((v, False) for v in value)
             else:
-                file_sets.add((value, False))
+                related_file_sets.add((value, False))
     if can_downstream:
         for link_field in RECURSE_FILE_SET_DOWNSTREAM_FIELDS.get(file_set_type, []):
-            if link_field not in file_set:
+            if link_field not in full_file_set:
                 continue
-            value = file_set[link_field]
+            value = full_file_set[link_field]
             if value:
                 if isinstance(value, list):
-                    file_sets.update((v, True) for v in value)
+                    related_file_sets.update((v, True) for v in value)
             else:
-                file_sets.add((value, True))
+                related_file_sets.add((value, True))
     for field in RECURSE_FILE_FIELDS:
-        if field in file_set:
-            value = file_set[field]
+        if field in full_file_set:
+            value = full_file_set[field]
             if value:
                 if isinstance(value, list):
                     files.update(value)
                 else:
                     files.add(value)
     return {
-        'file_sets': file_sets,
+        'related_file_sets': related_file_sets,
         'files': files,
     }
 
@@ -47,23 +47,23 @@ def find_all_file_sets_and_files(request, file_set_at_ids, include_downstream=Fa
     file_sets_seen = set()
     files_seen = set()
     file_sets = {
-        (at_id, include_downstream)
-        for at_id in file_set_at_ids
+        (file_set_at_id, include_downstream)
+        for file_set_at_id in file_set_at_ids
     }
     while file_sets:
-        fs, can_downstream = file_sets.pop()
-        if (fs, can_downstream) in file_sets_seen:
+        file_set_at_id, can_downstream = file_sets.pop()
+        if (file_set_at_id, can_downstream) in file_sets_seen:
             continue
-        file_sets_seen.add((fs, can_downstream))
-        full_fs = request.embed(fs + '@@object')
-        values = extract_file_sets_and_files(full_fs, can_downstream)
-        for rfs in values['file_sets']:
-            if rfs not in file_sets_seen:
-                file_sets.add(rfs)
+        file_sets_seen.add((file_set_at_id, can_downstream))
+        full_file_set = request.embed(file_set_at_id + '@@object')
+        values = extract_related_file_sets_and_files(full_file_set, can_downstream)
+        for related_file_set in values['related_file_sets']:
+            if related_file_set not in file_sets_seen:
+                file_sets.add(related_file_set)
         files_seen.update(values['files'])
     file_sets_seen = {
-        fs
-        for fs, can_downstream in file_sets_seen
+        file_set_at_id
+        for file_set_at_id, can_downstream in file_sets_seen
     }
     return {
         'files': sorted(files_seen),
