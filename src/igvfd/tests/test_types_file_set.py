c9df121ef0fd5fc52740a08c353df50cd9d1344b
Jennifer Jou <jjou@stanford.edu>
2026-04-16 14:43:51 -0700

    Adjust test due to reverted ticket

--- src/igvfd/tests/test_types_file_set.py
+++ src/igvfd/tests/test_types_file_set.py
@@ -197,7 +197,6 @@ def test_preferred_assay_slims(testapp, principal_analysis_set, measurement_set,
     testapp.patch_json(
         model_set_no_input['@id'],
         {
-            'assay_terms': [assay_term_scrna['@id']],
             'preferred_assay_titles': ['Perturb-seq']
         }
     )
