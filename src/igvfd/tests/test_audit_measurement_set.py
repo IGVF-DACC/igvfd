f4692d0926e261c91c018c0fb232d7f0723d86fa
Ian Whaling <78115078+ian-whaling@users.noreply.github.com>
2026-05-04 09:51:37 -0700

    IGVF-3478-fix-missing-mod (#2046)

--- src/igvfd/tests/test_audit_measurement_set.py
+++ src/igvfd/tests/test_audit_measurement_set.py
@@ -214,6 +214,8 @@ def test_audit_missing_modification(
     measurement_set,
     assay_term_crispr,
     tissue,
+    in_vitro_cell_line,
+    multiplexed_sample,
     crispr_modification_activation
 ):
     # CRISPR screens must also have modifications on all their samples
@@ -232,6 +234,29 @@ def test_audit_missing_modification(
         tissue['@id'],
         {
             'modifications': [crispr_modification_activation['@id']]
+        }
+    )
+    testapp.patch_json(
+        measurement_set['@id'],
+        {
+            'samples': [tissue['@id']]
+        }
+    )
+    res = testapp.get(measurement_set['@id'] + '@@audit')
+    assert all(
+        error['category'] != 'missing modification'
+        for error in res.json['audit'].get('NOT_COMPLIANT', [])
+    )
+    testapp.patch_json(
+        multiplexed_sample['@id'],
+        {
+            'multiplexed_samples': [in_vitro_cell_line['@id'], tissue['@id']]
+        }
+    )
+    testapp.patch_json(
+        measurement_set['@id'],
+        {
+            'samples': [multiplexed_sample['@id']]
         }
     )
     res = testapp.get(measurement_set['@id'] + '@@audit')
