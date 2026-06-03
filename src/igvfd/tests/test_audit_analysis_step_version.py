ec4cb7c6c73da9258f37e9cc61054c428770f086
Jennifer Jou <jjou@stanford.edu>
2026-06-03 10:57:37 -0700

    Remove print statement

--- src/igvfd/tests/test_audit_analysis_step_version.py
+++ src/igvfd/tests/test_audit_analysis_step_version.py
@@ -13,7 +13,6 @@ def test_audit_analysis_step_version_missing_workflows(
         for error in res.json['audit'].get('NOT_COMPLIANT', [])
     )
     res = testapp.get(analysis_step_version['@id'] + '@@audit')
-    print(res)
     assert all(
         error['category'] != 'missing workflows'
         for error in res.json['audit'].get('NOT_COMPLIANT', [])
