eda026d733e49b4caab5cc389009163b2848aa1e
Zhewei Shen <zwshen@stanford.edu>
2026-08-04 20:23:03 -0700

    rm test

--- src/igvfd/tests/test_audit_measurement_set.py
+++ src/igvfd/tests/test_audit_measurement_set.py
@@ -1391,9 +1391,9 @@ def test_audit_inconsistent_sequencing_kit(
         }
     )
     res = testapp.get(measurement_set['@id'] + '@@audit')
-    assert any(
-        error['category'] == 'missing sequencing kit'
-        for error in res.json['audit'].get('INTERNAL_ACTION', [])
+    assert all(
+        error['category'] != 'missing sequencing kit'
+        for error in res.json['audit'].get('NOT_COMPLIANT', [])
     )
     testapp.patch_json(
         measurement_set['@id'],
@@ -1402,10 +1402,6 @@ def test_audit_inconsistent_sequencing_kit(
         }
     )
     res = testapp.get(measurement_set['@id'] + '@@audit')
-    assert all(
-        error['category'] != 'missing sequencing kit'
-        for error in res.json['audit'].get('INTERNAL_ACTION', [])
-    )
     assert any(
         error['category'] == 'missing sequencing kit'
         for error in res.json['audit'].get('NOT_COMPLIANT', [])
