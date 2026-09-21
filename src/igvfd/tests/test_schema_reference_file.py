22b41d98e8b8f8fe3ec05c506422d4b3849d33a9
Ian Whaling <ian.whaling@gmail.com>
2026-09-21 12:01:16 -0700

    fixed test

--- src/igvfd/tests/test_schema_reference_file.py
+++ src/igvfd/tests/test_schema_reference_file.py
@@ -138,7 +138,17 @@ def test_schema_reference_file_regenerating_upload_credentials_on_invalid_file_c
     assert 'validation_error_detail' not in r.json['@graph'][0]
 
 
-def test_requiring_assembly_and_transcriptome_annotation(testapp, reference_file_with_guide_rna_sequences):
+@pytest.mark.parametrize('content_type', [
+    'guide RNA sequences reference',
+    'cell multiplexing oligonucleotide reference',
+])
+def test_requiring_assembly_and_transcriptome_annotation(testapp, reference_file_with_guide_rna_sequences, content_type):
+    # Other reference content types do not require assembly or transcriptome annotation.
+    testapp.patch_json(
+        reference_file_with_guide_rna_sequences['@id'],
+        {'content_type': content_type},
+        status=200,
+    )
     # tests genome ref files and transcriptome ref files requring assembly and transcriptome annotation metadata
     res = testapp.patch_json(
         reference_file_with_guide_rna_sequences['@id'],
@@ -173,6 +183,6 @@ def test_requiring_assembly_and_transcriptome_annotation(testapp, reference_file
     res = testapp.patch_json(
         reference_file_with_guide_rna_sequences['@id'],
         {
-            'content_type': 'guide RNA sequences reference'
+            'content_type': content_type
         })
     assert res.status_code == 200
