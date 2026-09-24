e4507d3a0ab5f569917e76088e38e2aa425413bc
Ian Whaling <ian.whaling@gmail.com>
2026-09-24 15:49:32 -0400

    audit

--- src/igvfd/tests/test_audit_item.py
+++ src/igvfd/tests/test_audit_item.py
@@ -1,4 +1,26 @@
 
+def test_audit_document_supersedes(testapp, experimental_protocol_document, plasmid_map_document):
+    original = experimental_protocol_document['@id']
+    replacement = plasmid_map_document['@id']
+    res = testapp.get(replacement + '@@audit')
+    assert not any(
+        error['category'] == 'inconsistent superseding'
+        for error in res.json['audit'].get('INTERNAL_ACTION', [])
+    )
+
+    testapp.patch_json(replacement, {'supersedes': [original]})
+    for status in ['in progress', 'deleted', 'archived', 'revoked']:
+        testapp.patch_json(original, {
+            'status': status,
+            'release_timestamp': '2024-03-06T12:34:56Z',
+        })
+        res = testapp.get(replacement + '@@audit')
+        assert any(
+            error['category'] == 'inconsistent superseding'
+            for error in res.json['audit'].get('INTERNAL_ACTION', [])
+        ) == (status not in ['archived', 'revoked'])
+
+
 def test_audit_item_schema_validation(testapp, item_donor):
     testapp.patch_json(item_donor['@id'] +
                        '?validate=false', {'disallowed': 'errs'})
